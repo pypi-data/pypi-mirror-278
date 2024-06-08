750ee31a7c1e5e5cd00ec5d8e71748842e16aa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:02:45 +0000

    Add package funda-scraper/1.2.1/funda_scraper-1.2.1-py3-none-any.whl

---
+++ packages/funda-scraper/funda_scraper-1.2.1-py3-none-any.whl/funda_scraper/utils.py
@@ -0,0 +1,12 @@
+"""Utilities for modules"""
+
+import logging
+
+logger = logging.getLogger("funda_scraper")
+logger.setLevel(logging.INFO)
+
+# # create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+logger.addHandler(ch)
