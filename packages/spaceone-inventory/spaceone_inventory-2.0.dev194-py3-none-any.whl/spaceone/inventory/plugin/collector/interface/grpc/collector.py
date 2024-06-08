45311650953051e3f54fb2d505f2be441a83fa8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:02:57 +0000

    Add package spaceone-inventory/2.0.dev194/spaceone_inventory-2.0.dev194-py3-none-any.whl

---
+++ packages/spaceone-inventory/spaceone_inventory-2.0.dev194-py3-none-any.whl/spaceone/inventory/plugin/collector/interface/grpc/collector.py
@@ -0,0 +1,95 @@
+from spaceone.core.pygrpc import BaseAPI
+from spaceone.api.inventory.plugin import collector_pb2, collector_pb2_grpc
+from spaceone.inventory.plugin.collector.error.response import *
+from spaceone.inventory.plugin.collector.service.collector_service import (
+    CollectorService,
+)
+
+VALID_RESOURCE_TYPES = {
+    "cloud_service_type": "inventory.CloudServiceType",
+    "cloud_service": "inventory.CloudService",
+    "error_data": "inventory.ErrorResource",
+    "region": "inventory.Region",
+    "namespace": "inventory.Namespace",
+    "metric": "inventory.Metric",
+}
+
+
+class Collector(BaseAPI, collector_pb2_grpc.CollectorServicer):
+    pb2 = collector_pb2
+    pb2_grpc = collector_pb2_grpc
+
+    def init(self, request, context):
+        params, metadata = self.parse_request(request, context)
+        collector_svc = CollectorService(metadata)
+        response: dict = collector_svc.init(params)
+        return self.dict_to_message(response)
+
+    def verify(self, request, context):
+        params, metadata = self.parse_request(request, context)
+        collector_svc = CollectorService(metadata)
+        collector_svc.verify(params)
+        return self.empty()
+
+    def collect(self, request, context):
+        params, metadata = self.parse_request(request, context)
+        collector_svc = CollectorService(metadata)
+        for response in collector_svc.collect(params):
+            response = self._select_valid_resource_and_resource_type(response)
+
+            if cloud_service_type := response.get("cloud_service_type"):
+                response["resource"] = cloud_service_type
+            elif cloud_service := response.get("cloud_service"):
+                response["resource"] = cloud_service
+            elif region := response.get("region"):
+                response["resource"] = region
+
+            elif namespace := response.get("namespace"):
+                response["resource"] = namespace
+
+            elif metric := response.get("metric"):
+                response["resource"] = metric
+
+            if error_data := response.get("error_data"):
+                response["resource"] = error_data
+
+            if response["state"] == "FAILURE":
+                response["resource_type"] = "inventory.ErrorResource"
+
+            if error_message := response.get("error_message"):
+                response["message"] = error_message
+
+            if match_keys := response.get("match_keys"):
+                response["match_rules"] = {}
+
+                for idx, keys in enumerate(match_keys, 1):
+                    response["match_rules"][str(idx)] = keys
+            yield self.dict_to_message(response)
+
+    def _select_valid_resource_and_resource_type(self, response: dict) -> dict:
+        resources = list(VALID_RESOURCE_TYPES.keys())
+        valid_resource = [
+            key for key in response.keys() if key in resources and response[key]
+        ]
+
+        self._check_resource_and_resource_type(valid_resource, response)
+
+        resources.remove(valid_resource[0])
+        for key in resources:
+            del response[key]
+
+        return response
+
+    @staticmethod
+    def _check_resource_and_resource_type(valid_resource, response) -> None:
+        if not len(valid_resource):
+            raise ERROR_NO_INPUT_FIELD()
+
+        if len(valid_resource) != 1:
+            raise ERROR_INVAILD_INPUT_FIELD(fields=valid_resource)
+
+        resource_type = response["resource_type"]
+        if resource_type != VALID_RESOURCE_TYPES[valid_resource[0]]:
+            raise ERROR_NOT_MATCH_RESOURCE_TYPE(
+                resource_type=resource_type, resource=valid_resource[0]
+            )
---
+++ packages/spaceone-inventory/spaceone_inventory-2.0.dev194-py3-none-any.whl/spaceone/inventory/plugin/collector/lib/utils.py
@@ -0,0 +1,168 @@
+import logging
+import json
+
+from spaceone.core.error import *
+from spaceone.core import utils
+from spaceone.inventory.plugin.collector.error.response import ERROR_INVALID_PARAMETER
+from spaceone.inventory.plugin.collector.model import (
+    CloudService,
+    CloudServiceType,
+    Reference,
+)
+from spaceone.inventory.plugin.collector.lib.metadata import (
+    convert_cloud_service_type_meta,
+    convert_cloud_service_meta,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+VALID_RESOURCE_TYPES = {
+    "cloud_service_type": "inventory.CloudServiceType",
+    "cloud_service": "inventory.CloudService",
+    "region": "inventory.Region",
+}
+
+
+def make_cloud_service_type(
+    name,
+    group,
+    provider,
+    metadata_path,
+    is_primary=False,
+    is_major=False,
+    service_code=None,
+    tags=None,
+    labels=None,
+) -> dict:
+    if tags is None:
+        tags = {}
+    if labels is None:
+        labels = []
+
+    cloud_service_type = CloudServiceType(
+        name=name,
+        group=group,
+        provider=provider,
+        json_metadata=utils.dump_json(
+            convert_cloud_service_type_meta(metadata_path), 4
+        ),
+        is_primary=is_primary,
+        is_major=is_major,
+        service_code=service_code,
+        tags=tags,
+        labels=labels,
+    )
+
+    return cloud_service_type.dict()
+
+
+def make_cloud_service(
+    name: str,
+    cloud_service_type: str,
+    cloud_service_group: str,
+    provider: str,
+    data: dict,
+    ip_addresses: list = None,
+    account: str = None,
+    instance_type: str = None,
+    instance_size: float = None,
+    region_code: str = None,
+    reference: Reference = None,
+    tags: dict = None,
+) -> dict:
+    if ip_addresses is None:
+        ip_addresses = []
+    if instance_size is None:
+        instance_size = 0
+    if tags is None:
+        tags = {}
+
+    cloud_service = CloudService(
+        name=name,
+        cloud_service_type=cloud_service_type,
+        cloud_service_group=cloud_service_group,
+        provider=provider,
+        json_data=utils.dump_json(data, 4),
+        json_metadata=utils.dump_json(
+            convert_cloud_service_meta(
+                provider, cloud_service_group, cloud_service_type
+            ),
+            4,
+        ),
+        ip_addresses=ip_addresses,
+        account=account,
+        instance_type=instance_type,
+        instance_size=instance_size,
+        region_code=region_code,
+        reference=reference,
+        tags=tags,
+    )
+
+    return cloud_service.dict()
+
+
+def make_response(
+    match_keys: list,
+    cloud_service_type=None,
+    cloud_service=None,
+    region=None,
+    metric=None,
+    namespace=None,
+    resource_type: str = "inventory.CloudService",
+) -> dict:
+    response = {
+        "state": "SUCCESS",
+        "resource_type": resource_type,
+        "match_keys": match_keys,
+    }
+
+    if resource_type == "inventory.CloudServiceType" and cloud_service_type is not None:
+        response["cloud_service_type"] = cloud_service_type
+        return response
+    elif resource_type == "inventory.CloudService" and cloud_service is not None:
+        response["cloud_service"] = cloud_service
+        return response
+    elif resource_type == "inventory.Region" and region is not None:
+        response["region"] = region
+        return response
+    elif resource_type == "inventory.Metric" and metric is not None:
+        response["metric"] = metric
+        return response
+    elif resource_type == "inventory.Namespace" and namespace is not None:
+        response["namespace"] = namespace
+        return response
+    else:
+        raise ERROR_INVALID_PARAMETER()
+
+
+def make_error_response(
+    error: Exception,
+    provider: str,
+    cloud_service_group: str,
+    cloud_service_type: str,
+    resource_type: str = "inventory.CloudService",
+    region_name: str = "",
+) -> dict:
+    if isinstance(error, ERROR_BASE):
+        error = ERROR_UNKNOWN(message=error)
+        error_message = error.message
+    elif type(error) is dict:
+        error_message = json.dumps(error)
+    else:
+        error_message = str(error)
+
+    _LOGGER.error(
+        f"[error_response] {cloud_service_group} / {cloud_service_type}",
+        exc_info=True,
+    )
+    return {
+        "state": "FAILURE",
+        "resource_type": "inventory.ErrorResource",
+        "error_message": error_message,
+        "error_data": {
+            "provider": provider,
+            "cloud_service_group": cloud_service_group,
+            "cloud_service_type": cloud_service_type,
+            "resource_type": resource_type,
+        },
+    }
