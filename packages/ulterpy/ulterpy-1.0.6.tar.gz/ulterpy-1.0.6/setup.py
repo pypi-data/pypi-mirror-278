faf21ae5a591efb665619eaa6c92d9247bd9e677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:02:46 +0000

    Add package ulterpy/1.0.6/ulterpy-1.0.6.tar.gz

---
+++ packages/ulterpy/ulterpy-1.0.6.tar.gz/ulterpy-1.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ulterpy',
+  version = '1.0.6',
+  description = 'ULTRA ERGONOMIC PYTHON',
+  url = 'https://github.com/pythonsystem/ulterpy',
+  author = 'Original By Python Community',
+  license = 'MIT',
+  long_description = open('README.txt', 'r').read(),
+  long_description_content_type = 'text/plain',
+  packages = find_packages(include = ['ulterpy*']),
+  install_requires = open('requirements.txt', 'r').read().split('\n'),
+  setup_requires = [],
+  tests_require = [],
+)
