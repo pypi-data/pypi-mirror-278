4a169076f014ff4016c129d6b2500445256cc9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:03:05 +0000

    Add package rtms-sdk/0.1.0/rtms_sdk-0.1.0.tar.gz

---
+++ packages/rtms-sdk/rtms_sdk-0.1.0.tar.gz/rtms_sdk-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='rtms_sdk',
+    version='0.1.0',
+    description='plc connect',
+    author='Bao Yun',
+    author_email='305713412@qq.com',
+    packages=find_packages()
+)
