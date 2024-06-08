ff046f5ef795006319f992cc8b9d0e232dd9cdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:03:17 +0000

    Add package personal-graph/2.2/personal_graph-2.2.tar.gz

---
+++ packages/personal-graph/personal_graph-2.2.tar.gz/personal_graph-2.2/personal_graph/graph_generator/__init__.py
@@ -0,0 +1,6 @@
+from personal_graph.graph_generator.generator import (
+    OpenAITextToGraphParser,
+    OllamaTextToGraphParser,
+)
+
+__all__ = ["OpenAITextToGraphParser", "OllamaTextToGraphParser"]
