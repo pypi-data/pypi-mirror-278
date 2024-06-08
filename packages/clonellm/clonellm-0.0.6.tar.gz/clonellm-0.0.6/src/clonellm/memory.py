a40c26f5f9e53481ee640798972ca62f49dcb4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:02:56 +0000

    Add package clonellm/0.0.6/clonellm-0.0.6.tar.gz

---
+++ packages/clonellm/clonellm-0.0.6.tar.gz/clonellm-0.0.6/src/clonellm/memory.py
@@ -0,0 +1,47 @@
+from typing import Sequence
+
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+_store = {}
+
+__all__ = ("InMemoryHistory", "get_session_history", "get_session_history_size", "clear_session_history")
+
+
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    """In memory implementation of chat message history."""
+
+    messages: list[BaseMessage] = Field(default_factory=list)
+    memory: int = -1
+
+    def add_messages(self, messages: Sequence[BaseMessage]) -> None:
+        """Add a list of messages to the store
+
+        Args:
+            messages (Sequence[BaseMessage]): A list of BaseMessage objects to store.
+
+        """
+        for message in messages:
+            self.messages.append(message)
+        if self.memory > 0:
+            self.messages = self.messages[-self.memory :]
+
+    def clear(self) -> None:
+        self.messages = []
+
+
+def get_session_history(session_id: str) -> InMemoryHistory:
+    if session_id not in _store:
+        _store[session_id] = InMemoryHistory()
+    return _store[session_id]
+
+
+def get_session_history_size(session_id: str) -> int:
+    if session_id not in _store:
+        return 0
+    return len(_store[session_id].messages)
+
+
+def clear_session_history(session_id: str) -> None:
+    _ = _store.pop(session_id, None)
