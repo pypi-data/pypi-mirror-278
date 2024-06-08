39f0ad3554b776523e3eb51e2a045e9605978683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:03:18 +0000

    Add package composio-claude/0.3.9rc3/composio_claude-0.3.9rc3-py3-none-any.whl

---
+++ packages/composio-claude/composio_claude-0.3.9rc3-py3-none-any.whl/composio_claude/__init__.py
@@ -0,0 +1,11 @@
+from composio_claude.toolset import ComposioToolset
+
+from composio.client.enums import Action, App, Tag
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "ComposioToolset",
+)
