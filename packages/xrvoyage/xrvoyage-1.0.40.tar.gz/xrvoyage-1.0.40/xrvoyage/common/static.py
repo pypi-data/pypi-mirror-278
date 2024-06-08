603b975e70c4f613e37cf5f5698bb51123755773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:02:45 +0000

    Add package xrvoyage/1.0.40/xrvoyage-1.0.40.tar.gz

---
+++ packages/xrvoyage/xrvoyage-1.0.40.tar.gz/xrvoyage-1.0.40/xrvoyage/common/static.py
@@ -0,0 +1,10 @@
+try:
+    from ..version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
+
+def get_version():
+    return __version__
+
+def print_version():
+    print(__version__)
