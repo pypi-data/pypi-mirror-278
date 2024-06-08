2128e1e4603c907e7721d67dc605e63812baa42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:02:59 +0000

    Add package qfpy/11.1.2/qfpy-11.1.2.tar.gz

---
+++ packages/qfpy/qfpy-11.1.2.tar.gz/qfpy-11.1.2/src/qfpy/character.py
@@ -0,0 +1,20 @@
+"""
+def to_simplified_chinese(string: str) -> str
+"""
+
+import re
+
+import zhconv
+
+
+def to_simplified_chinese(string: str) -> str:
+    """
+    转为简体中文
+    """
+    return zhconv.convert(string, "zh-cn")
+
+def extract_ch_en_num(string: str) -> str:
+    """
+    提取中文、英文、数字
+    """
+    return ''.join(re.findall('[\u4e00-\u9fa5a-zA-Z0-9]', string))
