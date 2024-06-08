0c5f387b9a6da7c7e2be1265e8a8b20219c5e792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:03:13 +0000

    Add package Skoal/0.401/Skoal-0.401.tar.gz

---
+++ packages/Skoal/Skoal-0.401.tar.gz/Skoal-0.401/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Skoal',
+    version='0.401',
+    packages=find_packages(),
+    scripts=['scripts/skoal.py'],
+    entry_points={
+        'console_scripts': [
+            'skoal=skoal.main:main',
+        ],
+    },
+    install_requires=[
+        'numpy>=1.26.4',
+        'scikit-learn>=1.4.2',
+        'astropy>=6.0.1',
+        'astropy-healpix>=1.0.3',
+        'astroplan>=0.10',
+        'scipy>=1.13.0',
+        'argparse',
+        'configparser>=7.0.0',
+        'requests>=2.31.0',
+        'ligo-gracedb>=2.12.0',
+        'lxml>=5.2.1',
+        'urllib3>=2.2.1',
+    ],
+    author='Benny Border',
+    author_email='borderbenja@gmail.com',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/borderbenja05/skoal',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
