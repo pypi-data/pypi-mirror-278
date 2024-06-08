c2e0721df5ac2035ef3dff6712388aac3dca992d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 14:02:37 +0000

    Add package indexify-extractor-sdk/0.0.75/indexify_extractor_sdk-0.0.75.tar.gz

---
+++ packages/indexify-extractor-sdk/indexify_extractor_sdk-0.0.75.tar.gz/indexify_extractor_sdk-0.0.75/indexify_extractor_sdk/content_downloader.py
@@ -0,0 +1,122 @@
+import boto3
+
+from .base_extractor import Content
+from . import coordinator_service_pb2
+from urllib.parse import urlparse
+from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
+from google.cloud import storage
+import httpx
+from typing import Dict
+import asyncio
+from google.protobuf.json_format import MessageToDict
+
+
+def disk_loader(file_path: str):
+    print(file_path)
+    file_path = file_path.removeprefix("file:/")
+    with open(file_path, "rb") as f:
+        return f.read()
+
+
+def s3_loader(s3_url: str) -> bytes:
+    parsed_url = urlparse(s3_url)
+    bucket_name = parsed_url.netloc
+    key = parsed_url.path.lstrip("/")
+
+    s3 = boto3.client("s3")
+
+    response = s3.get_object(Bucket=bucket_name, Key=key)
+    return response["Body"].read()
+
+
+def azure_blob_loader(blob_url: str) -> bytes:
+    token_credential = DefaultAzureCredential()
+    parsed_url = urlparse(blob_url)
+    account_url = f"https://{parsed_url.netloc}"
+    container_name = parsed_url.path.split("/")[1]
+    blob_name = "/".join(parsed_url.path.split("/")[2:])
+
+    blob_service_client = BlobServiceClient(
+        account_url=account_url, credential=token_credential
+    )
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, blob=blob_name
+    )
+
+    return blob_client.download_blob().readall()
+
+
+def gcp_storage_loader(storage_url: str) -> bytes:
+    parsed_url = urlparse(storage_url)
+    bucket_name = parsed_url.netloc
+    blob_name = parsed_url.path.lstrip("/")
+
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+
+    blob = bucket.blob(blob_name)
+    return blob.download_as_bytes()
+
+
+async def fetch_url(id, url):
+    try:
+        async with httpx.AsyncClient() as client:
+            print(f"downloading url {url}")
+            response = await client.get(url, follow_redirects=True)
+            response.raise_for_status()
+            return id, response.read()
+    except Exception as e:
+        return id, e
+
+
+async def download_parallel(urls):
+    tasks = [fetch_url(id, url) for id, url in urls.items()]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+    return results
+
+
+async def download_content(urls: Dict[str, str]) -> Dict[str, Content]:
+    out = {}
+    disk_urls = {}
+    s3_urls = {}
+    http_urls = {}
+    gs_urls = {}
+    for task_id, url in urls.items():
+        if url.startswith("file://"):
+            disk_urls[task_id] = url
+        elif url.startswith("s3://"):
+            s3_urls[task_id] = url
+        elif url.startswith("https://") or url.startswith("http://"):
+            http_urls[task_id] = url
+        elif url.startswith("gs://"):
+            gs_urls[task_id] = url
+        else:
+            out[task_id] = Exception(f"unsupported storage url {url}")
+    for task_id, url in gs_urls.items():
+        out[task_id] = gcp_storage_loader(url)
+    for task_id, url in s3_urls.items():
+        out[task_id] = s3_loader(url)
+    for task_id, url in disk_urls.items():
+        out[task_id] = disk_loader(url)
+
+    result = await download_parallel(http_urls)
+    for task_id, b in result:
+        out[task_id] = b
+
+    return out
+
+
+def create_content(bytes, task: coordinator_service_pb2.Task) -> Content:
+    metadata = task.content_metadata
+
+    labels = {}
+    for key, value in metadata.labels.items():
+        labels[key] = MessageToDict(value)
+
+    return Content(
+        content_type=metadata.mime,
+        data=bytes,
+        features=[],
+        labels=labels,
+    )
---
+++ packages/indexify-extractor-sdk/indexify_extractor_sdk-0.0.75.tar.gz/indexify_extractor_sdk-0.0.75/indexify_extractor_sdk/indexify_extractor.py
@@ -0,0 +1,119 @@
+import asyncio
+from . import coordinator_service_pb2
+from .base_extractor import ExtractorWrapper, ExtractorDescription
+from typing import Optional, Tuple, List
+from .base_extractor import Content, EXTRACTOR_MODULE_PATH
+import nanoid
+import json
+from .extractor_worker import ExtractorModule, create_executor, describe
+from .agent import ExtractorAgent
+import os
+from .coordinator_service_pb2 import Extractor
+from .downloader import save_extractor_description
+
+def local(extractor: str, text: Optional[str] = None, file: Optional[str] = None):
+    if text and file:
+        raise ValueError("You can only pass either text or file, not both.")
+    if not text and not file:
+        raise ValueError("You need to pass either text or file")
+    if text:
+        content = Content.from_text(text)
+    if file:
+        content = Content.from_file(file)
+    module, cls = extractor.split(":")
+    wrapper = ExtractorWrapper(module, cls)
+    result = wrapper.extract_batch({"task_id": content}, input_params={"task_id": "{}"})
+    print(result)
+
+
+def split_validate_extractor(name: str) -> Tuple[str, str]:
+    try:
+        module, cls = name.split(":")
+    except ValueError:
+        raise ValueError(
+            "The extractor name should be in the format 'module_name:class_name'"
+        )
+    return module, cls
+
+
+def join(
+    workers: int,
+    listen_port: int,
+    coordinator_addr: str = "localhost:8950",
+    ingestion_addr: str = "localhost:8900",
+    advertise_addr: Optional[str] = None,
+    config_path: Optional[str] = None,
+    extractor: Optional[str] = None,
+):
+    print(f"joining {coordinator_addr} and sending extracted content to {ingestion_addr}")
+    executor = create_executor(workers=workers, extractor_id=extractor)
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    descriptions: List[ExtractorDescription] = asyncio.get_event_loop().run_until_complete(
+        describe(asyncio.get_event_loop(), executor)
+    )
+
+    # Available extractors locally.
+    extractors: List[Extractor] = [] 
+
+    for description in descriptions:
+        embedding_schemas = {}
+        for name, embedding_schema in description.embedding_schemas.items():
+            embedding_schemas[name] = embedding_schema.model_dump_json()
+
+        metadata_schemas = {}
+        for name, metadata_schema in description.metadata_schemas.items():
+            metadata_schemas[name] = json.dumps(metadata_schema)
+
+        extractors.append(Extractor(
+            name=description.name,
+            description=description.description,
+            input_params=description.input_params,
+            input_mime_types=description.input_mime_types,
+            metadata_schemas=metadata_schemas,
+            embedding_schemas=embedding_schemas,
+        ))
+
+    id = nanoid.generate()
+    print(f"executor id: {id}")
+
+    server = ExtractorAgent(
+        id,
+        extractors=extractors,
+        executor=executor,
+        coordinator_addr=coordinator_addr,
+        num_workers=workers,
+        extractor_arg=extractor,
+        listen_port=listen_port,
+        ingestion_addr=ingestion_addr,
+        advertise_addr=advertise_addr,
+        config_path=config_path
+    )
+
+    try:
+        asyncio.get_event_loop().run_until_complete(server.run())
+    except asyncio.CancelledError as ex:
+        print("exiting gracefully", ex)
+
+
+def describe_sync(extractor):
+    module, cls = extractor.split(":")
+    wrapper = ExtractorWrapper(module, cls)
+    print(wrapper.describe())
+
+
+def install_local(extractor):
+    module, cls = extractor.split(":")
+    wrapper = ExtractorWrapper(module, cls)
+    description = wrapper.describe()
+
+    # Copy everything in the current directory to the extractors directory.
+    to_module = f"custom_{module}"
+    destination = os.path.join(EXTRACTOR_MODULE_PATH, to_module)
+    os.system(f"cp -r . {destination}")
+    print(f"copied to {destination} for testing")
+
+    # Create a new extractor description.
+    extractor_id = f"{to_module}.{module}:{cls}"
+    save_extractor_description(extractor_id, description)
+
+    print("extractor ready for testing. Run: indexify-extractor join-server")
