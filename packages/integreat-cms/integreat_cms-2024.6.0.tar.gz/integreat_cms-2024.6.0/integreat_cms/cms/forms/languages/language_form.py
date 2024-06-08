5ad4d8943e1ef42dae579ef9d4c89431afba3fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 15:03:00 +0000

    Add package integreat-cms/2024.6.0/integreat_cms-2024.6.0.tar.gz

---
+++ packages/integreat-cms/integreat_cms-2024.6.0.tar.gz/integreat_cms-2024.6.0/integreat_cms/cms/forms/languages/language_form.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ...models import Language
+from ..custom_model_form import CustomModelForm
+
+if TYPE_CHECKING:
+    from typing import Any
+
+
+class LanguageForm(CustomModelForm):
+    """
+    Form for creating and modifying language objects
+    """
+
+    class Meta:
+        """
+        This class contains additional meta configuration of the form class, see the :class:`django.forms.ModelForm`
+        for more information.
+        """
+
+        #: The model of this :class:`django.forms.ModelForm`
+        model = Language
+        #: The fields of the model which should be handled by this form
+        fields = [
+            "slug",
+            "bcp47_tag",
+            "english_name",
+            "native_name",
+            "text_direction",
+            "table_of_contents",
+            "social_media_webapp_title",
+            "social_media_webapp_description",
+            "primary_country_code",
+            "secondary_country_code",
+            "message_content_not_available",
+            "message_partial_live_content_not_available",
+        ]
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        r"""
+        Initialize language form
+
+        :param \*args: The supplied arguments
+        :param \**kwargs: The supplied keyword arguments
+        """
+
+        # Instantiate ModelForm
+        super().__init__(*args, **kwargs)
+
+        # Sort countries by translated name
+        sorted_language_choices = sorted(
+            self.fields["primary_country_code"].choices, key=lambda x: x[1]
+        )
+        self.fields["primary_country_code"].choices = sorted_language_choices
+        self.fields["secondary_country_code"].choices = sorted_language_choices
+
+        # Make left border rounded if no flag is selected yet
+        if not self.instance.primary_country_code:
+            self.fields["primary_country_code"].widget.attrs[
+                "class"
+            ] = "rounded-l border-l"
+        if not self.instance.secondary_country_code:
+            self.fields["secondary_country_code"].widget.attrs[
+                "class"
+            ] = "rounded-l border-l"
