0cfaeeca4cbe64f6941aff04963f9f23f2cc72dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-08 14:02:41 +0000

    Add package fitxf/0.0.4/fitxf-0.0.4-py3-none-any.whl

---
+++ packages/fitxf/fitxf-0.0.4-py3-none-any.whl/fitxf/__init__.py
@@ -0,0 +1,4 @@
+# Fit
+from nwae.math.fit.cluster.ClusterUnitTest import Cluster, ClusterUnitTest
+from nwae.math.fit.cluster.ClusterCosineUT import ClusterCosine, ClusterCosineUnitTest
+from nwae.math.fit.transform.FitXformPcaUnitTest import FitXformPca, FitXformPcaUnitTest
