d295693925a86f6c2ba899cfe4f866129528317f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:37 +0000

    Add package pygarmin/2.0.1/pygarmin-2.0.1-py3-none-any.whl

---
+++ packages/pygarmin/pygarmin-2.0.1-py3-none-any.whl/pygarmin/models.py
@@ -0,0 +1,155 @@
+# Garmin models
+
+# For reference, here are some of the product ID numbers used by different
+# Garmin models. Notice that this is not a one-to-one mapping in either
+# direction!
+
+product_ids = {
+    7:    ("GPS 50"),
+    13:   ("GPS 75"),
+    14:   ("GPS 55"),
+    15:   ("GPS 55 AVD"),
+    18:   ("GPS 65"),
+    20:   ("GPS 150"),
+    22:   ("GPS 95 AVD"),
+    23:   ("GPS 75"),
+    24:   ("GPS 95"),
+    25:   ("GPS 85"),
+    29:   ("GPSMAP 205",
+           "GPSMAP 210",
+           "GPSMAP 220"),
+    31:   ("GPS 40",
+           "GPS 45"),
+    33:   ("GNC 300"),
+    34:   ("GPS 155",
+           "GPS 165"),
+    35:   ("GPS 95"),
+    36:   ("GPS 95 AVD",
+           "GPS 95 XL"),
+    39:   ("GPS 89"),
+    41:   ("GPS 38",
+           "GPS 40",
+           "GPS 45 XL",
+           "GPS 45"),
+    42:   ("GPS 75"),
+    44:   ("GPSMAP 205"),
+    45:   ("GPS 90"),
+    47:   ("GPS 120"),
+    48:   ("GPSMAP 195"),
+    49:   ("GPSMAP 130",
+           "GPSMAP 135 Sounder",
+           "GPSMAP 175",
+           "GPSMAP 230",
+           "GPSMAP 235 Sounder"),
+    50:   ("GPSCOM 170"),
+    52:   ("GNC 250"),
+    53:   ("GPSCOM 190"),
+    55:   ("GPS 120 Chinese"),
+    56:   ("GPS 38 Chinese",
+           "GPS 40 Chinese",
+           "GPS 45 Chinese"),
+    59:   ("GPS II"),
+    61:   ("GPS 125 Sounder"),
+    62:   ("GPS 38 Japanese",
+           "GPS 40 Japanese"),
+    64:   ("GNC 250 XL",
+           "GPS 150 XL"),
+    67:   ("StreetPilot I"),  # gpsman
+    71:   ("GPS III Pilot"),
+    72:   ("GPS III"),
+    73:   ("GPS II Plus"),
+    74:   ("GPS 120 XL"),
+    76:   ("GPSMAP 130 Chinese",
+           "GPSMAP 230 Chinese"),
+    77:   ("GPS 12 XL",
+           "GPS 12"),
+    87:   ("GPS 12"),
+    88:   ("GPSMAP 215",
+           "GPSMAP 225"),
+    89:   ("GPSMAP 180"),  # gpsman
+    95:   ("GPS 126",
+           "GPS 128"),
+    96:   ("GPS 12 XL",
+           "GPS 12",
+           "GPS 48"),
+    97:   ("GPS II Plus"),
+    98:   ("GNC 300 XL",
+           "GPS 155 XL"),
+    100:  ("GPS 126 Chinese",
+           "GPS 128 Chinese"),
+    103:  ("GPS 12 Arabic"),  # gpsman
+    105:  ("GPS 12 XL Japanese"),
+    106:  ("GPS 12 XL Chinese"),
+    119:  ("GPS III Plus"),  # gpsman
+    111:  ("eMap"),  # gpsman
+    112:  ("GPS 92"),
+    116:  ("GPS 12CX"),  # gpsman
+    126:  ("GPSMAP 162"),  # gpsman
+    128:  ("GPSMAP 295"),  # gpsman
+    129:  ("GPS 12 Map"),  # gpsman
+    130:  ("eTrex"),  # gpsman
+    136:  ("GPSMAP 176"),  # gpsman
+    138:  ("GPS 12"),  # gpsman
+    141:  ("eTrex Summit"),  # gpsman
+    145:  ("GPSMAP 196"),  # gpsman
+    151:  ("StreetPilot 3"),  # gpsman
+    154:  ("eTrex Venture",
+           "eTrex Mariner"),  # gpsman
+    155:  ("GPS 5"),  # gpsman
+    156:  ("eTrex Europe"),  # gpsman
+    169:  ("eTrex Vista"),  # gpsman
+    173:  ("GPS 76"),  # gpsman
+    177:  ("GPSMAP 76"),  # gpsman
+    179:  ("eTrex Legend"),  # gpsman
+    194:  ("GPSMAP 76S"),  # gpsman
+    197:  ("Rino 110"),  # gpsman
+    209:  ("Rino 120"),  # gpsman
+    219:  ("eTrex Legend Japanese"),  # gpsman
+    231:  ("Quest"),  # gpsman
+    247:  ("GPS 72"),  # gpsman
+    248:  ("Geko 201"),  # gpsman
+    256:  ("Geko 301"),  # gpsman
+    264:  ("Rino 130"),  # gpsman
+    273:  ("GPS 18USB"),  # gpsman
+    282:  ("Forerunner"),  # gpsman
+    283:  ("Forerunner 301"),  # gpsman
+    285:  ("GPSmap 276C"),  # gpsman
+    289:  ("GPS 60"),  # gpsman
+    291:  ("GPSMAP 60CS"),
+    292:  ("GPSMAP 60CSx",
+           "GPSMAP 76CSx"),  # gpsman
+    295:  ("eTrex Summit"),  # gpsman
+    308:  ("GPSMAP 60"),  # gpsman
+    314:  ("ForeTrex"),  # gpsman
+    315:  ("eTrex Legend C",
+           "eTrex Vista C"),  # gpsman
+    382:  ("StreetPilot c320"),  # gpsman
+    404:  ("StreetPilot 2720"),  # gpsman
+    411:  ("eTrex Legend"),  # gpsman
+    419:  ("eTrex Venture"),  # gpsman
+    420:  ("eTrex Vista"),  # gpsman
+    421:  ("eTrex Legend Cx"),  # gpsman
+    430:  ("GPS 72"),  # gpsman
+    439:  ("GPSMAP 76"),  # gpsman
+    450:  ("Edge 205",
+           "Edge 305"),  # gpsman
+    481:  ("StreetPilot c340"),  # gpsman
+    484:  ("Forerunner 205",
+           "Forerunner 305"),  # gpsman
+    497:  ("StreetPilot c320",
+           "StreetPilot c330"),  # gpsman
+    532:  ("StreetPilot i2"),  # gpsman
+    557:  ("GPSMAP 378"),  # gpsman
+    574:  ("Geko 201"),  # gpsman
+    577:  ("Rino 530HCx"),  # gpsman
+    694:  ("eTrex Legend HCx",
+           "eTrex Vista HCx"),  # gpsman
+    695:  ("eTrex Summit HC",
+           "eTrex Venture HC"),  # gpsman
+    696:  ("eTrex H"),  # gpsman
+    786:  ("eTrex Summit HC",
+           "eTrex Venture HC"),  # gpsman
+    811:  ("GPS 20x USB"),  # gpsman
+    957:  ("eTrex Legend H"),  # gpsman
+    1095: ("GPS 72H"),  # gpsman
+}
