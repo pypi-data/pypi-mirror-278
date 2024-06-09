558f334f01b8cb82abeb28a8ffbe1ac1a07ad754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:21 +0000

    Add package magiceight/1.0/magiceight-1.0.tar.gz

---
+++ packages/magiceight/magiceight-1.0.tar.gz/magiceight-1.0/README.md
@@ -0,0 +1,3 @@
+# Eight
+
+Eight is a magic eight ball on the command line, written and packaged in Python!
---
+++ packages/magiceight/magiceight-1.0.tar.gz/magiceight-1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+
+
+setup(
+    name="magiceight",
+    version="1.0",
+    description="A magic eight ball CLI program",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LordUbuntu/MENACE",
+    keywords=["python", "fortune", "magic eight ball", "eight ball"],
+    license="MIT",
+    author="Jacobus Burger",
+    author_email="therealjacoburger@gmail.com",
+    packages=["magiceight"],
+    extras_require={
+        "dev": ["pytest>=7.2", "twine>=4.0.2", "color50>=1.0.1"],
+    },
+    python_requires=">=3.10",
+    platforms=["any"],
+    py_modules=["magiceight"],
+    entry_points={
+        "console_scripts": ["magiceight=magiceight.__main__:main"]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Topic :: Games/Entertainment",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
