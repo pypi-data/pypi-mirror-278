ca7c003d498c0dc39288fbbfe2d56dc35d645649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:41 +0000

    Add package STalk/0.0.3/stalk-0.0.3.tar.gz

---
+++ packages/STalk/stalk-0.0.3.tar.gz/stalk-0.0.3/src/Talk/NetObject/Client.py
@@ -0,0 +1,95 @@
+import socket
+import time
+import os
+from ..Command import Commands
+from ..NetObject import Workers
+
+class Client:
+    def __init__(self):
+        pass
+    def ConnectClient(self, HOST, PORT):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(b"Hello, world")
+
+class SleepyClient:
+    def __init__(self, sleeptime):
+        self.sleeptime = sleeptime
+    def ConnectClient(self, HOST, PORT):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((HOST, PORT))
+                while True:
+                    s.send(b"Hello, world")
+                    time.sleep(self.sleeptime)
+            except KeyboardInterrupt:
+                s.close()
+
+class CommandClient:
+    def __init__(self, name, commands, id):
+        self.name = name
+        self.workerthread = None
+        self.workerobject = None
+        self.isbusy = None
+        self.commands = commands
+        self.id = id
+    def ConnectClient(self, HOST, PORT):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                while True:
+                    try:
+                        s.connect((HOST, PORT))
+                        break
+                    except:
+                        time.sleep(1)
+                s.sendall(b"<CONNECTED>" + self.name)
+                data = ""
+                while len(data) == 0:
+                    data = s.recv(1024).decode()
+                if data != "<WELCOME " + self.name.decode() + ">":
+                    raise Exception("SERVER DID NOT REPOND CORRECTLY, INSTEAD GOT: " + data)
+                strid = str(self.id)
+                s.sendall(strid.encode("utf-8"))
+                if s.recv(1024).decode() != "<VALID>":
+                    raise Exception("RECEIVED INCORRECT RESPONSE")
+                s.sendall(b"<CLIENT>")
+                print("CLIENT CONNECTED")
+                self.workerthread, self.workerobject = Workers.StatusWorkerClient(HOST, PORT, self.name.decode(), self.id, self, self.commands)
+                commands = []
+                while True:
+                    command = s.recv(1024).decode()
+                    print(f"RECIEVED {command}")
+                    if command == "<END>" or command == "<END><END>":
+                        self.isbusy = True
+                        s.sendall(b"<END_RECIEVED>")
+                        if s.recv(1024).decode() != "<START_JOB>":
+                            raise Exception("RECEIVED INCORRECT RESPONSE")
+                        Command = Commands.Command(commands, self.commands)
+                        try:
+                            while Command.RunNext():
+                                pass
+                        except Exception:
+                            s.close()
+                            print("PERFORMING CLIENT RESET")
+                            os._exit(1)
+                        print("JOBDONE")
+                        s.sendall(b"<DONE_JOB>")
+                        if s.recv(1024).decode() != "<GET_OUTPUT>":
+                            raise Exception("RECEIVED INCORRECT RESPONSE")
+                        for command in Command.stdout:
+                            s.sendall(command.encode('utf-8'))
+                            s.sendall(b"<EOSTDO>")
+                            msg = s.recv(1024).decode()
+                            if msg != "<NEXT_OUTPUT>":
+                                raise Exception("RECEIVED INCORRECT RESPONSE")
+                        s.sendall(b"<OUTPUT_DONE>")
+                        commands = []
+                        self.isbusy = False
+                    elif command != "<NEXT_OUTPUT>":
+                        s.sendall(b"<COMMAND_RECIEVED>")
+                        commands.append(command)
+            except KeyboardInterrupt:
+                os._exit(1)
+            except ConnectionResetError:
+                print("CONNECTION CLOSED")
+                os._exit(1)
---
+++ packages/STalk/stalk-0.0.3.tar.gz/stalk-0.0.3/src/Talk/Objects/Queue.py
@@ -0,0 +1,42 @@
+import threading
+
+class CircularQueue:
+    def __init__(self, size):
+        self.size = size
+        self.count = 0
+        self.head = 0
+        self.tail = 0
+        self.queue = [None for _ in range(size)]
+        self.queuelock = threading.Lock()
+        self.current = self.head
+    def __iter__(self):
+        return self
+    def __next__(self):
+        with self.queuelock:
+            if self.current < self.tail:
+                self.current += 1
+                return self.queue[self.current-1]
+            else:
+                self.current = self.head
+                raise StopIteration
+    def EnQueue(self, item):
+        if self.count == self.size:
+            return False
+        with self.queuelock:
+            self.queue[self.tail] = item
+            self.tail += 1
+            self.tail = self.tail % self.size
+            self.count += 1
+        return True
+    def DeQueue(self):
+        if self.count == 0:
+            return True
+        returnval = None
+        with self.queuelock:
+            returnval = self.queue[self.head]
+            self.head += 1
+            self.head = self.head % self.size
+            self.current = self.head
+            self.count -= 1
+        return returnval
+            
