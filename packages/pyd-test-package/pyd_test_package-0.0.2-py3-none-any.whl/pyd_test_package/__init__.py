896e98de859b1a3db56a6bf14c42162fb260d8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:02:59 +0000

    Add package pyd-test-package/0.0.2/pyd_test_package-0.0.2-py3-none-any.whl

---
+++ packages/pyd-test-package/pyd_test_package-0.0.2-py3-none-any.whl/pyd_test_package/__init__.py
@@ -0,0 +1,8 @@
+# Designer:Yudong Pan
+# Coder:God's hand
+# Time:2024/6/8 21:28
+
+from .hello_world import print_hello
+from .clc_component import add_num, sum_array
+
+__all__ = ['add_num', 'sum_array', 'print_hello']
---
+++ packages/pyd-test-package/pyd_test_package-0.0.2-py3-none-any.whl/pyd_test_package/hello_world.py
@@ -0,0 +1,6 @@
+# Designer:Yudong Pan
+# Coder:God's hand
+# Time:2024/6/8 21:28
+
+def print_hello():
+    print("hello world!")
