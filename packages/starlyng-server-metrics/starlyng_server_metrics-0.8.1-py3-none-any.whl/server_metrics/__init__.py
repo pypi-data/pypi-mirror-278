d290c40d45cbfc7d015bc750a5a93d748dbf44d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:17 +0000

    Add package starlyng-server-metrics/0.8.1/starlyng_server_metrics-0.8.1-py3-none-any.whl

---
+++ packages/starlyng-server-metrics/starlyng_server_metrics-0.8.1-py3-none-any.whl/server_metrics/__init__.py
@@ -0,0 +1,25 @@
+"""
+__main__.py
+"""
+
+from server_metrics.utils.configuration import load_configuration
+from server_metrics.utils.file import clear_directory, create_directory
+from server_metrics.utils.execute import execute_commands_on_servers
+
+def main():
+    """
+    Runs the metric collectors for each listed server
+    """
+    directory = 'data/prom'
+
+    # Clear the data/prom directory
+    clear_directory(directory)
+
+    # Create the data/prom directory
+    create_directory(directory)
+
+    # Load configuration
+    servers, key_path, username, combined_metrics_dir = load_configuration()
+
+    # Execute ssh commands on servers
+    execute_commands_on_servers(servers, key_path, username, directory, combined_metrics_dir)
---
+++ packages/starlyng-server-metrics/starlyng_server_metrics-0.8.1-py3-none-any.whl/server_metrics/collectors/sensors_collector.py
@@ -0,0 +1,34 @@
+"""
+sensors_collector.py
+"""
+import json
+import logging
+import subprocess
+from server_metrics.utils.prom.sensors import create_sensors_prom_file
+from server_metrics.utils.models import Server
+from server_metrics.utils.ssh import build_ssh_command
+
+def run_sensors(server: Server, prom_directory: str):
+    """
+    Gets sensors data from servers using sensors
+
+    Args:
+        ip (str):
+        port (int):
+        key_path (str):
+        username (str):
+    """
+    try:
+        # Command to connect via SSH and run sensors
+        ssh_command = "sensors -j"
+        result = subprocess.run(build_ssh_command(server, ssh_command), capture_output=True, text=True, check=False)
+
+        if result.stdout:
+            json_data = json.loads(result.stdout)
+            create_sensors_prom_file(server.hostname, prom_directory, json_data)
+
+        elif result.stderr:
+            logging.error("Errors from %s:%s\n%s", server.ip, server.port, result.stderr)
+
+    except subprocess.CalledProcessError as e:
+        logging.error("Failed to execute on %s:%s: %s", server.ip, server.port, e)
