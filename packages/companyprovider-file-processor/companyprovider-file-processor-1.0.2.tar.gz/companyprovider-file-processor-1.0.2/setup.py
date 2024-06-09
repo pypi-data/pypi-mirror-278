1875841f82c626b830a09bf8315668431ce636f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:40 +0000

    Add package companyprovider-file-processor/1.0.2/companyprovider-file-processor-1.0.2.tar.gz

---
+++ packages/companyprovider-file-processor/companyprovider-file-processor-1.0.2.tar.gz/companyprovider-file-processor-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+	name='companyprovider-file-processor',
+	version='1.0.2',
+	author='Marcelo Lacroix',
+	author_email='mlacroix@tdsoft.com.br',
+	description='',
+	packages=find_packages(),
+	entry_points={
+        'console_scripts': ['companyprovider-file-processor=processor:main'],
+    },
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+	python_requires='>=3.6',
+	long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
