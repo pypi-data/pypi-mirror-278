1822c0fca4bfa254274ebf18aca3987e5265f5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:58 +0000

    Add package pcap-blur/0.1.1/pcap_blur-0.1.1.tar.gz

---
+++ packages/pcap-blur/pcap_blur-0.1.1.tar.gz/pcap_blur-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="pcap_blur",
+    version="0.1.1",
+    description="Pcap Blur is a command line tool that anonymizes network traffic.",
+    long_description=open("README.md").read(),
+    url="https://github.com/rafaelsilva81/pcap-blur",
+    author="Rafael Galdino da Silva",
+    author_email="rafaelgaldinosilva81@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pcap-blur=pcap_blur.main:main",
+            "pcap_blur=pcap_blur.main:main",
+            "pcapblur=pcap_blur.main:main",
+        ],
+    },
+    install_requires=requirements,
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: System :: Networking",
+        "Topic :: Utilities",
+    ],
+)
