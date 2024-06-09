307a86b7980721c86ddffaf111d9b3bec4551bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:29 +0000

    Add package ellar/0.7.7/ellar-0.7.7-py3-none-any.whl

---
+++ packages/ellar/ellar-0.7.7-py3-none-any.whl/ellar/core/middleware/middleware.py
@@ -0,0 +1,26 @@
+import typing as t
+
+from ellar.common.interfaces import IEllarMiddleware
+from ellar.common.types import ASGIApp
+from ellar.di import EllarInjector, injectable
+from ellar.utils import build_init_kwargs
+from starlette.middleware import Middleware
+
+T = t.TypeVar("T")
+
+
+class EllarMiddleware(Middleware, IEllarMiddleware):
+    @t.no_type_check
+    def __init__(self, cls: t.Type[T], **options: t.Any) -> None:
+        super().__init__(cls, **options)
+        injectable()(self.cls)
+        self.kwargs = build_init_kwargs(self.cls, self.kwargs)
+
+    @t.no_type_check
+    def __call__(self, app: ASGIApp, injector: EllarInjector) -> T:
+        self.kwargs.update(app=app)
+        try:
+            return injector.create_object(self.cls, additional_kwargs=self.kwargs)
+        except TypeError:  # pragma: no cover
+            # TODO: Fix future typing for lower python version.
+            return self.cls(**self.kwargs)
