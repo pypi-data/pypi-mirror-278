655a181d0965874387c69a1761b6e24342d5cffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 15:02:56 +0000

    Add package holded-dataflow/0.1.6/holded_dataflow-0.1.6.tar.gz

---
+++ packages/holded-dataflow/holded_dataflow-0.1.6.tar.gz/holded_dataflow-0.1.6/src/lib.rs
@@ -0,0 +1,15 @@
+use pyo3::prelude::*;
+mod blacklist;
+mod filter;
+mod parse;
+
+use filter::is_blacklisted;
+use parse::{parse_attributes, parse_binary_json};
+
+#[pymodule]
+fn holded_dataflow(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(is_blacklisted, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_attributes, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_binary_json, m)?)?;
+    Ok(())
+}
