7d06f2bb8652a1e3bd1bccffb90da383031c748d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:13 +0000

    Add package pypomes-db/1.0.0/pypomes_db-1.0.0-py3-none-any.whl

---
+++ packages/pypomes-db/pypomes_db-1.0.0-py3-none-any.whl/pypomes_db/__init__.py
@@ -0,0 +1,25 @@
+from .db_pomes import (
+    db_setup, db_get_engines, db_get_param, db_get_params,
+    db_get_connection_string, db_assert_connection, db_connect,
+    db_exists, db_select, db_insert, db_update, db_delete,
+    db_bulk_insert, db_bulk_update, db_update_lob,
+    db_execute, db_call_function, db_call_procedure,
+)
+from .migration_pomes import (
+    db_migrate_data, db_migrate_lobs,
+)
+
+__all__ = [
+    # db_pomes
+    "db_setup", "db_get_engines", "db_get_param", "db_get_params",
+    "db_get_connection_string", "db_assert_connection", "db_connect",
+    "db_exists", "db_select", "db_insert", "db_update", "db_delete",
+    "db_bulk_insert", "db_bulk_update", "db_update_lob",
+    "db_execute", "db_call_function", "db_call_procedure",
+    # migration_pomes
+    "db_migrate_data", "db_migrate_lobs",
+]
+
+from importlib.metadata import version
+__version__ = version("pypomes_db")
+__version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
