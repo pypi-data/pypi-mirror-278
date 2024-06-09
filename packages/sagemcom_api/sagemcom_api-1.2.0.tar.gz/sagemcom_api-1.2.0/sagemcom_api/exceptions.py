3ea584e0a0105695ffa5fffaf348339a2180b6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:04:03 +0000

    Add package sagemcom_api/1.2.0/sagemcom_api-1.2.0.tar.gz

---
+++ packages/sagemcom_api/sagemcom_api-1.2.0.tar.gz/sagemcom_api-1.2.0/sagemcom_api/exceptions.py
@@ -0,0 +1,39 @@
+"""Exceptions for the Sagemcom F@st client."""
+
+
+# Exceptions provided by SagemCom API
+class AccessRestrictionException(Exception):
+    """Raised when current user has access restrictions."""
+
+
+class AuthenticationException(Exception):
+    """Raised when authentication is not correct."""
+
+
+class LoginTimeoutException(Exception):
+    """Raised when a timeout is encountered during login."""
+
+
+class NonWritableParameterException(Exception):
+    """Raised when provided parameter is not writable."""
+
+
+class UnknownPathException(Exception):
+    """Raised when provided path does not exist."""
+
+
+class MaximumSessionCountException(Exception):
+    """Raised when the maximum session count is reached."""
+
+
+# Broad exceptions provided by this library
+class BadRequestException(Exception):
+    """TODO."""
+
+
+class UnauthorizedException(Exception):
+    """TODO."""
+
+
+class UnknownException(Exception):
+    """TODO."""
