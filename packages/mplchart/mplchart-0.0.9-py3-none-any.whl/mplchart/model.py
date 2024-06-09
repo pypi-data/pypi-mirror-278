276c86171bd1212a8881c05b21d802ebcabb05f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:04:03 +0000

    Add package mplchart/0.0.9/mplchart-0.0.9-py3-none-any.whl

---
+++ packages/mplchart/mplchart-0.0.9-py3-none-any.whl/mplchart/model.py
@@ -0,0 +1,36 @@
+""" primitive base class """
+
+from abc import ABC, abstractmethod
+
+
+class Primitive(ABC):
+    """Primitive abstract base class"""
+
+    @abstractmethod
+    def plot_handler(self, data, chart, ax=None):
+        """Plot handler is called before any callculation"""
+        ...
+
+    def clone(self, **kwargs):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.__dict__.update(self.__dict__, **kwargs)
+        return result
+
+
+class Wrapper(ABC):
+    """Indicator plotting wrapper"""
+
+    def __init__(self, indicator):
+        self.indicator = indicator
+
+    def __repr__(self, *args, **kwargs):
+        return getattr(self.indicator, "__name__", repr(self.indicator))
+
+    def check_result(self, data):
+        return True
+
+    @abstractmethod
+    def plot_result(self, data, chart, ax=None):
+        ...
+
