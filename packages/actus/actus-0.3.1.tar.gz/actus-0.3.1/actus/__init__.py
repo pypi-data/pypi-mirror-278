8b493e626c8ab9292d6117a4e098b5135856d872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:50 +0000

    Add package actus/0.3.1/actus-0.3.1.tar.gz

---
+++ packages/actus/actus-0.3.1.tar.gz/actus-0.3.1/actus/__init__.py
@@ -0,0 +1,34 @@
+"""
+Actus
+-----
+
+A logging library for python development
+
+Includes:
+- `info`
+- `warn`
+- `error`
+- `LogSection`
+- `Theme`
+- `set_theme`
+- `get_theme`
+"""
+
+__version__ = "0.3.1"
+__all__ = [
+    "info",
+    "warn",
+    "error",
+    "LogSection",
+    "Theme",
+    "set_theme",
+    "get_theme"
+]
+
+from ._log_section import LogSection
+from ._theme import Theme, set_theme, get_theme
+
+
+info = LogSection("Info", theme_key="info")
+warn = LogSection("Warning", theme_key="warn")
+error = LogSection("Error", theme_key="error")
