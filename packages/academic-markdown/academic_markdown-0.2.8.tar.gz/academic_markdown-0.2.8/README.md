52ef837e0ceb5a5f767a124680fddbedbf16a6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:03:17 +0000

    Add package academic-markdown/0.2.8/academic_markdown-0.2.8.tar.gz

---
+++ packages/academic-markdown/academic_markdown-0.2.8.tar.gz/academic_markdown-0.2.8/README.md
@@ -0,0 +1,6 @@
+# Academic Markdown CLI
+
+This is the command line tool used in the
+[academic_markdown](https://github.com/cochaviz/academic_markdown) project.
+While there should be a discrete README for this project, for now I would like
+to ask anyone to move there for more information on the project.
---
+++ packages/academic-markdown/academic_markdown-0.2.8.tar.gz/academic_markdown-0.2.8/src/scripts/build_docker.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# For more information:
+# https://docs.docker.com/engine/reference/commandline/build/
+#
+# academic_markdown.py will look for a docker image with the name
+# cochaviz/academic_markdown which is why this was provided 
+# specifically. At some point I will probably add it to a container
+# registry and you won't need to run this anymore.
+#
+
+PREAMBLE="academic_markdown (build_docker.sh):"
+
+# if container is installed, its fine
+if [[ $(docker image ls | grep cochaviz/academic_markdown) ]]; then
+    # unless someone really wants to build
+    if [[ ! $# || ! $1 = "-f" ]]; then
+        echo "$PREAMBLE Found docker image (cochaviz/academic_markdown), skipping build..."
+        exit 0
+    fi
+    # remove -f option (the rest are build commands)
+    shift
+fi
+
+# if there are no build commands
+if [[ $# ]]; then
+    echo "$PREAMBLE Attempting to pull from registry"
+    docker pull ghcr.io/cochaviz/academic_markdown:main
+fi
+
+# if there are explit build commands or the previous failed
+if [[ $?  || ! $# ]]; then
+    echo "$PREAMBLE Pulling unuccessful, or explicit build arguments given, building locally"
+    docker build -t zoharcochavi/academic-markdown $@ .devcontainer/
+fi
+
+exit $?
