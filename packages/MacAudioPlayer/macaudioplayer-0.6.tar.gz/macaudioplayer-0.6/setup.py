bc22154468a77ac12703074730fa9117b3893112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:53 +0000

    Add package MacAudioPlayer/0.6/macaudioplayer-0.6.tar.gz

---
+++ packages/MacAudioPlayer/macaudioplayer-0.6.tar.gz/macaudioplayer-0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MacAudioPlayer',
+    version='0.6',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'play=my_audio_player.player:play',
+        ],
+    },
+)
