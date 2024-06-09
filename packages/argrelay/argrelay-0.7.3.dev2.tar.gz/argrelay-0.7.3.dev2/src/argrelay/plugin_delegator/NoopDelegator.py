204fa013a2c1b316b4cc3ff149061d767665aaab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 15:03:01 +0000

    Add package argrelay/0.7.3.dev2/argrelay-0.7.3.dev2.tar.gz

---
+++ packages/argrelay/argrelay-0.7.3.dev2.tar.gz/argrelay-0.7.3.dev2/src/argrelay/plugin_delegator/NoopDelegator.py
@@ -0,0 +1,26 @@
+from argrelay.plugin_delegator.AbstractDelegator import AbstractDelegator
+from argrelay.relay_server.LocalServer import LocalServer
+from argrelay.runtime_context.InterpContext import InterpContext
+from argrelay.schema_response.InvocationInput import InvocationInput
+
+
+class NoopDelegator(AbstractDelegator):
+
+    def run_invoke_control(
+        self,
+        interp_ctx: InterpContext,
+        local_server: LocalServer,
+    ) -> InvocationInput:
+        invocation_input = InvocationInput.with_interp_context(
+            interp_ctx,
+            delegator_plugin_entry = local_server.plugin_config.plugin_instance_entries[
+                self.plugin_instance_id
+            ],
+            custom_plugin_data = {},
+        )
+        return invocation_input
+
+    @staticmethod
+    def invoke_action(invocation_input: InvocationInput):
+        # Do nothing:
+        pass
