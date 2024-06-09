8b3ff758dfbcec07aadf97b2be59ec3d05df9d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:08 +0000

    Add package python-printer-command-line/1.0.0/python_printer_command_line-1.0.0-py3-none-any.whl

---
+++ packages/python-printer-command-line/python_printer_command_line-1.0.0-py3-none-any.whl/pypcl/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# import everything from pypcl.py
+from .pypcl import *
+from .toolbox import *
+from .hppcl import HpPclDocument
+from .hpcode39 import Barcode39
+from .zpl import ZplDocument
+from .adserial import PrinterSerialAdapter
+from .adsocket import PrinterSocketAdapter
+from .adcups import PrinterCupsAdapter
+from .exceptions import *
+from .adusb import PrinterUsbAdapter
