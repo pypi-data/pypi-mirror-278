a48fd17e23fef3dac66b5c6ca0fea36b5fa6c0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:03:05 +0000

    Add package python-115/0.0.9.3.4/python_115-0.0.9.3.4.tar.gz

---
+++ packages/python-115/python_115-0.0.9.3.4.tar.gz/python_115-0.0.9.3.4/p115/cmd/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+__all__ = ["parser", "subparsers"]
+
+from .init import parser, subparsers
+
+from .iterdir import *
+from .qrcode import *
+from .check import *
+from .download import *
