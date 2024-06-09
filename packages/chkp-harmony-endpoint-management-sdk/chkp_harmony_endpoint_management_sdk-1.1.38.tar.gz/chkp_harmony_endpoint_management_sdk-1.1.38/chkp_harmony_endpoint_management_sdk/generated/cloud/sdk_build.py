dc5d752102d8ea9f290ef05ca31a44203da34a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 15:02:58 +0000

    Add package chkp-harmony-endpoint-management-sdk/1.1.38/chkp_harmony_endpoint_management_sdk-1.1.38.tar.gz

---
+++ packages/chkp-harmony-endpoint-management-sdk/chkp_harmony_endpoint_management_sdk-1.1.38.tar.gz/chkp_harmony_endpoint_management_sdk-1.1.38/chkp_harmony_endpoint_management_sdk/generated/cloud/sdk_build.py
@@ -0,0 +1,5 @@
+
+from chkp_harmony_endpoint_management_sdk.classes.harmony_endpoint_sdk_info import HarmonyEndpointSDKInfo
+
+def sdk_build_info() -> HarmonyEndpointSDKInfo:
+    return HarmonyEndpointSDKInfo(sdk_build="9435737705",sdk_version="1.1.38",spec="web-mgmt-external-api-production",spec_version="1.9.212",released_on="2024-06-09T10:07:06.796444")
---
+++ packages/chkp-harmony-endpoint-management-sdk/chkp_harmony_endpoint_management_sdk-1.1.38.tar.gz/chkp_harmony_endpoint_management_sdk-1.1.38/chkp_harmony_endpoint_management_sdk/generated/saas/sdk_build.py
@@ -0,0 +1,5 @@
+
+from chkp_harmony_endpoint_management_sdk.classes.harmony_endpoint_sdk_info import HarmonyEndpointSDKInfo
+
+def sdk_build_info() -> HarmonyEndpointSDKInfo:
+    return HarmonyEndpointSDKInfo(sdk_build="9435737705",sdk_version="1.1.38",spec="harmony-endpoint-cloud-api-prod",spec_version="1.0.626",released_on="2024-06-09T10:07:06.797166")
