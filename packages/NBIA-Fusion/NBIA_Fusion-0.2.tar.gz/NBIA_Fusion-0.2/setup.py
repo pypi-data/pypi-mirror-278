e8eaba19111076ea7991347f7180facc5bfb83b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:56 +0000

    Add package NBIA-Fusion/0.2/NBIA_Fusion-0.2.tar.gz

---
+++ packages/NBIA-Fusion/NBIA_Fusion-0.2.tar.gz/NBIA_Fusion-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="NBIA_Fusion",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        # "numpy",
+        # "torch",
+        # "open3d",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
