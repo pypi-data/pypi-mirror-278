b8d7befd1891551b8705dcf9a91ce1883ad5c177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:16 +0000

    Add package mountaineer/0.5.0/mountaineer-0.5.0.tar.gz

---
+++ packages/mountaineer/mountaineer-0.5.0.tar.gz/mountaineer-0.5.0/ci_webapp/ci_webapp/cli.py
@@ -0,0 +1,29 @@
+from click import command
+from mountaineer.cli import handle_build, handle_runserver, handle_watch
+
+
+@command()
+def runserver():
+    handle_runserver(
+        package="ci_webapp",
+        webservice="ci_webapp.main:app",
+        webcontroller="ci_webapp.app:controller",
+        port=5006,
+        subscribe_to_mountaineer=True,
+    )
+
+
+@command()
+def watch():
+    handle_watch(
+        package="ci_webapp",
+        webcontroller="ci_webapp.app:controller",
+        subscribe_to_mountaineer=True,
+    )
+
+
+@command()
+def build():
+    handle_build(
+        webcontroller="ci_webapp.app:controller",
+    )
