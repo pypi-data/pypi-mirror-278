71b8973c0ccd4cb8c10b15d971bc8a3151e5aac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:30 +0000

    Add package dfss/1.7.3/dfss-1.7.3.tar.gz

---
+++ packages/dfss/dfss-1.7.3.tar.gz/dfss-1.7.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dfss',
+    version='1.7.3',
+    author='zetao.zhang',
+    author_email='zetao.zhang@sophgo.com',
+    description='download_from_sophon_sftp',
+    packages=find_packages(),
+	package_data={"dfss":["output/*"]},
+    include_package_data=True,
+    python_requires='>=3.5',
+)
