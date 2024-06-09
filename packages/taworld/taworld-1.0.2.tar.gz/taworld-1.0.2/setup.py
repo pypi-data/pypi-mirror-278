24c6dec55ea03117dd08983fb3f8e6572ce242a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:02:55 +0000

    Add package taworld/1.0.2/taworld-1.0.2.tar.gz

---
+++ packages/taworld/taworld-1.0.2.tar.gz/taworld-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'taworld',
+  version = '1.0.2',
+  description = 'TECHNICAL ANALYSIS WORLD',
+  url = 'https://github.com/pythonsystem/taworld',
+  author = 'Original By Kevin Johnson',
+  license = 'MIT',
+  long_description = open('README.txt', 'r').read(),
+  long_description_content_type = 'text/plain',
+  packages = find_packages(include = ['taworld*']),
+  install_requires = open('requirements.txt', 'r').read().split('\n'),
+  setup_requires = [],
+  tests_require = [],
+)
