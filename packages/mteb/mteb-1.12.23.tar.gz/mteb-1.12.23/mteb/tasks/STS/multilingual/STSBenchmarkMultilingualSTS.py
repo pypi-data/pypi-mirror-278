e460f014712dd89ff620b53c79caf9b8a3d88b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:03:15 +0000

    Add package mteb/1.12.23/mteb-1.12.23.tar.gz

---
+++ packages/mteb/mteb-1.12.23.tar.gz/mteb-1.12.23/mteb/tasks/STS/multilingual/STSBenchmarkMultilingualSTS.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks import AbsTaskSTS, MultilingualTask
+
+_LANGUAGES = {
+    "en": ["eng-Latn"],
+    "de": ["deu-Latn"],
+    "es": ["spa-Latn"],
+    "fr": ["fra-Latn"],
+    "it": ["ita-Latn"],
+    "nl": ["nld-Latn"],
+    "pl": ["pol-Latn"],
+    "pt": ["por-Latn"],
+    "ru": ["rus-Cyrl"],
+    "zh": ["cmn-Hans"],
+}
+
+_SPLITS = ["dev", "test"]
+
+
+class STSBenchmarkMultilingualSTS(AbsTaskSTS, MultilingualTask):
+    fast_loading = True
+    metadata = TaskMetadata(
+        name="STSBenchmarkMultilingualSTS",
+        dataset={
+            "path": "mteb/stsb_multi_mt",
+            "revision": "29afa2569dcedaaa2fe6a3dcfebab33d28b82e8c",
+        },
+        description=(
+            "Semantic Textual Similarity Benchmark (STSbenchmark) dataset,"
+            "but translated using DeepL API."
+        ),
+        reference="https://github.com/PhilipMay/stsb-multi-mt/",
+        type="STS",
+        category="s2s",
+        eval_splits=_SPLITS,
+        eval_langs=_LANGUAGES,
+        main_score="cosine_spearman",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation=None,
+        n_samples=None,
+        avg_character_length=None,
+    )
+
+    @property
+    def metadata_dict(self) -> dict[str, str]:
+        metadata_dict = super().metadata_dict
+        metadata_dict["min_score"] = 0
+        metadata_dict["max_score"] = 5
+        return metadata_dict
+
+    def dataset_transform(self) -> None:
+        for lang, subset in self.dataset.items():
+            self.dataset[lang] = subset.rename_column("similarity_score", "score")
