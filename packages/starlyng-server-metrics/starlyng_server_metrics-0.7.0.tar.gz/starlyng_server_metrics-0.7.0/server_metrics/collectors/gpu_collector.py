86c59f59a5059c99b8157084f0d9ae2a5d3c13b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:21 +0000

    Add package starlyng-server-metrics/0.7.0/starlyng_server_metrics-0.7.0.tar.gz

---
+++ packages/starlyng-server-metrics/starlyng_server_metrics-0.7.0.tar.gz/starlyng_server_metrics-0.7.0/server_metrics/collectors/gpu_collector.py
@@ -0,0 +1,36 @@
+"""
+gpu_collector.py
+"""
+import json
+import logging
+import subprocess
+from server_metrics.utils.prom.gpu import create_gpu_prom_file
+from server_metrics.utils.models import Server
+from server_metrics.utils.ssh import build_ssh_command
+from server_metrics.utils.xml_parser import xml_to_json
+
+def run_nvidia_smi(server: Server, prom_directory: str):
+    """
+    Gets GPU data from servers using nvidia-smi
+
+    Args:
+        ip (str):
+        port (int):
+        key_path (str):
+        username (str):
+    """
+    try:
+        # Command to connect via SSH and run nvidia-smi
+        ssh_command = "nvidia-smi -q -x"
+        result = subprocess.run(build_ssh_command(server, ssh_command), capture_output=True, text=True, check=False)
+
+        if result.stdout:
+            json_output = json.dumps(xml_to_json(result.stdout), indent=4)
+            json_data = json.loads(json_output)
+            create_gpu_prom_file(server.hostname, prom_directory, json_data)
+
+        elif result.stderr:
+            logging.error("Errors from %s:%s\n%s", server.ip, server.port, result.stderr)
+
+    except subprocess.CalledProcessError as e:
+        logging.error("Failed to execute on %s:%s: %s", server.ip, server.port, e)
