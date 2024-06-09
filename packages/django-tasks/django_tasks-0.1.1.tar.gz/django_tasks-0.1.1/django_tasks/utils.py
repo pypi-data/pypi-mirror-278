972a0b45478cd617ca3109d393b61c4ac88b7090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:03:05 +0000

    Add package django-tasks/0.1.1/django_tasks-0.1.1.tar.gz

---
+++ packages/django-tasks/django_tasks-0.1.1.tar.gz/django_tasks-0.1.1/django_tasks/utils.py
@@ -0,0 +1,33 @@
+import inspect
+import json
+from collections import deque
+from typing import Any, Callable
+
+
+def is_global_function(func: Callable) -> bool:
+    if not inspect.isfunction(func) or inspect.isbuiltin(func):
+        return False
+
+    if "<locals>" in func.__qualname__:
+        return False
+
+    return True
+
+
+def is_json_serializable(obj: Any) -> bool:
+    """
+    Determine, as efficiently as possible, whether an object is JSON-serializable.
+    """
+    try:
+        # HACK: JSON-encode an object, without loading it all into memory
+        deque(json.JSONEncoder().iterencode(obj), maxlen=0)
+        return True
+    except (TypeError, OverflowError):
+        return False
+
+
+def json_normalize(obj: Any) -> Any:
+    """
+    Round-trip encode object as JSON to normalize types.
+    """
+    return json.loads(json.dumps(obj))
