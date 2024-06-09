9d3666ca70ce44e7a63c27b059590070fa06156a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:56 +0000

    Add package TorLog/0.0.5/torlog-0.0.5.tar.gz

---
+++ packages/TorLog/torlog-0.0.5.tar.gz/torlog-0.0.5/torlog_package/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TorLog',
+    version='0.0.5',
+    author='tore.ofc',
+    description='Simple logging tool for Python | tore.ofc on discord.',
+    url='https://github.com/Tore09/torlog',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+)
