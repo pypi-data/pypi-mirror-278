2af294735f6017617c763cf0d295955bb57559ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:04:17 +0000

    Add package osy/0.2.4/osy-0.2.4.tar.gz

---
+++ packages/osy/osy-0.2.4.tar.gz/osy-0.2.4/osy/matsh.py
@@ -0,0 +1,28 @@
+import csv
+import pkg_resources
+
+
+def sol(text):
+    data_path = pkg_resources.resource_filename('osy', 'data/oneliner.csv')
+    with open(data_path, 'r', encoding='utf-8', errors='ignore') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 2:
+                task, answer = row
+                if text in task:
+                    print(task, "\n----\n", answer)
+                    return 0
+    print(text, "\n----\n", "Ответ не найден")
+
+
+def tor(text):
+    data_path = pkg_resources.resource_filename('osy', 'data/onelinert.csv')
+    with open(data_path, 'r', encoding='utf-8', errors='ignore') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 2:
+                task, answer = row
+                if text in task:
+                    print(task, "\n----\n", answer.replace(".", ".\n"))
+                    return 0
+    print(text, "\n----\n", "Ответ не найден")
