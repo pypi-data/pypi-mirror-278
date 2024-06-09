0471c6b84eddb4839fc6e71a62d04b4ec69272f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:04:20 +0000

    Add package scipy-doctest/1.2.0/scipy_doctest-1.2.0-py3-none-any.whl

---
+++ packages/scipy-doctest/scipy_doctest-1.2.0-py3-none-any.whl/scipy_doctest/__init__.py
@@ -0,0 +1,12 @@
+"""
+Doctests on steroids.
+
+Whitespace-insensitive, numpy-aware, floating-point-aware doctest helpers.
+"""
+
+
+__version__ = "1.2.0"
+
+from .impl import DTChecker, DTFinder, DTParser, DTRunner, DebugDTRunner, DTConfig  # noqa
+from .frontend import testmod, testfile, find_doctests, run_docstring_examples      # noqa
+
