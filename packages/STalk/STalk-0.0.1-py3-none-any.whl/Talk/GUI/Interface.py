75b933c8cf63e7bcb915333aa1738ced16b3c6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:03:00 +0000

    Add package STalk/0.0.1/STalk-0.0.1-py3-none-any.whl

---
+++ packages/STalk/STalk-0.0.1-py3-none-any.whl/Talk/GUI/Interface.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import os
+from ..NetObject import Server
+
+cServer = None
+
+def SubmitCommands(commands, server):
+    temp = commands.get("1.0","end").split("\n")
+    commands = []
+    for value in temp:
+        if value != "":
+            commands.append(value)
+    with server.commandlock:
+        server.commands.append(commands)
+        print("COMMAND SUBMITTED")
+
+def Update(server):
+    server.update = True
+
+def ShutdownServer(server, serverthread):
+    print("SHUTTING DOWN")
+    os._exit(1)
+
+def StartServer(root, button):
+    global cServer
+    cServer = Server.Server("10.101.1.59", 4245, root)
+    serverthread = cServer.StartServer()
+    commands = tk.Text(root, height = 5, width = 52)
+    update = tk.Button(root, width=20, height=2, text='Update', command=lambda: Update(cServer))
+    submit = tk.Button(root, width=20, height=2, text='Submit', command=lambda: SubmitCommands(commands, cServer))
+    shutdown = tk.Button(root, width=20, height=2, text='Shutdown', command=lambda: ShutdownServer(cServer, serverthread))
+    button.pack_forget()
+    commands.pack()
+    update.pack()
+    submit.pack()
+    shutdown.pack()
+    
+def main():
+    root = tk.Tk()
+    button = tk.Button(root, text='Start Server', width=40, height=4, command=lambda: StartServer(root, button))
+    button.pack_forget()
+    button.pack()
+    root.mainloop()
