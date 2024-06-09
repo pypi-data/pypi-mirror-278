42832c7d27c6b6da61b02b828b2c74cc4ba0707e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:03:07 +0000

    Add package smart-function/0.0.1/smart-function-0.0.1.tar.gz

---
+++ packages/smart-function/smart-function-0.0.1.tar.gz/smart-function-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smart-function',
+    version='0.0.1',
+    url='https://github.com/username/my_package',  # Replace with your URL
+    author='Author Name',
+    author_email='dreuter14@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=[],
+)
