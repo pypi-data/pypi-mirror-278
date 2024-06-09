08335bfc5c3ede6b53827729adae369396022104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:03:54 +0000

    Add package psqlpy/0.6.2/psqlpy-0.6.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/psqlpy/psqlpy-0.6.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/psqlpy/extra_types.py
@@ -0,0 +1,29 @@
+from ._internal.extra_types import (
+    BigInt,
+    Float32,
+    Float64,
+    Integer,
+    PyCustomType,
+    PyJSON,
+    PyJSONB,
+    PyMacAddr6,
+    PyMacAddr8,
+    PyText,
+    PyVarChar,
+    SmallInt,
+)
+
+__all__ = [
+    "SmallInt",
+    "Integer",
+    "BigInt",
+    "PyJSONB",
+    "PyJSON",
+    "PyMacAddr6",
+    "PyMacAddr8",
+    "PyVarChar",
+    "PyText",
+    "PyCustomType",
+    "Float32",
+    "Float64",
+]
