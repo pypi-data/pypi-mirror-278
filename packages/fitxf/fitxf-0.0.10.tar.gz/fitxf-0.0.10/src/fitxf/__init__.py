39585728dda512e4966b21cc4210ce339442d631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 15:02:55 +0000

    Add package fitxf/0.0.10/fitxf-0.0.10.tar.gz

---
+++ packages/fitxf/fitxf-0.0.10.tar.gz/fitxf-0.0.10/src/fitxf/__init__.py
@@ -0,0 +1,9 @@
+# Fit
+from nwae.math.fit.cluster.ClusterUnitTest import Cluster, ClusterUnitTest
+from nwae.math.fit.cluster.ClusterCosineUT import ClusterCosine, ClusterCosineUnitTest
+from nwae.math.fit.transform.FitXformUnitTest import FitXformPca, FitXformUnitTest
+from nwae.math.fit.utils.FitUtilsUnitTest import FitUtils, FitUtilsUt
+from nwae.math.fit.utils.TensorUtils import TensorUtils, TensorUtilsUnitTest
+
+# Algo
+from nwae.math.algo.encoding.Base64 import Base64
