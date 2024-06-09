a3dea1e127a2eac530857b25852c9768b42e5d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:02:58 +0000

    Add package 15Watt-Wsgi/0.9.3/15watt_wsgi-0.9.3.tar.gz

---
+++ packages/15Watt-Wsgi/15watt_wsgi-0.9.3.tar.gz/15watt_wsgi-0.9.3/src/15Watt_Wsgi/BaseController.py
@@ -0,0 +1,31 @@
+from .Request import Request
+from .Response import Response
+from .Exceptions import Unauthorized
+
+
+def decoratorLoginRequired(func):
+	"""
+		A decorator for controller methods, that checks if the user has logged in via BasicAuth.
+
+		If not so, a 401 Unauthorized exception is raised.
+	"""
+	def wrapper(self, request: Request, response: Response):
+		if 'Basic' != request.getEnvByKey('AUTH_TYPE'):
+			raise Unauthorized(
+				returnCode=401,
+				returnMsg=f"Invalid auth type: {request.getEnvByKey('AUTH_TYPE')}"
+			)
+
+		if request.getEnvByKey('REMOTE_USER') is None:
+			raise Unauthorized(returnCode=401, returnMsg='Unauthorized')
+
+		return func(self, request, response)
+	return wrapper
+
+
+class BaseController(object):
+	"""
+		Basisklasse of all controllers.
+	"""
+	def __init__(self, config: dict):
+		self._config = config
