2d99da5504576c3b86a02ba5e29f64c206ec84eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:03:01 +0000

    Add package Tami4EdgeAPI/3.0/tami4edgeapi-3.0.tar.gz

---
+++ packages/Tami4EdgeAPI/tami4edgeapi-3.0.tar.gz/tami4edgeapi-3.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="Tami4EdgeAPI",
+    version="3.0",
+    author="Guy Shefer",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Guy293/Tami4EdgeAPI",
+    packages=setuptools.find_packages(),
+    install_requires=["requests", "pypasser", "pyjwt"],
+)
