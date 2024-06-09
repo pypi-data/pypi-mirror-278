74ae269fb8de96f3dc09f615e65b74e93938627e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 04:04:20 +0000

    Add package metalgpu/0.5.27/metalgpu-0.5.27.tar.gz

---
+++ packages/metalgpu/metalgpu-0.5.27.tar.gz/metalgpu-0.5.27/src/metalgpu/test.py
@@ -0,0 +1,34 @@
+from interface import Interface
+from operators import sqrt
+
+instance = Interface()  # Initialise the metal instance
+shader_string = """
+#include <metal_stdlib>
+
+using namespace metal;
+
+kernel void adder(device int *arr1 [[buffer(0)]],
+        device int *arr2 [[buffer(1)]],
+        device int *arr3 [[buffer(2)]],
+        uint id [[thread_position_in_grid]]) {
+    arr3[id] = arr2[id] + arr1[id];
+}
+"""
+# Note: For clearer code, use instance.load_shader(shaderPath) to load a metal file
+
+instance.load_shader_from_string(shader_string)
+instance.set_function("adder")
+
+buffer_size = 10  # Number of items in the buffer
+buffer_type = "int"
+
+initial_array = [i for i in range(buffer_size)]
+
+buffer1 = instance.array_to_buffer(initial_array).as_float()
+
+buffer2 = instance.array_to_buffer(initial_array)
+
+buffer3 = sqrt(buffer1)
+
+
+
