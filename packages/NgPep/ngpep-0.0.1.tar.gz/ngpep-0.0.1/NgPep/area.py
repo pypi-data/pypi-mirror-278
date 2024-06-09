80ce0fdf6ede42ba9873c514e0802ff32df89fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:02:58 +0000

    Add package NgPep/0.0.1/ngpep-0.0.1.tar.gz

---
+++ packages/NgPep/ngpep-0.0.1.tar.gz/ngpep-0.0.1/NgPep/area.py
@@ -0,0 +1,8 @@
+def areaOfSquare(length):
+    return length * length
+
+def areaOfTriangle(base, height):
+    return 0.5 * base * height
+
+def areaOfRectangle(length, breadth):
+    return length * breadth
---
+++ packages/NgPep/ngpep-0.0.1.tar.gz/ngpep-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="NgPep",
+    version="0.0.1",
+    author="Peter Ojo",
+    author_email="peterojo2dev@gmail.com",
+    description="area of different shapes",
+    long_description="Area of a square, rectangle and triangle",
+    packages=find_packages()
+)
