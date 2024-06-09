668f6c2753a91a7ee13d80ed1083ab5b19ddbb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 14:03:29 +0000

    Add package photobooth-app/3.1.4/photobooth_app-3.1.4.tar.gz

---
+++ packages/photobooth-app/photobooth_app-3.1.4.tar.gz/photobooth_app-3.1.4/tests/tests/utils.py
@@ -0,0 +1,55 @@
+import io
+import logging
+import subprocess
+
+from PIL import Image, ImageChops
+
+from photobooth.services.backends.abstractbackend import AbstractBackend
+
+logger = logging.getLogger(name=None)
+
+
+def get_images(backend: AbstractBackend):
+    # logger.info(f"testing backend {backend.__module__}")
+    # backend.start()
+
+    try:
+        with Image.open(io.BytesIO(backend.wait_for_hq_image())) as img:
+            img.verify()
+    except Exception as exc:
+        raise AssertionError(f"backend did not return valid image bytes, {exc}") from exc
+
+    try:
+        with Image.open(io.BytesIO(backend.wait_for_lores_image())) as img:
+            img.verify()
+    except Exception as exc:
+        raise AssertionError(f"backend did not return valid image bytes, {exc}") from exc
+
+    # stop backend, ensure process is joined properly to collect coverage:
+    # https://pytest-cov.readthedocs.io/en/latest/subprocess-support.html#if-you-use-multiprocessing-process
+    # backend.stop()
+
+
+def is_same(img1: Image.Image, img2: Image.Image):
+    # ensure rgb for both before compare, kind of ignore transparency.
+    img1 = img1.convert("RGB")
+    img2 = img2.convert("RGB")
+
+    # img1.show()
+    # img2.show()
+
+    diff = ImageChops.difference(img2, img1)
+    logger.info(diff.getbbox())
+
+    # getbbox returns None if all same, otherwise anything that is evalued to false
+    return not bool(diff.getbbox())
+
+
+def video_duration(input_video):
+    result = subprocess.run(
+        ["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", input_video],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    print(result.stdout)
+    return float(result.stdout)
