2caf18a9363b75a761fad49a4f310f6dc37a68ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 03:02:58 +0000

    Add package mtpynux/1.0.6/mtpynux-1.0.6.tar.gz

---
+++ packages/mtpynux/mtpynux-1.0.6.tar.gz/mtpynux-1.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'mtpynux',
+  version = '1.0.6',
+  description = 'METATRADER PYTHON LINUX',
+  url = 'https://github.com/pythonsystem/mtpynux',
+  author = 'Original By Lucas Campagna',
+  license = 'MIT',
+  long_description = open('README.txt', 'r').read(),
+  long_description_content_type = 'text/plain',
+  packages = find_packages(include = ['mtpynux*']),
+  install_requires = open('requirements.txt', 'r').read().split('\n'),
+  setup_requires = [],
+  tests_require = [],
+)
