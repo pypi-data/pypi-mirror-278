b5c1d9796dc990915b0455238b6d2e00b8f223cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-09 15:02:59 +0000

    Add package botrun-ask-folder/4.6.93/botrun_ask_folder-4.6.93.tar.gz

---
+++ packages/botrun-ask-folder/botrun_ask_folder-4.6.93.tar.gz/botrun_ask_folder-4.6.93/sh/deployment.sh
@@ -0,0 +1,5 @@
+mkdir -p /Users/chiubowen/PycharmProjects/botrun_back/lib_botrun/botrun_ask_folder
+cp -f /Users/chiubowen/PycharmProjects/botrun_ask_folder/*.py /Users/chiubowen/PycharmProjects/botrun_back/lib_botrun/botrun_ask_folder
+cp -f /Users/chiubowen/PycharmProjects/botrun_ask_folder/bots/*.botrun* /Users/chiubowen/PycharmProjects/botrun_back/bots
+cp -f /Users/chiubowen/PycharmProjects/botrun_ask_folder/requirements.txt /Users/chiubowen/PycharmProjects/botrun_back/lib_botrun/botrun_ask_folder
+cp -fr /Users/chiubowen/PycharmProjects/botrun_ask_folder/qdrant_storage /Users/chiubowen/PycharmProjects/botrun_back
