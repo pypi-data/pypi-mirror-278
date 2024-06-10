df2bbddfec9e5e80c164f0626908caf36eabf3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:17 +0000

    Add package weldx/0.6.8/weldx-0.6.8.tar.gz

---
+++ packages/weldx/weldx-0.6.8.tar.gz/weldx-0.6.8/weldx/transformations/types.py
@@ -0,0 +1,17 @@
+"""shared type definitions."""
+
+from typing import Union
+
+import numpy.typing as npt
+import pint
+import xarray as xr
+from scipy.spatial.transform import Rotation
+
+types_coordinates = Union[xr.DataArray, npt.ArrayLike, pint.Quantity]
+types_orientation = Union[xr.DataArray, npt.ArrayLike, Rotation]
+
+
+__all__ = [
+    "types_coordinates",
+    "types_orientation",
+]
