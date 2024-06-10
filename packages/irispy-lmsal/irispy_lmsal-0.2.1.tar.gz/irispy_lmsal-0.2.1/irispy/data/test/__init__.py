ee858a772a6451d41f8008b87e200f8b220d8856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 04:03:53 +0000

    Add package irispy-lmsal/0.2.1/irispy_lmsal-0.2.1.tar.gz

---
+++ packages/irispy-lmsal/irispy_lmsal-0.2.1.tar.gz/irispy_lmsal-0.2.1/irispy/data/test/__init__.py
@@ -0,0 +1,40 @@
+"""
+IRISPy test data files.
+"""
+
+from pathlib import Path
+
+from astropy.utils.data import get_pkg_data_filename
+
+import irispy
+
+__all__ = ["rootdir", "file_list", "get_test_filepath"]
+
+rootdir = Path(irispy.__file__).parent / "data" / "test"
+file_list = str(Path(rootdir).glob("/*.[!p]*"))
+
+
+def get_test_filepath(filename, **kwargs):
+    """
+    Return the full path to a test file in the ``data/test`` directory.
+
+    Parameters
+    ----------
+    filename : `str`
+        The name of the file inside the ``data/test`` directory.
+
+    Return
+    ------
+    filepath : `str`
+        The full path to the file.
+
+    See Also
+    --------
+    astropy.utils.data.get_pkg_data_filename : Get package data filename
+
+    Notes
+    -----
+    This is a wrapper around `astropy.utils.data.get_pkg_data_filename` which
+    sets the ``package`` kwarg to be 'irispy.data.test`.
+    """
+    return get_pkg_data_filename(filename, package="irispy.data.test", **kwargs)
