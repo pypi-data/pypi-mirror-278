bec827788d11f4c4c9573b147ce4fdd45476db88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 04:04:14 +0000

    Add package mathdf/0.0.6/mathdf-0.0.6.tar.gz

---
+++ packages/mathdf/mathdf-0.0.6.tar.gz/mathdf-0.0.6/setup.py
@@ -0,0 +1,19 @@
+﻿from setuptools import setup
+
+setup(
+    name="mathdf",
+    version="0.0.6",
+    py_modules=["mathdf"],
+    author="Ваше Имя",
+    author_email="ваш_email@example.com",
+    description="Краткое описание вашего пакета",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ваш_пользователь_на_github/my_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
