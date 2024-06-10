ea7e7d4467d3ad4644743c30bd1adc3da631cc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:38 +0000

    Add package seffytestpackage/0.2/seffytestpackage-0.2.tar.gz

---
+++ packages/seffytestpackage/seffytestpackage-0.2.tar.gz/seffytestpackage-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seffytestpackage',
+    version='0.2',
+    description='A simple calculator package',
+    author='Youssef Saad',
+    author_email='youssef.saad@mail.utoronto.ca',
+    packages=find_packages(),
+    install_requires=[],
+    setup_requires=['wheel'],
+)
