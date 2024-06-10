80066efe0e1d38fe343a5c49b8b889ffd8b8eca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:03:57 +0000

    Add package mwxlib/0.95.4/mwxlib-0.95.4-py3-none-any.whl

---
+++ packages/mwxlib/mwxlib-0.95.4-py3-none-any.whl/mwx/__init__.py
@@ -0,0 +1,26 @@
+#! python3
+"""mwxlib framework (based on matplotlib/wx)
+"""
+from .framework import __version__, __author__
+from .framework import FSM
+from .framework import Menu, MenuBar, StatusBar
+from .framework import Frame, MiniFrame, ShellFrame, deb
+
+## Controls
+## from . import controls
+from .controls import Param, LParam, Knob, ControlPanel, Clipboard, Icon
+from .controls import Button, ToggleButton, TextCtrl, Choice, Gauge, Indicator
+
+## Plugman
+## from . import graphman
+## from .graphman import Frame as ViewFrame, Layer, Thread, Graph
+
+## Matplot
+## from .matplot2 import MatplotPanel
+## from .matplot2g import GraphPlot
+## from .matplot2lg import LinePlot
+## from .matplot2lg import Histogram
+## from .matplot2lg import LineProfile
+
+## Gnuplot
+## from .mgplt import Gnuplot, GnuplotFrame
