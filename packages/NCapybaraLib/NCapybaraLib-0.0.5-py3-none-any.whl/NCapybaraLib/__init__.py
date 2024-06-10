121e0278871095082b145f1ac18a5bd39387abf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 04:04:07 +0000

    Add package NCapybaraLib/0.0.5/NCapybaraLib-0.0.5-py3-none-any.whl

---
+++ packages/NCapybaraLib/NCapybaraLib-0.0.5-py3-none-any.whl/NCapybaraLib/__init__.py
@@ -0,0 +1,9 @@
+"""
+NCapybaraLib; By NorbCodes.
+
+A small library with a bunch of functions and thingies I made for fun.
+
+https://pypi.org/project/NCapybaraLib/
+"""
+
+from ._internal.init import print_hello_world, inject_function
