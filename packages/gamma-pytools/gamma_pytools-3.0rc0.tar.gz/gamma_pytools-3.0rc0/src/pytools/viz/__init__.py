8b38447a363b724e206ee3306b0920743c2cada7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 16:03:17 +0000

    Add package gamma-pytools/3.0rc0/gamma_pytools-3.0rc0.tar.gz

---
+++ packages/gamma-pytools/gamma_pytools-3.0rc0.tar.gz/gamma_pytools-3.0rc0/src/pytools/viz/__init__.py
@@ -0,0 +1,9 @@
+"""
+A lean MVC framework for rendering basic visualizations in different styles, e.g.,
+as `matplotlib` charts or as plain text.
+"""
+
+from ._matplot import *
+from ._notebook import *
+from ._text import *
+from ._viz import *
