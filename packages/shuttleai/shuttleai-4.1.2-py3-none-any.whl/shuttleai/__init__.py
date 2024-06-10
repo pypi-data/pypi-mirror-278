8fd12f0e39d918c46498744e5c72c40c2c58f1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:03:52 +0000

    Add package shuttleai/4.1.2/shuttleai-4.1.2-py3-none-any.whl

---
+++ packages/shuttleai/shuttleai-4.1.2-py3-none-any.whl/shuttleai/__init__.py
@@ -0,0 +1,9 @@
+__title__ = "shuttleai"
+__version__ = "4.1.2"
+
+from ._patch import _patch_httpx
+from .client import AsyncShuttleAI, ShuttleAI
+
+_patch_httpx()
+
+__all__ = ["ShuttleAI", "AsyncShuttleAI"]
