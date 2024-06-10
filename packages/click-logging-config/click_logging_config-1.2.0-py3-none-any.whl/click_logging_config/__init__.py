eafb27f1ba1f26e2a70e7a24624ef570003585e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:04:02 +0000

    Add package click-logging-config/1.2.0/click_logging_config-1.2.0-py3-none-any.whl

---
+++ packages/click-logging-config/click_logging_config-1.2.0-py3-none-any.whl/click_logging_config/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2022 Russell Smiley
+#
+#  This file is part of click_logging_config.
+
+"""Quick and easy logging parameters for click commands."""
+
+from ._click import logging_parameters  # noqa: F401
+from ._logging import LoggingConfiguration, LoggingState  # noqa: F401
+from ._version import __version__  # noqa: F401
