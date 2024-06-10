ff13c4dfa96276a4f85b4eee065acb51f503a1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:04:05 +0000

    Add package textlong/0.3.0/textlong-0.3.0-py3-none-any.whl

---
+++ packages/textlong/textlong-0.3.0-py3-none-any.whl/textlong/__init__.py
@@ -0,0 +1,16 @@
+from .utils import markdown
+from .hub import load_resource_prompt
+
+from .md import idea, rewrite, fetch, translate, outline, outline_self, outline_detail
+from .project import Project
+
+__all__ = [
+  "idea",
+  "rewrite",
+  "fetch",
+  "translate",
+  "outline",
+  "outline_self",
+  "outline_detail",
+  "Project",
+]
