1d8bb201b5cf7aa2de9ea9fcebefaf4b4413567a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:53 +0000

    Add package qontract-reconcile/0.10.1rc878/qontract_reconcile-0.10.1rc878-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc878-py3-none-any.whl/reconcile/typed_queries/clusters.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from reconcile.gql_definitions.common.clusters import (
+    ClusterV1,
+    query,
+)
+from reconcile.utils import gql
+from reconcile.utils.gql import GqlApi
+
+
+def get_clusters(
+    gql_api: Optional[GqlApi] = None,
+    name: Optional[str] = None,
+) -> list[ClusterV1]:
+    variables = {}
+    if name:
+        variables["name"] = name
+    api = gql_api if gql_api else gql.get_api()
+    data = query(query_func=api.query, variables=variables)
+    return list(data.clusters or [])
---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc878-py3-none-any.whl/reconcile/utils/vaultsecretref.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import (
+    Optional,
+    cast,
+)
+
+from reconcile.utils.vault import (
+    VaultClient,
+    _VaultClient,
+)
+
+
+@dataclass
+class VaultSecretRef:
+    _ALL_FIELDS = "all"
+
+    path: str
+    field: str
+    format: Optional[str] = None
+    version: Optional[int] = None
+
+    def get(self, field=None, default=None):
+        secret_content = self._resolve_secret()
+        if field:
+            return secret_content.get(field, default)
+        if self.field == VaultSecretRef._ALL_FIELDS:
+            return secret_content
+        return secret_content.get(self.field, default)
+
+    def _resolve_secret(self) -> dict[str, str]:
+        vault_client = cast(_VaultClient, VaultClient())
+        if self.field == VaultSecretRef._ALL_FIELDS:
+            return vault_client.read_all(self.__dict__)
+        field_value = vault_client.read(self.__dict__)
+        return {self.field: field_value}
