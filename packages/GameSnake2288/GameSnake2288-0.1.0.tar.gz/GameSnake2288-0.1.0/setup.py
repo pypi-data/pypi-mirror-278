7fed901d26c18d46f121aa09b86b870162eed89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:03:40 +0000

    Add package GameSnake2288/0.1.0/GameSnake2288-0.1.0.tar.gz

---
+++ packages/GameSnake2288/GameSnake2288-0.1.0.tar.gz/GameSnake2288-0.1.0/setup.py
@@ -0,0 +1,13 @@
+# my_module/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="GameSnake2288",
+    version="0.1.0",
+    description="U can run snake game in 1 sec",
+    author="NRx",
+    packages=find_packages(),
+    install_requires=[
+        'pygame',
+    ],
+)
