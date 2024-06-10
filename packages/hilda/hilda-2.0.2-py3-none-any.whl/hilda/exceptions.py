51449bcb0aed220d44fa02966909e887b15593d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:04:11 +0000

    Add package hilda/2.0.2/hilda-2.0.2-py3-none-any.whl

---
+++ packages/hilda/hilda-2.0.2-py3-none-any.whl/hilda/exceptions.py
@@ -0,0 +1,77 @@
+__all__ = ['HildaException', 'SymbolAbsentError', 'EvaluatingExpressionError', 'CreatingObjectiveCSymbolError',
+           'ConvertingToNsObjectError', 'ConvertingFromNSObjectError', 'DisableJetsamMemoryChecksError',
+           'GettingObjectiveCClassError', 'AccessingRegisterError', 'AccessingMemoryError',
+           'BrokenLocalSymbolsJarError', 'AddingLldbSymbolError', 'LLDBException', 'InvalidThreadIndexError']
+
+
+class HildaException(Exception):
+    """ A domain exception for hilda errors. """
+    pass
+
+
+class LLDBException(Exception):
+    """ A domain exception for lldb errors. """
+
+    def __init__(self, message: str):
+        super().__init__()
+        self.message = message
+
+
+class SymbolAbsentError(HildaException):
+    """ Raise when trying to get a symbol that doesn't exist. """
+    pass
+
+
+class EvaluatingExpressionError(HildaException):
+    """ Raise when failing to evaluate an expression. """
+    pass
+
+
+class CreatingObjectiveCSymbolError(HildaException):
+    """ Raise when failing to create an ObjectiveC Symbol. """
+    pass
+
+
+class ConvertingToNsObjectError(HildaException):
+    """ Raise when failing to convert python object to NS object. """
+    pass
+
+
+class ConvertingFromNSObjectError(HildaException):
+    """ Raise when failing to convert NS object to python object. """
+    pass
+
+
+class DisableJetsamMemoryChecksError(HildaException):
+    """ Raise when failing to disable jetsam memory checks. """
+    pass
+
+
+class GettingObjectiveCClassError(HildaException):
+    """ Raise when failing to get an ObjectiveC class. """
+    pass
+
+
+class AccessingRegisterError(HildaException):
+    """ Raise when failing to access a register. """
+    pass
+
+
+class AccessingMemoryError(HildaException):
+    """ Raise when failing to access memory. """
+    pass
+
+
+class BrokenLocalSymbolsJarError(HildaException):
+    """ Raise when attempt to load an invalid symbols jar pickle """
+    pass
+
+
+class AddingLldbSymbolError(HildaException):
+    """ Raise when failing to convert a LLDB symbol to Hilda's symbol. """
+    pass
+
+
+class InvalidThreadIndexError(HildaException):
+    """ Raise when thread idx invalid """
+    pass
