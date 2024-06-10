72b6f8f7d1d11be15951f7a3717e397ba63555a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:20 +0000

    Add package axcorsrmri/0.0.3/axcorsrmri-0.0.3.tar.gz

---
+++ packages/axcorsrmri/axcorsrmri-0.0.3.tar.gz/axcorsrmri-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='axcorsrmri',
+    version='0.0.3',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Technion Computational MRI Lab',
+    author_email='tcml.bme@gmail.com',
+    url='https://github.com/TechnionComputationalMRILab/AxCorSRMRI',
+    packages=find_packages(),
+    install_requires=requirements,
+)
