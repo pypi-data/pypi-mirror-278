bf9f05133961e45ebc73757402b4a98c6dbad4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:23 +0000

    Add package chatbot-code/110.6/chatbot_code-110.6.tar.gz

---
+++ packages/chatbot-code/chatbot_code-110.6.tar.gz/chatbot_code-110.6/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+
+setup(
+    name='chatbot_code',
+    version='110.6',
+    packages=find_packages(include=['chatbot', 'chatbot.*', 'app1', 'app1.*']),
+    include_package_data=True,
+    install_requires=[],  # No dependencies here
+    entry_points={
+        'console_scripts': [
+            'manage.py = chatbot.manage:main',
+            'configure-aws = chatbot.scripts.configure_aws:main',
+            'readme = chatbot.scripts.readme:main',
+        ],
+    },
+    package_data={
+        '': ['README.md'],
+        'app1': ['static/*', 'templates/*', 'templatetags/*'],
+        'chatbot': ['scripts/*'],
+    },
+    author='Swati Saini',
+    author_email='swati@mixorg.com',
+    description='A Django app for chatbot functionality.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/saini2001/chatbot_code',
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
