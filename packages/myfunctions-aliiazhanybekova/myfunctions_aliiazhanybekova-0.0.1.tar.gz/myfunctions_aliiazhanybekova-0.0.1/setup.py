3011dcbb5c7f608c1d7ba497662a99c009b40004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 05:03:05 +0000

    Add package myfunctions-aliiazhanybekova/0.0.1/myfunctions_aliiazhanybekova-0.0.1.tar.gz

---
+++ packages/myfunctions-aliiazhanybekova/myfunctions_aliiazhanybekova-0.0.1.tar.gz/myfunctions_aliiazhanybekova-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_aliiazhanybekova',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    url='',
+    license='MIT',
+    author='aliia',
+    author_email='aliia.zhanybekova@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
