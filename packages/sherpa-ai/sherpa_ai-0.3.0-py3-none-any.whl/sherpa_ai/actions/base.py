1f36a65d490e3406008b172f5b7731ce04300e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:03:55 +0000

    Add package sherpa-ai/0.3.0/sherpa_ai-0.3.0-py3-none-any.whl

---
+++ packages/sherpa-ai/sherpa_ai-0.3.0-py3-none-any.whl/sherpa_ai/actions/base.py
@@ -0,0 +1,47 @@
+import json
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from functools import cached_property
+
+from pydantic import BaseModel, Field
+
+
+class ActionResource(BaseModel):
+    """
+    Resource used for an action.
+
+    Attributes:
+        source (str): Source of the resource, such as document id or url.
+        content (str): Content of the resource.
+    """
+
+    source: str
+    content: str
+
+
+class BaseAction(ABC, BaseModel):
+    name: str
+    args: dict
+    usage: str
+
+    @abstractmethod
+    def execute(self, **kwargs):
+        pass
+
+    def __str__(self):
+        tool_desc = {"name": self.name, "args": self.args, "usage": self.usage}
+
+        return json.dumps(tool_desc, indent=4)
+
+
+class BaseRetrievalAction(BaseAction, ABC):
+    resources: list[ActionResource] = Field(default_factory=list)
+
+    def add_resources(self, resources: list[dict]):
+        action_resources = self.resources
+        action_resources.clear()
+
+        for resource in resources:
+            action_resources.append(
+                ActionResource(source=resource["Source"], content=resource["Document"])
+            )
---
+++ packages/sherpa-ai/sherpa_ai-0.3.0-py3-none-any.whl/sherpa_ai/actions/context_search.py
@@ -0,0 +1,56 @@
+from typing import Any
+
+from loguru import logger
+
+from sherpa_ai.actions.base import BaseRetrievalAction
+from sherpa_ai.connectors.vectorstores import get_vectordb
+from sherpa_ai.tools import ContextTool
+
+
+SEARCH_SUMMARY_DESCRIPTION = """Role Description: {role_description}
+Task: {task}
+
+Relevant Documents:
+{documents}
+
+
+Review and analyze the provided documents with respect to the task. Craft a concise and short, unified summary that distills key information that is most relevant to the task, incorporating reference links within the summary.
+Only use the information given. Do not add any additional information. The summary should be less than {n} setences
+"""  # noqa: E501
+
+
+class ContextSearch(BaseRetrievalAction):
+    role_description: str
+    task: str
+    llm: Any  # The BaseLanguageModel from LangChain is not compatible with Pydantic 2 yet
+    description: str = SEARCH_SUMMARY_DESCRIPTION
+    n: int = 5
+    _context: Any
+
+    # Override the name and args from BaseAction
+    name: str = "Context Search"
+    args: dict = {"query": "string"}
+    usage: str = "Search the conversation history with the user"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._context = ContextTool(memory=get_vectordb())
+
+    def execute(self, query) -> str:
+        result, resources = self._context._run(query, return_resources=True)
+
+        self.add_resources(resources)
+
+        # result = "Context Search"
+        logger.debug("Context Search Result: {}", result)
+
+        prompt = self.description.format(
+            task=self.task,
+            documents=result,
+            n=self.n,
+            role_description=self.role_description,
+        )
+
+        result = self.llm.predict(prompt)
+
+        return result
