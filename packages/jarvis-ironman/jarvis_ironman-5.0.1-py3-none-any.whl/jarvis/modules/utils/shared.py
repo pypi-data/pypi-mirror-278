ca21fd5d7a6de34a42dd2f12595ee158c4f02727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:04:01 +0000

    Add package jarvis-ironman/5.0.1/jarvis_ironman-5.0.1-py3-none-any.whl

---
+++ packages/jarvis-ironman/jarvis_ironman-5.0.1-py3-none-any.whl/jarvis/modules/utils/shared.py
@@ -0,0 +1,26 @@
+# noinspection PyUnresolvedReferences
+"""This is a space for variables shared across multiple modules.
+
+>>> Shared
+
+"""
+
+import time
+
+start_time = time.time()
+greeting = False
+called_by_offline = False
+called_by_bg_tasks = False
+
+text_spoken = None
+offline_caller = None
+tv = {}
+
+processes = {}
+hosted_device = {}
+
+called = {
+    "report": False,
+    "locate_places": False,
+    "directions": False,
+}
