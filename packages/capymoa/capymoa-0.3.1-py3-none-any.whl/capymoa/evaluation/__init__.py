95d24ec2d00d990609151749b7eb10be5963abd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:27 +0000

    Add package capymoa/0.3.1/capymoa-0.3.1-py3-none-any.whl

---
+++ packages/capymoa/capymoa-0.3.1-py3-none-any.whl/capymoa/evaluation/__init__.py
@@ -0,0 +1,31 @@
+from .evaluation import (
+    cumulative_evaluation,
+    cumulative_ssl_evaluation,
+    prequential_evaluation,
+    windowed_evaluation,
+    prequential_evaluation_multiple_learners,
+    prequential_ssl_evaluation,
+    ClassificationEvaluator,
+    ClassificationWindowedEvaluator,
+    RegressionWindowedEvaluator,
+    RegressionEvaluator,
+    PredictionIntervalEvaluator,
+    PredictionIntervalWindowedEvaluator,
+    AUCEvaluator,
+)
+
+__all__ = [
+    "prequential_evaluation",
+    "prequential_ssl_evaluation",
+    "cumulative_evaluation",
+    "cumulative_ssl_evaluation",
+    "windowed_evaluation",
+    "prequential_evaluation_multiple_learners",
+    "ClassificationEvaluator",
+    "ClassificationWindowedEvaluator",
+    "RegressionWindowedEvaluator",
+    "RegressionEvaluator",
+    "PredictionIntervalEvaluator",
+    "PredictionIntervalWindowedEvaluator",
+    "AUCEvaluator",
+]
