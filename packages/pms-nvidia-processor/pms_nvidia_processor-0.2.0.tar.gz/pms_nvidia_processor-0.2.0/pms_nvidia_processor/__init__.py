1cdb9e893c0b9508717fe6f0e5970c9bbb6bcb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:51 +0000

    Add package pms-nvidia-processor/0.2.0/pms_nvidia_processor-0.2.0.tar.gz

---
+++ packages/pms-nvidia-processor/pms_nvidia_processor-0.2.0.tar.gz/pms_nvidia_processor-0.2.0/pms_nvidia_processor/__init__.py
@@ -0,0 +1,5 @@
+from ._dpir_processor import DPIRProcessor
+from ._dru_rbpn_sr_f3_processor import DRURBPNSRF3Processor
+from ._dru_rbpn_sr_f5_processor import DRURBPNSRF5Processor
+
+__version__ = "0.2.0"
---
+++ packages/pms-nvidia-processor/pms_nvidia_processor-0.2.0.tar.gz/pms_nvidia_processor-0.2.0/pms_nvidia_processor/_dependency.py
@@ -0,0 +1,10 @@
+from typing import List, Optional, Tuple, Iterable, Literal, Dict, Any
+from dataclasses import dataclass
+import os
+import time
+import uvloop
+import asyncio
+import numpy as np
+from loguru import logger
+import pms_tensorrt as TRT
+from pms_inference_engine import IEngineProcessor, EngineIOData, register
