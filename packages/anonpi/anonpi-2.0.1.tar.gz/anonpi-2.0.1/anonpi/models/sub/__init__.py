284b7bf3bc0c1c71cc0ef8d98c00feaf0a9b34f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:03:59 +0000

    Add package anonpi/2.0.1/anonpi-2.0.1.tar.gz

---
+++ packages/anonpi/anonpi-2.0.1.tar.gz/anonpi-2.0.1/anonpi/models/sub/__init__.py
@@ -0,0 +1,13 @@
+"""
+This module contains the models for the main application.
+
+Do not edit the classes manually or import them directly.
+"""
+
+from anonpi.models.sub.anonobject import AnonObject
+from anonpi.models.sub.callconn import CallBaseAnon
+from anonpi.models.sub.userconn import UserBaseConn
+from anonpi.models.sub.records import *
+# Path: models/sub/callconn.py
+__slots__ = ['CallBaseAnon', 'AnonObject', 'UserBaseConn', 'CallRecord', 'TopupRecord',
+             'RecordsBase', 'RecordRepresentation', 'UserCallRecords', 'UserTopupRecords']
