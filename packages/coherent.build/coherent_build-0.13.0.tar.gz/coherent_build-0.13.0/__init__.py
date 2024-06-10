d06e4e395bebc45c6a3c5260d164c5e793fc534a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:36 +0000

    Add package coherent.build/0.13.0/coherent_build-0.13.0.tar.gz

---
+++ packages/coherent.build/coherent_build-0.13.0.tar.gz/coherent_build-0.13.0/__init__.py
@@ -0,0 +1,18 @@
+__requires__ = [
+    'wheel',
+    'pip-run',
+    'setuptools_scm',
+    'build',
+    'git-fame',
+    'jaraco.context',
+    'requests',
+    'packaging',
+    'jaraco.functools',
+    'python-dateutil',
+]
+
+
+from .backend import build_wheel, build_sdist
+
+
+__all__ = ['build_wheel', 'build_sdist']
