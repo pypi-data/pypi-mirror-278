b863e18d82d1f8bc44df7a0e7f4a390825caf686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 16:03:23 +0000

    Add package automatic-contract-creation/0.0.0.1/automatic_contract_creation-0.0.0.1.tar.gz

---
+++ packages/automatic-contract-creation/automatic_contract_creation-0.0.0.1.tar.gz/automatic_contract_creation-0.0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+	name="automatic_contract_creation",
+	version="0.0.0.1",
+	author="Mira Terekhova",
+	author_email="miraterekhova@mail.ru",
+	description="This is package designed for auto-generation of contracts and tests in the soda syntax",
+	long_description=readme(),
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.7',
+)
