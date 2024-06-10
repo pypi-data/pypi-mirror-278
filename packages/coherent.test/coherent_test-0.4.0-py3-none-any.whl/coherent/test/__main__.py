4a6b566da3179ff3e6f5693ab4c04c080f7ab27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:11 +0000

    Add package coherent.test/0.4.0/coherent_test-0.4.0-py3-none-any.whl

---
+++ packages/coherent.test/coherent_test-0.4.0-py3-none-any.whl/coherent/test/__main__.py
@@ -0,0 +1,18 @@
+import os
+import runpy
+import sys
+
+import pip_run
+from coherent.build import bootstrap
+
+
+def run():
+    os.environ.update(
+        PYTEST_ADDOPTS='--doctest-modules',
+    )
+    with bootstrap.write_pyproject(), pip_run.deps.load('.[test]') as home:
+        sys.path.insert(0, str(home))
+        runpy.run_module('pytest', run_name='__main__')
+
+
+__name__ == '__main__' and run()
