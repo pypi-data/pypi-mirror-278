3f288f1ffe472bf497ca3a164b917b5d7bfe5b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:04:03 +0000

    Add package caty/0.2423.1/caty-0.2423.1-py3-none-any.whl

---
+++ packages/caty/caty-0.2423.1-py3-none-any.whl/caty/_dump_gen.py
@@ -0,0 +1,33 @@
+from sys import stdin
+
+from caty.plugins import Plugins
+from caty.main import main
+
+
+head = '''
+from sys import stdin
+
+from caty.main import main
+
+'''.lstrip()
+
+tpl = r'''
+def dump_{ext}():
+    try:
+        main(stdin, '{ext}')
+    except Exception as x:
+        print(f'\n ! {{x}} !\n')
+
+'''
+
+def gen():
+    exts = [Plugin.ext[0] for Plugin in Plugins]
+    with open('dump.py', 'w') as dump, open('../pyproject.toml', 'a') as prj:
+        dump.write(head)
+        for ext in exts:
+            print(ext)
+            dump.write(tpl.format(**locals()))
+            prj.write(f'dump_{ext} = "caty.dump:dump_{ext}"\n')
+
+
+if __name__ == '__main__': gen()
