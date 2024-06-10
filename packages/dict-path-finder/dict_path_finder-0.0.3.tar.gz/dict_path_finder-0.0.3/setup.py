58a11c87694ea234ec77b4c946391f82817ce042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:03:53 +0000

    Add package dict-path-finder/0.0.3/dict_path_finder-0.0.3.tar.gz

---
+++ packages/dict-path-finder/dict_path_finder-0.0.3.tar.gz/dict_path_finder-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dict_path_finder",
+    version="0.0.3",
+    author="Jinhan Kim",
+    author_email="developers.xyz@gmail.com",
+    description="Find dictionary path by key and value",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/developers-dev/",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
