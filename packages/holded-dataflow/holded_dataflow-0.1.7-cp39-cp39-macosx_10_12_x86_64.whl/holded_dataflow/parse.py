b139bfb7f7175de9a7c2924dcfe8fec9fe8ab3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 04:03:54 +0000

    Add package holded-dataflow/0.1.7/holded_dataflow-0.1.7-cp39-cp39-macosx_10_12_x86_64.whl

---
+++ packages/holded-dataflow/holded_dataflow-0.1.7-cp39-cp39-macosx_10_12_x86_64.whl/holded_dataflow/parse.py
@@ -0,0 +1,34 @@
+from typing import Dict, Any
+
+
+def parse_attributes(attributes: Dict[str, str]) -> Dict[str, Any]:
+    """
+    Parse attributes using the Rust function from my_module.
+
+    Parameters:
+        attributes (dict): A dictionary containing attribute key-value pairs.
+
+    Returns:
+        dict: A dictionary containing parsed attribute key-value pairs.
+    """
+
+
+def parse_binary_json(data: bytes) -> Any:
+    """
+    Parse binary JSON data and return the parsed JSON object.
+
+    Args:
+        data (bytes): Binary JSON data.
+
+    Returns:
+        Any: The parsed JSON object.
+    """
+
+
+def old_cookie_parser(cookie: str) -> dict:
+    """
+    Parse a string cookie value and convert it into a json object
+    with more data.
+    :param cookie:
+    :return: dict
+    """
