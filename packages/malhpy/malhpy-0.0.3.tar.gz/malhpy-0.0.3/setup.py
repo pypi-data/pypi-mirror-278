12b6626335369654f06ea2ff26afa9299f0e758b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 04:03:56 +0000

    Add package malhpy/0.0.3/malhpy-0.0.3.tar.gz

---
+++ packages/malhpy/malhpy-0.0.3.tar.gz/malhpy-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="malhpy",
+    version="0.0.3",
+    py_modules=["malhpy"],
+    author="Ваше Имя",
+    author_email="ваш_email@example.com",
+    description="Краткое описание вашего пакета",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ваш_пользователь_на_github/my_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
