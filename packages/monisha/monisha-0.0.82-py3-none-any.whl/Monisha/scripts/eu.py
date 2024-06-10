806b476197fcae517b85f2cf30cae903e45cf667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:10 +0000

    Add package monisha/0.0.82/monisha-0.0.82-py3-none-any.whl

---
+++ packages/monisha/monisha-0.0.82-py3-none-any.whl/Monisha/scripts/eu.py
@@ -0,0 +1,9 @@
+
+
+class Apis(object):
+
+    DATA01 = "https://www.x-rates.com/calculator/?from={}&to={}&amount=1"
+
+class Links(object):
+
+    DATA01 = "https://www.x-rates.com/calculator/?from={}&to={}&amount=1"
