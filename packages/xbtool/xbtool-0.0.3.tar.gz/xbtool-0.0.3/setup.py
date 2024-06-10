ca3d47312816c010e447189e468a7d948e73d7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 04:04:02 +0000

    Add package xbtool/0.0.3/xbtool-0.0.3.tar.gz

---
+++ packages/xbtool/xbtool-0.0.3.tar.gz/xbtool-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='xbtool',##包名
+    version='0.0.3',##版本号
+    packages=find_packages(),
+    description='A library of your own tools',##介绍
+    python_requires='>=3.8',
+##    long_description=open('README.md').read(),
+    # python3，readme文件中文报错
+    # long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+##    url='http://github.com/yourusername/my_package',
+    author='XiangBing',
+    author_email='2677506404@qq.com',
+    license='MIT',
+    install_requires=[
+        # 依赖列表
+    ],
+    classifiers=[
+        # 分类信息
+    ]
+)
