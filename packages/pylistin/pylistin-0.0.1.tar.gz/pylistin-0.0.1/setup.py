e39ffad031d3ffbb98044eed7a79059beedc2113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:03:36 +0000

    Add package pylistin/0.0.1/pylistin-0.0.1.tar.gz

---
+++ packages/pylistin/pylistin-0.0.1.tar.gz/pylistin-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pylisting",
+    license="MIT",
+    author="Camilo Andres Barbosa Gracia",
+    packages=['pylisting'],
+    
+    version="1.0.0",
+    description="functions for work with list in python.",
+    long_description=open('README.md').read(),  
+
+)
