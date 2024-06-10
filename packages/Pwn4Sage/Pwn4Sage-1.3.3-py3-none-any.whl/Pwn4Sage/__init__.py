7fd685c0d75684d5d772914440c1b19554fd4e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:03:50 +0000

    Add package Pwn4Sage/1.3.3/Pwn4Sage-1.3.3-py3-none-any.whl

---
+++ packages/Pwn4Sage/Pwn4Sage-1.3.3-py3-none-any.whl/Pwn4Sage/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from Pwn4Sage.pwn import *
+
+__all__ = ['Pwn4Sage']
+__title__ = 'Pwn4Sage'
+__version__ = '1.3.3'
+__author__ = 'Harry0597'
+__copyright__ = 'Copyright 2023 Harry0597'
