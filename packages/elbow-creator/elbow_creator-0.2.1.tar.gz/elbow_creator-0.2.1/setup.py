32e5ffa0773601eba5b7ffd8d88d6976e7868f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 16:03:12 +0000

    Add package elbow-creator/0.2.1/elbow_creator-0.2.1.tar.gz

---
+++ packages/elbow-creator/elbow_creator-0.2.1.tar.gz/elbow_creator-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elbow_creator',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'bpy',
+        'mathutils'
+    ],
+    entry_points={
+        'console_scripts': [
+            'create_elbow=elbow_creator.elbow_creator:create_elbow',
+        ],
+    },
+    author='Vamshi Gangadhar',
+    author_email='vamshi.gangadhar365@gmail.com',
+    description='A package to create an elbow mesh in Blender',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/VamshiGangadhar/elbow_creator',
+)
