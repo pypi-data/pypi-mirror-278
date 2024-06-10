cab8ba25c75b85d2876c333261bf846426d603b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 05:02:56 +0000

    Add package fsspreadsheet/6.1.3/fsspreadsheet-6.1.3-py3-none-any.whl

---
+++ packages/fsspreadsheet/fsspreadsheet-6.1.3-py3-none-any.whl/fsspreadsheet/fs_extension.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import Any
+from .FsSpreadsheet.fs_workbook import FsWorkbook
+from .json import Json
+from .xlsx import Xlsx
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_fromXlsxFile_Static_Z721C83C5(path: str) -> FsWorkbook:
+    return Xlsx.from_xlsx_file(path)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_fromXlsxStream_Static_4D976C1A(stream: Any) -> FsWorkbook:
+    return Xlsx.from_xlsx_stream(stream)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_fromXlsxBytes_Static_Z3F6BC7B1(bytes: bytearray) -> FsWorkbook:
+    return Xlsx.from_xlsx_bytes(bytes)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_toXlsxFile_Static(path: str, wb: FsWorkbook) -> None:
+    Xlsx.to_xlsx_file(path, wb)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_toXlsxBytes_Static_32154C9D(wb: FsWorkbook) -> bytearray:
+    return Xlsx.to_xlsx_bytes(wb)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_ToXlsxFile_Z721C83C5(this: FsWorkbook, path: str) -> None:
+    FsSpreadsheet_FsWorkbook__FsWorkbook_toXlsxFile_Static(path, this)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_ToXlsxBytes(this: FsWorkbook) -> bytearray:
+    return FsSpreadsheet_FsWorkbook__FsWorkbook_toXlsxBytes_Static_32154C9D(this)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_fromJsonString_Static_Z721C83C5(json: str) -> FsWorkbook:
+    return Json.from_json_string(json)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_toJsonString_Static_32154C9D(wb: FsWorkbook) -> str:
+    return Json.to_json_string(wb)
+
+
+def FsSpreadsheet_FsWorkbook__FsWorkbook_ToJsonString(this: FsWorkbook) -> str:
+    return FsSpreadsheet_FsWorkbook__FsWorkbook_toJsonString_Static_32154C9D(this)
+
+
+__all__ = ["FsSpreadsheet_FsWorkbook__FsWorkbook_fromXlsxFile_Static_Z721C83C5", "FsSpreadsheet_FsWorkbook__FsWorkbook_fromXlsxStream_Static_4D976C1A", "FsSpreadsheet_FsWorkbook__FsWorkbook_fromXlsxBytes_Static_Z3F6BC7B1", "FsSpreadsheet_FsWorkbook__FsWorkbook_toXlsxFile_Static", "FsSpreadsheet_FsWorkbook__FsWorkbook_toXlsxBytes_Static_32154C9D", "FsSpreadsheet_FsWorkbook__FsWorkbook_ToXlsxFile_Z721C83C5", "FsSpreadsheet_FsWorkbook__FsWorkbook_ToXlsxBytes", "FsSpreadsheet_FsWorkbook__FsWorkbook_fromJsonString_Static_Z721C83C5", "FsSpreadsheet_FsWorkbook__FsWorkbook_toJsonString_Static_32154C9D", "FsSpreadsheet_FsWorkbook__FsWorkbook_ToJsonString"]
+
