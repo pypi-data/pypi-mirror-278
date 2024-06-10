a025a45bdc3d826efa511f5274a8311efaa2c093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:04:00 +0000

    Add package launchable/1.90.0/launchable-1.90.0.tar.gz

---
+++ packages/launchable/launchable-1.90.0.tar.gz/launchable-1.90.0/tests/data/pytest/tests/test_funcs1.py
@@ -0,0 +1,6 @@
+def test_func1():
+    assert 1 == True  # noqa: E712
+
+
+def test_func2():
+    assert 1 == False  # noqa: E712
---
+++ packages/launchable/launchable-1.90.0.tar.gz/launchable-1.90.0/tests/data/pytest/tests/test_funcs2.py
@@ -0,0 +1,6 @@
+def test_func3():
+    assert True == True  # noqa: E712
+
+
+def test_func4():
+    assert False == False  # noqa: E712
