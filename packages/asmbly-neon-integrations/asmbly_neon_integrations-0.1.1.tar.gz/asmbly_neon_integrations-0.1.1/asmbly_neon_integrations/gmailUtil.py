ae1d79bd84069d2631ef2345b2a488dc02481c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:03:43 +0000

    Add package asmbly-neon-integrations/0.1.1/asmbly_neon_integrations-0.1.1.tar.gz

---
+++ packages/asmbly-neon-integrations/asmbly_neon_integrations-0.1.1.tar.gz/asmbly_neon_integrations-0.1.1/asmbly_neon_integrations/gmailUtil.py
@@ -0,0 +1,31 @@
+import smtplib, ssl, logging
+
+from credentials import get_creds
+
+creds = get_creds()
+
+
+#################################################################################
+# Sent a MIME email object to its recipient using GMail
+#################################################################################
+def sendMIMEmessage(MIMEmessage):
+    if not "@" in MIMEmessage["To"]:
+        raise ValueError("Message doesn't have a sane destination address")
+
+    MIMEmessage["From"] = "Asmbly AdminBot"
+
+    logging.debug(
+        f"""Sending email subject "{MIMEmessage['Subject']}" to {MIMEmessage['To']}"""
+    )
+
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(creds.gmail_user, creds.gmail_pass)
+            server.sendmail(
+                creds.gmail_user, MIMEmessage["To"], MIMEmessage.as_string()
+            )
+    except:
+        logging.exception(
+            f"""Failed sending email subject "{MIMEmessage['Subject']}" to {MIMEmessage['To']}"""
+        )
