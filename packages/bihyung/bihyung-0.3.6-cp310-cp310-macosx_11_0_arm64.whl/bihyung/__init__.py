4ada5ed9509836ca31af0ddb47c0129a3fcf946c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:04:11 +0000

    Add package bihyung/0.3.6/bihyung-0.3.6-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/bihyung/bihyung-0.3.6-cp310-cp310-macosx_11_0_arm64.whl/bihyung/__init__.py
@@ -0,0 +1,16 @@
+from .bihyung import *
+from pathlib import Path
+
+# it won't work when package is archived within a package
+# we might need to create temp files...
+_server_path = (Path(__file__).parent / "server").absolute().as_posix()
+
+def daemon_from_model(model: bihyung.Model) -> bihyung.DaemonHandle:
+    return bihyung._daemon_from_model(model, _server_path)
+
+__doc__ = bihyung.__doc__
+if hasattr(bihyung, "__all__"):
+    __all__ = bihyung.__all__
+else:
+    __all__ = []
+
