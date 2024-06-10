47d6e60b97d2988856261020aa7ccc7d0adbcd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:04:07 +0000

    Add package chat-notes/0.1.3/chat_notes-0.1.3.tar.gz

---
+++ packages/chat-notes/chat_notes-0.1.3.tar.gz/chat_notes-0.1.3/README.md
@@ -0,0 +1,19 @@
+# ChatAppleNotes
+
+A simple application to parse your local Apple Notes and chat with them using Ollama.
+
+## Installation
+
+```bash
+pip install -e .
+```
+## Usage
+```bash
+chat-notes
+```
+## TODO
+
+1. Integrate with llama-index to index all of the notes 
+2. Chat in RAG mode 
+3. Possible idea: insert as much similar Notes as possible in context to get high recall 
+4. Add OpenAI chat
---
+++ packages/chat-notes/chat_notes-0.1.3.tar.gz/chat_notes-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chat_notes',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'macnotesapp',
+        'ollama',
+        'termcolor',
+        'transformers',
+        'torch',
+    ],
+    entry_points={
+        'console_scripts': [
+            'chat-notes=src.parse_notes_local:cli',
+        ],
+    },
+)
