c700c064151420422520169c50b83a4ac529eb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:04:03 +0000

    Add package whisper-run/0.1.0/whisper_run-0.1.0-py3-none-any.whl

---
+++ packages/whisper-run/whisper_run-0.1.0-py3-none-any.whl/whisper_run/config.py
@@ -0,0 +1,13 @@
+whisper_models = [
+    "distil-whisper/distil-large-v3",
+    "distil-whisper/distil-large-v2",
+    "openai/whisper-large-v2",
+    "openai/whisper-large",
+    "openai/whisper-medium",
+    "openai/whisper-small",
+    "openai/whisper-tiny",
+    "openai/whisper-base",
+    "openai/whisper-large-v3",
+    "openai/whisper-medium.en",
+    "openai/whisper-small.en"
+]
