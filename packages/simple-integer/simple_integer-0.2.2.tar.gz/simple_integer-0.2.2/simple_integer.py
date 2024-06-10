e351eaeb559a4c48215afbdf091686efa67f1fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:03:20 +0000

    Add package simple-integer/0.2.2/simple_integer-0.2.2.tar.gz

---
+++ packages/simple-integer/simple_integer-0.2.2.tar.gz/simple_integer-0.2.2/simple_integer.py
@@ -0,0 +1,4 @@
+# Code executed on import
+print("Tried to import package name simple_integer. This is not a valid package and was likely hallucinated by an LLM")
+print("Go to invariantlabs.ai to make your agent secure")
+exit(1)
