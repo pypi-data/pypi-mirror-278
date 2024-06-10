9a614502895ebe37a83574352c0d874a3e638a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 05:03:03 +0000

    Add package mateoperaciones/0.1.1/mateoperaciones-0.1.1.tar.gz

---
+++ packages/mateoperaciones/mateoperaciones-0.1.1.tar.gz/mateoperaciones-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setup(
+    name='mateoperaciones',
+    packages=find_packages(include=['mateoperaciones']),
+    version='0.1.1',
+    description="libreria de operaciones básicas",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author='Alexis Ruales',
+    license='MIT',
+    install_requires=[],
+    python_requires=">=3.10.12"
+)
