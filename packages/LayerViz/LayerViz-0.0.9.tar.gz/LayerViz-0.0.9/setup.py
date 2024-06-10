4b9c53af8d1bc4fe3ba1188f1444ece9fe7dccda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:03:56 +0000

    Add package LayerViz/0.0.9/LayerViz-0.0.9.tar.gz

---
+++ packages/LayerViz/LayerViz-0.0.9.tar.gz/LayerViz-0.0.9/setup.py
@@ -0,0 +1,34 @@
+
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+with open("README (1).md", "r") as fh:
+    long_description = fh.read()
+
+
+VERSION = '0.0.9'
+DESCRIPTION = 'A Python Library for Visualizing Keras Model that covers a variety of Layers'
+LONG_DESCRIPTION = long_description
+
+setup(
+    name="LayerViz",
+    version=VERSION,
+    author="Swajay Nandanwade",
+    author_email="swajaynandanwade@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',  # This is important if your README is in markdown
+    package_dir= {"": "libname"},
+    packages=find_packages(where = "libname"),
+    install_requires=['graphviz'],
+    keywords=['python', 'keras', 'visualize', 'models', 'layers'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
