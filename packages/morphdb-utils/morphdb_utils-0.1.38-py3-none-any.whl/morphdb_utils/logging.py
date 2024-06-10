ef5abb048197b304d4a91985eff36e872b817a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 15:04:35 +0000

    Add package morphdb-utils/0.1.38/morphdb_utils-0.1.38-py3-none-any.whl

---
+++ packages/morphdb-utils/morphdb_utils-0.1.38-py3-none-any.whl/morphdb_utils/logging.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json
+
+def render(data):
+    print(render_raw(data))
+
+
+def render_raw(data):
+    if isinstance(data, pd.DataFrame):
+        return json.dumps(convert_dataframe_to_render_format(data))
+    else:
+        return
+
+
+def convert_dataframe_to_render_format(df: pd.DataFrame):
+    data = json.loads(df.to_json(orient="records"))
+    headers = list(df.columns)
+    return {"records": data, "headers": headers}
