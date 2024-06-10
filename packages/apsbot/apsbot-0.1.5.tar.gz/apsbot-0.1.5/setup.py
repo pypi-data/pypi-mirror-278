8086ee0429f35f538154b3ff4d64ff62b536b2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 14:03:38 +0000

    Add package apsbot/0.1.5/apsbot-0.1.5.tar.gz

---
+++ packages/apsbot/apsbot-0.1.5.tar.gz/apsbot-0.1.5/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="apsbot",
+    version="0.1.5",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "aps-toolkit",
+        "pyperclip",
+        "tabulate",
+        "openai"
+    ],
+    entry_points={
+        "console_scripts": [
+            "apsbot=apsbot.cli:apsbot",
+        ],
+    },
+    author="chuongmep",
+    author_email="chuongpqvn@gmail.com",
+    description="A simple CLI tool to interact with the Autodesk Forge API.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chuongmep/aps-bot.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
