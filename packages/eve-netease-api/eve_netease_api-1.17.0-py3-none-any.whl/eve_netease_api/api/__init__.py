07ac502f87c14ffb4db83a50856190eee70e4bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-10 03:04:45 +0000

    Add package eve-netease-api/1.17.0/eve_netease_api-1.17.0-py3-none-any.whl

---
+++ packages/eve-netease-api/eve_netease_api-1.17.0-py3-none-any.whl/eve_netease_api/api/__init__.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from eve_netease_api.api.alliance_api import AllianceApi
+from eve_netease_api.api.assets_api import AssetsApi
+from eve_netease_api.api.bookmarks_api import BookmarksApi
+from eve_netease_api.api.calendar_api import CalendarApi
+from eve_netease_api.api.character_api import CharacterApi
+from eve_netease_api.api.clones_api import ClonesApi
+from eve_netease_api.api.contacts_api import ContactsApi
+from eve_netease_api.api.contracts_api import ContractsApi
+from eve_netease_api.api.corporation_api import CorporationApi
+from eve_netease_api.api.dogma_api import DogmaApi
+from eve_netease_api.api.faction_warfare_api import FactionWarfareApi
+from eve_netease_api.api.fittings_api import FittingsApi
+from eve_netease_api.api.fleets_api import FleetsApi
+from eve_netease_api.api.incursions_api import IncursionsApi
+from eve_netease_api.api.industry_api import IndustryApi
+from eve_netease_api.api.insurance_api import InsuranceApi
+from eve_netease_api.api.killmails_api import KillmailsApi
+from eve_netease_api.api.location_api import LocationApi
+from eve_netease_api.api.loyalty_api import LoyaltyApi
+from eve_netease_api.api.mail_api import MailApi
+from eve_netease_api.api.market_api import MarketApi
+from eve_netease_api.api.opportunities_api import OpportunitiesApi
+from eve_netease_api.api.planetary_interaction_api import PlanetaryInteractionApi
+from eve_netease_api.api.routes_api import RoutesApi
+from eve_netease_api.api.search_api import SearchApi
+from eve_netease_api.api.skills_api import SkillsApi
+from eve_netease_api.api.sovereignty_api import SovereigntyApi
+from eve_netease_api.api.status_api import StatusApi
+from eve_netease_api.api.universe_api import UniverseApi
+from eve_netease_api.api.user_interface_api import UserInterfaceApi
+from eve_netease_api.api.wallet_api import WalletApi
+from eve_netease_api.api.wars_api import WarsApi
