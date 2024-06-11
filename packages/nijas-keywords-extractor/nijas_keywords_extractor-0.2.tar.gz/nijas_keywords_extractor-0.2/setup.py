50e600b8c6536b11f439ff319709e647e51508a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:33 +0000

    Add package nijas-keywords-extractor/0.2/nijas_keywords_extractor-0.2.tar.gz

---
+++ packages/nijas-keywords-extractor/nijas_keywords_extractor-0.2.tar.gz/nijas_keywords_extractor-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2' 
+DESCRIPTION = 'kinyarwanda keywords extractor'
+LONG_DESCRIPTION = 'Packages for extracting keywords from kinyarwanda text'
+
+# Setting up
+setup(
+        name="nijas_keywords_extractor", 
+        version=VERSION,
+        author="Janvier Niyitegeka",
+        author_email="nijas2012@yahoo.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pandas','numpy','yake'],
+        requires_python = ">=3.10.9",
+        keywords=['python', 'kinyarwanda','keywords'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ]
+)
