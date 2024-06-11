886af05a1211fef1c556bbe15f8e29960fe05234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:03:55 +0000

    Add package demopythonlib/0.1.3/demopythonlib-0.1.3.tar.gz

---
+++ packages/demopythonlib/demopythonlib-0.1.3.tar.gz/demopythonlib-0.1.3/demopythonlib/__init__.py
@@ -0,0 +1,8 @@
+from .cmd import say_hi
+from .do_something import Calculator
+
+
+__all__ = [
+    'say_hi',
+    'Calculator',
+]
---
+++ packages/demopythonlib/demopythonlib-0.1.3.tar.gz/demopythonlib-0.1.3/demopythonlib/cmd.py
@@ -0,0 +1,7 @@
+
+
+def say_hi():
+    name = input("Enter your name: ")
+    print(f"Hello, {name}!")    
+
+    
