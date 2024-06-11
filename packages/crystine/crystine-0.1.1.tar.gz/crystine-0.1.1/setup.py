f15c92e6ac0c518ff1ac017497b4eaaacc0f6d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:44 +0000

    Add package crystine/0.1.1/crystine-0.1.1.tar.gz

---
+++ packages/crystine/crystine-0.1.1.tar.gz/crystine-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup , find_packages
+
+classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+
+
+setup(
+    name='crystine',
+    version='0.1.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'swap1=crystine.swap1:main',
+            'swap2=crystine.swap2:main',
+        ],
+    },
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scipy",
+        "matplotlib>=3.2.0",
+       
+    ],
+    author='Anand',
+    author_email='anandsr724@gmail.com',
+    description='data analysis for compuational chem',
+    python_requires='>=3.6',
+)
