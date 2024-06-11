8298709670822f44e8092fe238567d05652901a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:03:36 +0000

    Add package postgreslite/0.1.4/postgreslite-0.1.4.tar.gz

---
+++ packages/postgreslite/postgreslite-0.1.4.tar.gz/postgreslite-0.1.4/postgreslite/handler.py
@@ -0,0 +1,96 @@
+import sqlite3
+import asyncio
+
+from .pool import PoolConnection, AsyncPoolConnection
+from datetime import datetime, date
+
+__all__ = (
+    "PostgresLite",
+)
+
+
+class CustomCursor(sqlite3.Cursor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+def dict_factory(cursor: sqlite3.Cursor, row: list) -> dict:
+    """ Convert SQLite3 data to dict """
+    d = {}
+    for index, col in enumerate(cursor.description):
+        d[col[0]] = row[index]
+    return d
+
+
+class PostgresLite:
+    def __init__(
+        self,
+        filename: str = "storage.db",
+        loop: asyncio.AbstractEventLoop = None
+    ):
+        self._prepare_settings()
+        self._filename = filename
+
+        if filename != ":memory:":
+            if not filename.endswith(".db"):
+                raise ValueError("Database filename must end with '.db'")
+
+        self.loop = loop or asyncio.get_event_loop()
+
+    def connect(self) -> PoolConnection:
+        """ Makes a connection to the database and returns the pool (sync) """
+        conn = sqlite3.connect(
+            self._filename,
+            isolation_level=None,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+        conn.row_factory = dict_factory
+        db = conn.cursor()
+        return PoolConnection(db)
+
+    def connect_async(self) -> AsyncPoolConnection:
+        """ Makes a connection to the database and returns the pool (async) """
+        conn = sqlite3.connect(
+            self._filename,
+            isolation_level=None,
+            check_same_thread=False,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+        conn.row_factory = dict_factory
+        db = conn.cursor()
+        pool = AsyncPoolConnection(db, self.loop)
+        pool._task = self.loop.create_task(pool._queue_manager())
+
+        return pool
+
+    def _prepare_settings(self) -> None:
+        """ Prepare SQLite settings for better experience """
+
+        def adapt_date_iso(val) -> str:
+            """Adapt datetime.date to ISO 8601 date."""
+            return val.isoformat()
+
+        def adapt_datetime_iso(val) -> str:
+            """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
+            return val.isoformat()
+
+        sqlite3.register_adapter(date, adapt_date_iso)
+        sqlite3.register_adapter(datetime, adapt_datetime_iso)
+
+        def convert_date(val) -> date:
+            """Convert ISO 8601 date to datetime.date object."""
+            return date.fromisoformat(val.decode())
+
+        def convert_datetime(val) -> datetime:
+            """Convert ISO 8601 datetime to datetime.datetime object."""
+            return datetime.fromisoformat(val.decode())
+
+        def convert_timestamp(val) -> datetime:
+            """Convert Unix epoch timestamp to datetime.datetime object."""
+            return datetime.fromtimestamp(int(val))
+
+        sqlite3.register_converter("date", convert_date)
+        sqlite3.register_converter("datetime", convert_datetime)
+        sqlite3.register_converter("timestamp", convert_timestamp)
---
+++ packages/postgreslite/postgreslite-0.1.4.tar.gz/postgreslite-0.1.4/postgreslite/pool.py
@@ -0,0 +1,150 @@
+import sqlite3
+import asyncio
+
+from typing import Any
+
+__all__ = (
+    "PoolConnection",
+    "AsyncPoolConnection",
+    "SQLStatements",
+)
+
+
+class SQLStatements:
+    def __init__(self, *args: Any):
+        self._args = args
+
+    @property
+    def prepared(self) -> tuple:
+        """ Prepare statements for SQLite with *args provided from earlier """
+        arg_len = len(self._args)
+
+        if arg_len <= 0:
+            return ()
+        return self._args
+
+
+class PoolConnection:
+    def __init__(
+        self,
+        pool: sqlite3.Connection
+    ):
+        self._pool = pool
+
+    def _init_executor(self, query: str, *args: Any) -> sqlite3.Cursor:
+        """ Initialize SQL executor with args for 'Prepared Statements' """
+        prep = SQLStatements(*args)
+        data = self._pool.execute(query, prep.prepared)
+        return data
+
+    def execute(self, query: str, *args: Any) -> str:
+        """ Execute SQL command with args for 'Prepared Statements' """
+        data = self._init_executor(query, *args)
+
+        status_word = query.strip().split(" ")[0].upper()
+        status_code = data.rowcount if data.rowcount > 0 else 0
+        if status_word == "SELECT":
+            status_code = len(data.fetchall())
+
+        return f"{status_word} {status_code}"
+
+    def fetch(self, query: str, *args: Any) -> list:
+        """ Fetch DB data with args for 'Prepared Statements' """
+        return self._init_executor(query, *args).fetchall()
+
+    def fetchrow(self, query: str, *args: Any) -> dict:
+        """ Fetch DB row (one row only) with args for 'Prepared Statements' """
+        return self._init_executor(query, *args).fetchone()
+
+
+class AsyncPoolConnection(PoolConnection):
+    def __init__(
+        self,
+        pool: sqlite3.Connection,
+        loop: asyncio.AbstractEventLoop
+    ):
+        super().__init__(pool)
+        self.loop = loop
+
+        self._queue = asyncio.Queue()
+        self._task: asyncio.Task = None
+
+    async def _queue_manager(self):
+        while True:
+            query, args, future = await self._queue.get()
+
+            try:
+                await self._background_task(query, *args, future=future)
+            except Exception:
+                pass
+
+            self._queue.task_done()
+
+    async def _background_task(self, query: str, *args: Any, future: asyncio.Future) -> None:
+        prep = SQLStatements(*args)
+
+        try:
+            cursor: sqlite3.Cursor = await self.loop.run_in_executor(
+                None,
+                self._pool.execute,
+                query,
+                prep.prepared
+            )
+
+            future.set_result(cursor)
+        except Exception as e:
+            future.set_exception(e)
+
+    async def _init_executor(self, query: str, *args: Any) -> sqlite3.Cursor:
+        """ Initialize SQL executor with args for 'Prepared Statements' """
+        future = self.loop.create_future()
+        await self._queue.put((query, args, future))
+
+        try:
+            await future
+            return future.result()
+        except Exception:
+            raise future.exception()
+
+    @property
+    def queue_size(self) -> int:
+        """ Get the size of the queue """
+        return self._queue.qsize()
+
+    async def close(self) -> None:
+        """ Close the connection """
+        await self._queue.join()
+        self._task.cancel()
+        self._pool.close()
+
+    async def execute(self, query: str, *args: Any) -> str:
+        """ Execute SQL command with args for 'Prepared Statements' """
+        try:
+            data = await self._init_executor(query, *args)
+        except Exception as e:
+            raise e
+
+        status_word = query.strip().split(" ")[0].upper()
+        status_code = data.rowcount if data.rowcount > 0 else 0
+        if status_word == "SELECT":
+            status_code = len(data.fetchall())
+
+        return f"{status_word} {status_code}"
+
+    async def fetch(self, query: str, *args: Any) -> list:
+        """ Fetch DB data with args for 'Prepared Statements' """
+        try:
+            data = await self._init_executor(query, *args)
+        except Exception as e:
+            raise e
+
+        return data.fetchall()
+
+    async def fetchrow(self, query: str, *args: Any) -> dict:
+        """ Fetch DB row (one row only) with args for 'Prepared Statements' """
+        try:
+            data = await self._init_executor(query, *args)
+        except Exception as e:
+            raise e
+
+        return data.fetchone()
