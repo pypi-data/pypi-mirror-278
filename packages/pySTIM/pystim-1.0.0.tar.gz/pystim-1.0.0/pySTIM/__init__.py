d6891a6164142eeee1f1e114fca5a7fc0c373aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:03:47 +0000

    Add package pySTIM/1.0.0/pystim-1.0.0.tar.gz

---
+++ packages/pySTIM/pystim-1.0.0.tar.gz/pystim-1.0.0/pySTIM/__init__.py
@@ -0,0 +1,6 @@
+from ._processing import *
+from ._fileIO import *
+from ._sp_plot import * 
+from ._utils import *
+
+
---
+++ packages/pySTIM/pystim-1.0.0.tar.gz/pystim-1.0.0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+	name='pySTIM',
+	version='1.0.0',
+)
