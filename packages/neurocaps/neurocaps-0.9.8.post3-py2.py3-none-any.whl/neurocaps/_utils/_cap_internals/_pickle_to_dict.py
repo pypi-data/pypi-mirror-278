bb5087798d10819859e9b6a4d03fadf80f1d6eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:03:59 +0000

    Add package neurocaps/0.9.8.post3/neurocaps-0.9.8.post3-py2.py3-none-any.whl

---
+++ packages/neurocaps/neurocaps-0.9.8.post3-py2.py3-none-any.whl/neurocaps/_utils/_cap_internals/_pickle_to_dict.py
@@ -0,0 +1,7 @@
+import joblib
+
+def _convert_pickle_to_dict(pickle_file):
+    with open(pickle_file, "rb") as f:
+        subject_timeseries = joblib.load(f)
+        
+    return subject_timeseries
