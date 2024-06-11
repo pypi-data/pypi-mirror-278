fbed85489103190cc31a577b2c9e00d6f12d3555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:03:58 +0000

    Add package GeneralAgent/0.2.34/generalagent-0.2.34.tar.gz

---
+++ packages/GeneralAgent/generalagent-0.2.34.tar.gz/generalagent-0.2.34/GeneralAgent/interpreter/__init__.py
@@ -0,0 +1,6 @@
+from .interpreter import Interpreter
+from .role_interpreter import RoleInterpreter
+from .python_interpreter import PythonInterpreter
+from .knowlege_interpreter import KnowledgeInterperter
+from .applescript_interpreter import AppleScriptInterpreter
+from .shell_interpreter import ShellInterpreter
