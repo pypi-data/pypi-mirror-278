c0bc53ec2f963cc0a88f2347d5f5fa783d608d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:03:42 +0000

    Add package betterconsolemenu/0.1a0/betterconsolemenu-0.1a0.tar.gz

---
+++ packages/betterconsolemenu/betterconsolemenu-0.1a0.tar.gz/betterconsolemenu-0.1a0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="betterconsolemenu",
+    version="v0.1-alpha",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["bcm"],
+    url="https://github.com/mas6y6/betterconsolemenu",
+    description="Console menu but better and fancy",
+    download_url="https://github.com/mas6y6/betterconsolemenu/tree/main/dist"
+)
