5d876315feaf152e00b92f60009bb6f28b660df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:03:59 +0000

    Add package simplelogincmd/0.5.0/simplelogincmd-0.5.0.tar.gz

---
+++ packages/simplelogincmd/simplelogincmd-0.5.0.tar.gz/simplelogincmd-0.5.0/simplelogincmd/cli/commands/alias_commands/_get.py
@@ -0,0 +1,30 @@
+import click
+
+from simplelogincmd.cli import const, util
+from simplelogincmd.database.models import Alias
+
+
+def _get(id, include, exclude, header):
+    fields = util.output.get_display_fields_from_options(
+        const.ALIAS_FIELD_ORDER, include, exclude
+    )
+    if len(fields) == 0:
+        return
+    cfg = util.init.cfg()
+    sl = util.init.sl(cfg)
+    db = util.init.db(cfg)
+    id = util.input.resolve_id(db, Alias, id)
+    success, obj = sl.get_alias(id)
+    if not success:
+        click.echo(obj)
+        return None
+    db.session.upsert(obj)
+    db.session.commit()
+    if header is None:
+        header = cfg.get("display.headers")
+    util.output.display_model_list(
+        [obj],
+        fields,
+        pager_threshold=0,
+        header=header,
+    )
