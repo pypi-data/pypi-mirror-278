12c332a8f1d09d0c0df9185f8f09707fd97ec619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:03:35 +0000

    Add package test-nvf-builds/0.0.3.16/test_nvf_builds-0.0.3.16-py2.py3-none-any.whl

---
+++ packages/test-nvf-builds/test_nvf_builds-0.0.3.16-py2.py3-none-any.whl/oasysnow/federated/common/attestation_service/verification.py
@@ -0,0 +1,46 @@
+import json
+import jwt
+import requests
+
+
+
+class VerificationServer:
+    def __init__(self, audience: str, nonce:str, server_pk_hash: str):
+        self.audience = audience
+        self.nonce = nonce
+        self.server_pk_hash = server_pk_hash
+
+    def verify(self, report: str) -> bool:
+        try:
+            issuer_url = "https://confidentialcomputing.googleapis.com/.well-known/openid-configuration"
+            issuer = json.loads(requests.get(issuer_url).text)
+            jwks_uri = issuer['jwks_uri']
+            supported_algs = issuer['id_token_signing_alg_values_supported']
+            optional_custom_headers = {"User-agent": "custom-user-agent"}
+            supported_algs = issuer['id_token_signing_alg_values_supported']
+            jwks_client = jwt.PyJWKClient(jwks_uri, headers=optional_custom_headers)
+            signing_key = jwks_client.get_signing_key_from_jwt(report)
+            data = jwt.decode(
+                report,
+                signing_key.key,
+                algorithms=supported_algs,
+                audience=self.audience,
+            )
+            if data['eat_nonce'][0] != self.nonce:
+                raise Exception("Nonce mismatch error.")
+            if data['eat_nonce'][1] != self.server_pk_hash:
+                raise Exception("Server public key digest mismatch error.")
+            
+            print(data)
+            print('>>>>>> Attestation SUCCEEDED')
+            return True
+        except Exception as e:
+            print('>>>>>> Attestation FAILED')
+            print(e)
+            return False
+
+
+
+
+
+
