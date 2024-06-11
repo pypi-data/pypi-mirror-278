1dc241794018fff878583d08ccd33de96f549c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:19 +0000

    Add package aleph-message/0.4.6/aleph-message-0.4.6.tar.gz

---
+++ packages/aleph-message/aleph-message-0.4.6.tar.gz/aleph-message-0.4.6/aleph_message/models/execution/__init__.py
@@ -0,0 +1,5 @@
+from .abstract import BaseExecutableContent
+from .instance import InstanceContent
+from .program import ProgramContent
+
+__all__ = ["BaseExecutableContent", "InstanceContent", "ProgramContent"]
