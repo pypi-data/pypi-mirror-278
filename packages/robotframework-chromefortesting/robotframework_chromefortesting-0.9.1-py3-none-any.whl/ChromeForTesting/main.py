436926158ec2cbcd292f9dea562a4919b0ea5d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 16:05:21 +0000

    Add package robotframework-chromefortesting/0.9.1/robotframework_chromefortesting-0.9.1-py3-none-any.whl

---
+++ packages/robotframework-chromefortesting/robotframework_chromefortesting-0.9.1-py3-none-any.whl/ChromeForTesting/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+'''
+Chrome for Testing (CfT) for Robot Framework
+
+*** Settings ***
+Library    ChromeForTesting    ${channel}=stable    ${headless}=False
+
+GitHub: https://github.com/extraorange/robotframework-chromefortesting
+
+Disclaimer: Distributed as-is, without warranties or guarantees.
+
+Author: extraorange
+Date: 13 Nov 2023
+Version: 0.9.1
+License: GNU General Public License v3.0
+'''
+
+from .chromelabs import download_assets, load_local_assets, update_assets
+from .config import Config, State
+
+
+class ChromeForTesting:
+    def __init__(self, channel: str = "stable", headless: bool = False):
+
+        config = Config(channel, headless)
+        if config.state is State.LIVE:
+            pass
+        elif config.state in [State.INITIAL, State.NEWCHANNEL]:
+            assets = download_assets(config)
+            config.write(assets)
+            assets.expose_to_system()
+        elif config.state in [State.UPDATE, State.REPAIR]:
+            assets = update_assets(config)
+            config.write(assets)
+            assets.expose_to_system()
+        elif config.state is State.LATEST:
+            assets = load_local_assets(config)
+            assets.expose_to_system()
