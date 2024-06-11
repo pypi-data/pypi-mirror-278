17f89d9763951eee94d73edbce6719e1b972b156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:26 +0000

    Add package cg/60.9.6/cg-60.9.6.tar.gz

---
+++ packages/cg/cg-60.9.6.tar.gz/cg-60.9.6/cg/cli/demultiplex/base.py
@@ -0,0 +1,36 @@
+"""Module for demultiplexing CLI."""
+
+import logging
+
+import click
+
+from cg.cli.demultiplex.demux import (
+    confirm_flow_cell_sync,
+    copy_novaseqx_flow_cells,
+    create_manifest_files,
+    demultiplex_all,
+    demultiplex_flow_cell,
+)
+from cg.cli.demultiplex.finish import finish_group
+from cg.cli.demultiplex.sample_sheet import sample_sheet_commands
+
+LOG = logging.getLogger(__name__)
+
+
+@click.group(name="demultiplex")
+def demultiplex_cmd_group():
+    """Command group for the demultiplex CLI."""
+    LOG.info("Running cg demultiplex.")
+
+
+demultiplex_cmd_group: click.Group
+for sub_cmd in [
+    create_manifest_files,
+    confirm_flow_cell_sync,
+    demultiplex_flow_cell,
+    demultiplex_all,
+    finish_group,
+    copy_novaseqx_flow_cells,
+    sample_sheet_commands,
+]:
+    demultiplex_cmd_group.add_command(sub_cmd)
---
+++ packages/cg/cg-60.9.6.tar.gz/cg-60.9.6/cg/cli/upload/coverage.py
@@ -0,0 +1,40 @@
+"""Code for uploading coverage reports via CLI"""
+
+import click
+
+from cg.meta.upload.coverage import UploadCoverageApi
+from cg.models.cg_config import CGConfig
+from cg.store.models import Case
+from cg.store.store import Store
+
+from .utils import suggest_cases_to_upload
+
+
+@click.command("coverage")
+@click.option(
+    "-r",
+    "--re-upload",
+    is_flag=True,
+    help="re-upload existing analysis",
+)
+@click.argument("family_id", required=False)
+@click.pass_obj
+def upload_coverage(context: CGConfig, re_upload, family_id):
+    """Upload coverage from an analysis to Chanjo."""
+
+    click.echo(click.style("----------------- COVERAGE --------------------"))
+
+    status_db: Store = context.status_db
+
+    if not family_id:
+        suggest_cases_to_upload(status_db=status_db)
+        raise click.Abort
+
+    case_obj: Case = status_db.get_case_by_internal_id(internal_id=family_id)
+    upload_coverage_api = UploadCoverageApi(
+        status_api=status_db,
+        hk_api=context.housekeeper_api,
+        chanjo_api=context.chanjo_api,
+    )
+    coverage_data = upload_coverage_api.data(case_obj.analyses[0])
+    upload_coverage_api.upload(coverage_data, replace=re_upload)
