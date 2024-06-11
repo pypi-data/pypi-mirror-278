158d1f77110da7bdf47d756a6ec795cd822d1472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:03:49 +0000

    Add package PyDiscoBot/1.0.1/pydiscobot-1.0.1.tar.gz

---
+++ packages/PyDiscoBot/pydiscobot-1.0.1.tar.gz/pydiscobot-1.0.1/PyDiscoBot/PyDiscoBot/err.py
@@ -0,0 +1,22 @@
+from typing import Callable
+
+err_callback: Callable = None
+
+
+async def err(message: str | Exception) -> None:
+    """ Helper function to send error or notification messages to notify channel with a single parameter.\n
+        **If a notification channel does not exist**, the notification is printed to console instead\n
+        **param message**: message to report\n
+        **returns**: None\n
+        """
+    global err_callback
+    if not message:
+        return
+    if not err_callback:
+        return print(message)
+    await err_callback(message)
+
+
+def register_callback(callback: Callable):
+    global err_callback
+    err_callback = callback
