ae97eda3ca495d79f3180286dc8cb5500836af29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 17:04:20 +0000

    Add package rtms-sdk/0.1.15/rtms_sdk-0.1.15.tar.gz

---
+++ packages/rtms-sdk/rtms_sdk-0.1.15.tar.gz/rtms_sdk-0.1.15/rtms_sdk_module/Exception/CRNWarningException.py
@@ -0,0 +1,2 @@
+class CRNWarningException(Exception):  # 堆垛机故障 不可重试
+    pass
---
+++ packages/rtms-sdk/rtms_sdk-0.1.15.tar.gz/rtms_sdk-0.1.15/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='rtms_sdk',
+    version='0.1.15',
+    description='plc connect',
+    author='Bao Yun',
+    author_email='305713412@qq.com',
+    packages=find_packages(),
+    install_requires=[
+
+    ]
+)
