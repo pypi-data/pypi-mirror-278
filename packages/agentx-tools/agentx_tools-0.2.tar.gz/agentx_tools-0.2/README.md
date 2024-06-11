a7c87fd4b7e2a146816f41391a9a47a0edc1c6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:03:26 +0000

    Add package agentx-tools/0.2/agentx_tools-0.2.tar.gz

---
+++ packages/agentx-tools/agentx_tools-0.2.tar.gz/agentx_tools-0.2/README.md
@@ -0,0 +1,21 @@
+![AgentX Logo](agentx/resources/logo.png)
+
+# AgentX
+
+AgentX is a comprehensive toolkit designed for use with Large Language Models (LLMs). It provides a variety of tools that can be utilized by LLM agents to perform a wide range of tasks, from date manipulation to statistical analysis, web scraping, and internet searching.
+
+## Features
+
+- **Date Tools**: Perform various date-related operations such as getting the current date, calculating the number of days between dates, and converting dates to different formats.
+- **RAG Search Tools**: Search through text files for specific medical codes or other information.
+- **Scraping Tools**: Scrape data from websites.
+- **Search Tools**: Search the internet for information on various topics.
+- **Statistical Tools**: Perform statistical calculations such as mean, median, standard deviation, and more.
+
+## Installation
+
+To install AgentX, run the following command:
+```
+pip install agentx
+```
+
---
+++ packages/agentx-tools/agentx_tools-0.2.tar.gz/agentx_tools-0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agentx-tools',
+    version='0.2',
+    packages=find_packages(),
+    description='A collection of tools for Large Language Model Agents',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/Tylerbryy/agentx',
+    author='Tyler Gibbs',
+    author_email='tylergibbs048@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        'numpy==1.26.4',
+        'langchain-core>=0.1.52,<0.2.0',  
+        'crewai-tools>=0.2.0,<=0.2.6',
+        'langchain-community>=0.0.38,<0.1.0',  
+        'langchain>=0.1.4,<0.2.0',  
+    ],
+)
