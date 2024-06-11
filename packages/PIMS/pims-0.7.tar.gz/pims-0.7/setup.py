f92246b4d8cd73a56af631155ed1118574405ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:46 +0000

    Add package PIMS/0.7/pims-0.7.tar.gz

---
+++ packages/PIMS/pims-0.7.tar.gz/pims-0.7/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+import versioneer
+
+
+try:
+    descr = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+except IOError:
+    descr = ''
+
+setup_parameters = dict(
+    name="PIMS",
+    license="BSD-3-clause",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Python Image Sequence",
+    author="PIMS Contributors",
+    python_requires=">=3.7",
+    install_requires=[
+        'imageio',
+        'numpy>=1.19',
+        'packaging',
+        'slicerator>=0.9.8',
+        'tifffile',
+    ],
+    author_email="dallan@pha.jhu.edu",
+    url="https://github.com/soft-matter/pims",
+    packages=["pims", "pims.utils", "pims.tests"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    long_description=descr,
+    long_description_content_type='text/markdown',
+)
+
+setup(**setup_parameters)
