0087ace3307be98702c6d0ba22ec2a61de2301fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:04:00 +0000

    Add package pkg-support-resistance/0.3.1/pkg_support_resistance-0.3.1.tar.gz

---
+++ packages/pkg-support-resistance/pkg_support_resistance-0.3.1.tar.gz/pkg_support_resistance-0.3.1/tests/expected_output_test.py
@@ -0,0 +1,134 @@
+"""Expected output test"""
+
+vanilla_sr_exec_pipeline_expected_output_test = [
+    {
+        "pivotPrice": 48184.0,
+        "limitsUp": 48495.0,
+        "limitsDown": 47347.53,
+        "score": 67,
+        "accumulatedVolume": 44687.15635,
+    },
+    {
+        "pivotPrice": 47534.34,
+        "limitsUp": 47874.98,
+        "limitsDown": 46763.68,
+        "score": 41,
+        "accumulatedVolume": 23864.294279999995,
+    },
+    {
+        "pivotPrice": 45000.0,
+        "limitsUp": 45000.0,
+        "limitsDown": 44700.0,
+        "score": 8,
+        "accumulatedVolume": 6010.54166,
+    },
+    {
+        "pivotPrice": 43996.5,
+        "limitsUp": 44141.37,
+        "limitsDown": 43100.0,
+        "score": 40,
+        "accumulatedVolume": 17909.42211,
+    },
+    {
+        "pivotPrice": 43473.45,
+        "limitsUp": 43580.0,
+        "limitsDown": 42697.01,
+        "score": 37,
+        "accumulatedVolume": 18501.36496,
+    },
+    {
+        "pivotPrice": 42819.86,
+        "limitsUp": 42850.0,
+        "limitsDown": 42041.7,
+        "score": 15,
+        "accumulatedVolume": 6355.429050000001,
+    },
+    {
+        "pivotPrice": 42259.58,
+        "limitsUp": 42787.38,
+        "limitsDown": 42017.33,
+        "score": 24,
+        "accumulatedVolume": 12506.830009999998,
+    },
+    {
+        "pivotPrice": 41619.99,
+        "limitsUp": 42365.48,
+        "limitsDown": 41115.0,
+        "score": 91,
+        "accumulatedVolume": 42944.990900000004,
+    },
+    {
+        "pivotPrice": 41071.29,
+        "limitsUp": 41157.26,
+        "limitsDown": 40753.88,
+        "score": 8,
+        "accumulatedVolume": 4474.11918,
+    },
+]
+
+
+kmeans_sr_exec_pipeline_expected_output_test = [
+    {
+        "pivotPrice": 41247.07,
+        "limitsDown": 40965.2125,
+        "limitsUp": 41334.82750000001,
+        "score": 25,
+        "accumulatedVolume": 11484.42265,
+    },
+    {
+        "pivotPrice": 41541.4925,
+        "limitsDown": 41350.675,
+        "limitsUp": 41683.4625,
+        "score": 44,
+        "accumulatedVolume": 16764.66682,
+    },
+    {
+        "pivotPrice": 41740.695,
+        "limitsDown": 41685.535,
+        "limitsUp": 42090.5675,
+        "score": 54,
+        "accumulatedVolume": 25677.978769999998,
+    },
+    {
+        "pivotPrice": 42616.125,
+        "limitsDown": 42105.2425,
+        "limitsUp": 42692.68749999999,
+        "score": 68,
+        "accumulatedVolume": 28562.038430000004,
+    },
+    {
+        "pivotPrice": 42939.847499999996,
+        "limitsDown": 42811.5175,
+        "limitsUp": 43384.869999999995,
+        "score": 65,
+        "accumulatedVolume": 29510.197329999995,
+    },
+    {
+        "pivotPrice": 43669.6875,
+        "limitsDown": 43419.9975,
+        "limitsUp": 44002.247500000005,
+        "score": 64,
+        "accumulatedVolume": 25561.89504,
+    },
+    {
+        "pivotPrice": 45788.325,
+        "limitsDown": 44330.0825,
+        "limitsUp": 45788.325,
+        "score": 13,
+        "accumulatedVolume": 12449.31098,
+    },
+    {
+        "pivotPrice": 46947.585,
+        "limitsDown": 46947.585,
+        "limitsUp": 47633.6625,
+        "score": 55,
+        "accumulatedVolume": 40150.74441,
+    },
+    {
+        "pivotPrice": 47886.935000000005,
+        "limitsDown": 47651.009999999995,
+        "limitsUp": 48355.61749999999,
+        "score": 111,
+        "accumulatedVolume": 39565.819639999994,
+    },
+]
