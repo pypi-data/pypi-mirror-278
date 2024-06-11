e801b35b7cd43470d3db53b7f0a8cf6c402e168a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:03:32 +0000

    Add package jisa-stubs/1.1/jisa-stubs-1.1.tar.gz

---
+++ packages/jisa-stubs/jisa-stubs-1.1.tar.gz/jisa-stubs-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='jisa-stubs',
+    version='1.1',
+    description='JISA Python wrapper stubs',
+    url='https://github.com/OE-FET/JISA.git',
+    author='William Wood',
+    author_email='waw31@cam.ac.uk',
+    license='unlicense',
+    packages=['jisa-stubs'],
+    include_package_data=True,
+    zip_safe=False
+)
