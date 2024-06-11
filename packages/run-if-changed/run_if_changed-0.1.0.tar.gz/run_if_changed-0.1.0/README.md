26dddfb57fffb0a5c7c771303e3c2b6b3a92c609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:44 +0000

    Add package run-if-changed/0.1.0/run_if_changed-0.1.0.tar.gz

---
+++ packages/run-if-changed/run_if_changed-0.1.0.tar.gz/run_if_changed-0.1.0/README.md
@@ -0,0 +1,32 @@
+# `run-if` conditionally run command if targets don't exist or dependencies have changed.
+
+This is a simple python script that bascially does what checkexec (https://github.com/kurtbuilds/checkexec), but it uses a hash
+of the contents of the dependencies to decide if the command should be ran and supports multiple targets.
+
+```bash
+$ run-if main.cpp == g++ main.pp -o main == main
+```
+
+If `main` does not exist, or if the contents of `main.cpp` have changed since the last time it `run-if` was called,
+the command will be run.
+
+The syntax is different than checkexec
+```bash
+$ run-if [DEPENDENCY...] == <COMMAND> == [TARGET...]
+```
+
+Multiple targets can be listed and both targets and dependencies can be files or directories.
+
+```bash
+$ run-if -- src/ == cmake --build build == build/test1 build/test2 build/data/
+```
+
+Currently the hash of dependencies are being computed with shell commands using the `subprocess` module, so it will fail to run
+if `md5sum` or `gawk` are missing, or if the default shell does not support pipes.
+
+
+# Install
+
+```bash
+$ pip install run-if
+```
---
+++ packages/run-if-changed/run_if_changed-0.1.0.tar.gz/run_if_changed-0.1.0/run_if_changed/cli.py
@@ -0,0 +1,81 @@
+import argparse
+import json
+import pathlib
+import subprocess
+import typing
+
+import rich
+import typer
+
+app = typer.Typer()
+console = rich.console.Console()
+
+
+CMD_SEP = "=="  # unfortunately, using -> causes problems with the shells...
+DB_NAME = ".run-if.json"
+
+
+def compute_hash(path: pathlib.Path):
+    if path.is_file():
+        return (
+            subprocess.check_output(f"md5sum {path}", shell=True)
+            .decode()
+            .split()[0]
+            .strip()
+        )
+    if path.is_dir():
+        return (
+            subprocess.check_output(
+                f"find {path} -type f | xargs md5sum | gawk '{{print $1}}' | md5sum",
+                shell=True,
+            )
+            .decode()
+            .split()[0]
+            .strip()
+        )
+
+    print(f"Cannot compute hash for '{path}'. It is not a file or directory.")
+    raise typer.Exit(3)
+
+
+@app.command()
+def run_if(arguments: typing.List[str]):
+    dependencies_command_targets = [[], [], []]
+
+    DB_PATH = pathlib.Path(DB_NAME)
+    if not DB_PATH.exists():
+        DB_PATH.write_text("{}")
+
+    current_type = 0
+    for a in arguments:
+        if a.strip() == CMD_SEP:
+            current_type += 1
+            if current_type > 2:
+                print(
+                    f"Too many '{CMD_SEP}' found in argument list. There should only be two."
+                )
+                raise typer.Exit(2)
+            continue
+        dependencies_command_targets[current_type].append(a)
+
+    run_command = False
+    for t in [pathlib.Path(a) for a in dependencies_command_targets[2]]:
+        if not t.exists():
+            run_command = True
+            break
+
+    dep_hashes = json.loads(DB_PATH.read_text())
+    for dep in [pathlib.Path(a) for a in dependencies_command_targets[0]]:
+        _hash = compute_hash(dep)
+
+        if dep_hashes.get(str(dep), None) != _hash:
+            run_command = True
+        dep_hashes[str(dep)] = _hash
+
+    DB_PATH.write_text(json.dumps(dep_hashes))
+
+    if run_command:
+        subprocess.run(dependencies_command_targets[1])
+        raise typer.Exit(0)
+
+    raise typer.Exit(1)
