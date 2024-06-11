9ca01a089cf53e615fae685b8a28c63daeb78ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:32 +0000

    Add package jaxsim/0.2.1.dev123/jaxsim-0.2.1.dev123.tar.gz

---
+++ packages/jaxsim/jaxsim-0.2.1.dev123.tar.gz/jaxsim-0.2.1.dev123/src/jaxsim/parsers/descriptions/collision.py
@@ -0,0 +1,121 @@
+import abc
+import dataclasses
+from typing import List
+
+import jax.numpy as jnp
+import numpy as np
+import numpy.typing as npt
+
+from jaxsim import logging
+
+from .link import LinkDescription
+
+
+@dataclasses.dataclass
+class CollidablePoint:
+    """
+    Represents a collidable point associated with a parent link.
+
+    Attributes:
+        parent_link (LinkDescription): The parent link to which the collidable point is attached.
+        position (npt.NDArray): The position of the collidable point relative to the parent link.
+        enabled (bool): A flag indicating whether the collidable point is enabled for collision detection.
+
+    """
+
+    parent_link: LinkDescription
+    position: npt.NDArray = dataclasses.field(default_factory=lambda: np.zeros(3))
+    enabled: bool = True
+
+    def change_link(
+        self, new_link: LinkDescription, new_H_old: npt.NDArray
+    ) -> "CollidablePoint":
+        """
+        Move the collidable point to a new parent link.
+
+        Args:
+            new_link (LinkDescription): The new parent link to which the collidable point is moved.
+            new_H_old (npt.NDArray): The transformation matrix from the new link's frame to the old link's frame.
+
+        Returns:
+            CollidablePoint: A new collidable point associated with the new parent link.
+
+        """
+        msg = f"Moving collidable point: {self.parent_link.name} -> {new_link.name}"
+        logging.debug(msg=msg)
+
+        return CollidablePoint(
+            parent_link=new_link,
+            position=(new_H_old @ jnp.hstack([self.position, 1.0])).squeeze()[0:3],
+            enabled=self.enabled,
+        )
+
+    def __eq__(self, other):
+        retval = (
+            self.parent_link == other.parent_link
+            and (self.position == other.position).all()
+            and self.enabled == other.enabled
+        )
+        return retval
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}("
+            + f"parent_link={self.parent_link.name}"
+            + f", position={self.position}"
+            + f", enabled={self.enabled}"
+            + ")"
+        )
+
+
+@dataclasses.dataclass
+class CollisionShape(abc.ABC):
+    """
+    Abstract base class for representing collision shapes.
+
+    Attributes:
+        collidable_points (List[CollidablePoint]): A list of collidable points associated with the collision shape.
+
+    """
+
+    collidable_points: List[CollidablePoint]
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}("
+            + "collidable_points=[\n    "
+            + ",\n    ".join(str(cp) for cp in self.collidable_points)
+            + "\n])"
+        )
+
+
+@dataclasses.dataclass
+class BoxCollision(CollisionShape):
+    """
+    Represents a box-shaped collision shape.
+
+    Attributes:
+        center (npt.NDArray): The center of the box in the local frame of the collision shape.
+
+    """
+
+    center: npt.NDArray
+
+    def __eq__(self, other):
+        return (self.center == other.center).all() and super().__eq__(other)
+
+
+@dataclasses.dataclass
+class SphereCollision(CollisionShape):
+    """
+    Represents a spherical collision shape.
+
+    Attributes:
+        center (npt.NDArray): The center of the sphere in the local frame of the collision shape.
+
+    """
+
+    center: npt.NDArray
+
+    def __eq__(self, other):
+        return (self.center == other.center).all() and super().__eq__(other)
---
+++ packages/jaxsim/jaxsim-0.2.1.dev123.tar.gz/jaxsim-0.2.1.dev123/src/jaxsim/typing.py
@@ -0,0 +1,39 @@
+from typing import Any, Hashable
+
+import jax
+
+# =========
+# JAX types
+# =========
+
+ScalarJax = jax.Array
+IntJax = ScalarJax
+BoolJax = ScalarJax
+FloatJax = ScalarJax
+
+ArrayJax = jax.Array
+VectorJax = ArrayJax
+MatrixJax = ArrayJax
+
+PyTree = (
+    dict[Hashable, "PyTree"] | list["PyTree"] | tuple["PyTree"] | None | jax.Array | Any
+)
+
+# =======================
+# Mixed JAX / NumPy types
+# =======================
+
+Array = jax.typing.ArrayLike
+Vector = Array
+Matrix = Array
+
+Int = int | IntJax
+Bool = bool | ArrayJax
+Float = float | FloatJax
+
+ArrayLike = Array
+VectorLike = Vector
+MatrixLike = Matrix
+IntLike = Int
+BoolLike = Bool
+FloatLike = Float
