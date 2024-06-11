16209c71b9c7e36d3c714cb623b6fef8a8d63274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 16:04:54 +0000

    Add package pyldk/1.1.7/pyldk-1.1.7-py3-none-win_amd64.whl

---
+++ packages/pyldk/pyldk-1.1.7-py3-none-win_amd64.whl/pyldk/pyldk.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File     : pyldk.py
+# @Author   : jade
+# @Date     : 2023/3/16 16:29
+# @Email    : jadehh@1ive.com
+# @Software : Samples
+# @Desc     :
+from pyldk.hasp_adapter import *
+import platform
+class PyLdk(object):
+    def __init__(self,JadeLog=None):
+        self.JadeLog = JadeLog
+        if getOperationSystem() == "Windows":
+            if (platform.architecture()[0]) == "64bit":
+                libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib/{}/x64'.format(getOperationSystem())))
+            else:
+                libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib/{}/x86'.format(getOperationSystem())))
+
+        else:
+            libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib/{}/{}'.format(getOperationSystem(),self.get_system_arch())))
+        try:
+            self.adapter = BaseAdapter(os.path.join(libs_dir,os.listdir(libs_dir)[0]),JadeLog)
+        except:
+            self.log("加密狗初始化失败,失败原因为:加密狗文件被删除")
+            Exit(-1)
+
+    def get_system_arch(self):
+        return (platform.uname().processor)
+
+    def log(self, msg, log_level="ERROR"):
+        if self.JadeLog:
+            if log_level == "INFO":
+                self.JadeLog.INFO(msg)
+            elif log_level == "ERROR":
+                self.JadeLog.ERROR(msg)
+        else:
+            print(msg)
+
+    def get_feature_id(self):
+        feature_id = self.adapter.get_info()
+        return feature_id
+
+    def login(self,feature_id=None,is_mutiple_feature_id=False):
+        ## 首先判断加密狗是否存在
+        login_status = False
+        if feature_id is None:
+            feature_id = 0
+        haspStruct = self.adapter.login(feature_id)
+        if haspStruct.status == 0:
+            self.adapter.logout(haspStruct.handle)
+            if feature_id == 0:
+                feature_id = self.get_feature_id()
+            haspStruct = self.adapter.login(feature_id)
+            if haspStruct.status == 0:
+                if feature_id > 0:
+                    login_status = True
+                else:
+                    self.adapter.log("加密狗初始化失败,请检查加密狗的授权,或者重新授权")
+                    self.adapter.logout(haspStruct.handle)
+            else:
+                self.adapter.log("加密狗初始化失败",haspStruct.status)
+        else:
+            if is_mutiple_feature_id is False:
+                self.adapter.show_staus("加密狗初始化失败", haspStruct.status,feature_id)
+        return haspStruct,feature_id,login_status
+
+
+    def get_ldk(self,feature_id):
+        ldk_status = False
+        lower_users = self.adapter.bool_lower_users(feature_id)
+        if lower_users:
+            ldk_status = True
+        return ldk_status
