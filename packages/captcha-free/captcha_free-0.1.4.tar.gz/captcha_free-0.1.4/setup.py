d4ae0be3e857602b33b3b059f4bab667a5a944d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:04:03 +0000

    Add package captcha-free/0.1.4/captcha_free-0.1.4.tar.gz

---
+++ packages/captcha-free/captcha_free-0.1.4.tar.gz/captcha_free-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="captcha_free",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "openai-whisper",
+        "requests"
+    ],
+    description="A Selenium WebDriver wrapper that bypasses reCAPTCHA using OpenAI Whisper",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/theriley106/captcha_free",
+    author="theriley106",
+    author_email="christopherlambert106@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
