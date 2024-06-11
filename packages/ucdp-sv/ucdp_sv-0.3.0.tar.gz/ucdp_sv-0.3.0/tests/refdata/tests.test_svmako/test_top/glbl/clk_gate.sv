e1c8771972a91d7dc52fdad41bdc07886ea31cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:04:29 +0000

    Add package ucdp-sv/0.3.0/ucdp_sv-0.3.0.tar.gz

---
+++ packages/ucdp-sv/ucdp_sv-0.3.0.tar.gz/ucdp_sv-0.3.0/tests/refdata/tests.test_svmako/test_top/glbl/clk_gate.sv
@@ -0,0 +1,28 @@
+// GENERATE INPLACE BEGIN fileheader() =========================================
+//
+// Module:     glbl.clk_gate
+// Data Model: glbl.clk_gate.ClkGateMod
+//
+//
+// GENERATE INPLACE END fileheader =============================================
+
+// GENERATE INPLACE BEGIN header() =============================================
+// GENERATE INPLACE END header =================================================
+
+// GENERATE INPLACE BEGIN beginmod() ===========================================
+module clk_gate ( // glbl.clk_gate.ClkGateMod
+  input  wire  clk_i,
+  output logic clk_o,
+  input  wire  ena_i
+);
+// GENERATE INPLACE END beginmod ===============================================
+
+    // GENERATE INPLACE BEGIN logic() ==========================================
+    // GENERATE INPLACE END logic ==============================================
+
+// GENERATE INPLACE BEGIN endmod() =============================================
+endmodule // clk_gate
+// GENERATE INPLACE END endmod =================================================
+
+// GENERATE INPLACE BEGIN footer() =============================================
+// GENERATE INPLACE END footer =================================================
