7834e64115e3c99a323ecaff020f4e17ae4e04af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:04:22 +0000

    Add package rack/0.1.2/rack-0.1.2-py3-none-any.whl

---
+++ packages/rack/rack-0.1.2-py3-none-any.whl/rack/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024 Jérôme Carretero <cJ-rack@zougloub.eu> & contributors
+# SPDX-License-Identifier: MIT
+
+"""Persistent (LMDB-backed) data structures mimicking as Python native containers"""
+
+__version__ = "0.1.2"
