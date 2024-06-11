4cffdf53f324d26e18e64299a9e7b08833bf1750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:58 +0000

    Add package xontrib-macro/0.4.2/xontrib_macro-0.4.2-py3-none-any.whl

---
+++ packages/xontrib-macro/xontrib_macro-0.4.2-py3-none-any.whl/xontrib/macro/signal.py
@@ -0,0 +1,19 @@
+import sys
+import signal
+
+class DisableInterrupt:
+    def __enter__(self):
+        self.handler_called = False
+        self.original_handler = signal.getsignal(signal.SIGINT)
+
+        def handler(signum, frame):
+            self.handler_called = True
+            print("KeyboardInterrupt will be raised at the end of current transaction.", file=sys.stderr)
+
+        signal.signal(signal.SIGINT, handler)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        signal.signal(signal.SIGINT, self.original_handler)
+        if self.handler_called:
+            raise KeyboardInterrupt("KeyboardInterrupt was received during transaction.")
