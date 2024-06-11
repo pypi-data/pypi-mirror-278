a6e5e73ace5dd551dd3a426c92ccfef8e8a4838d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:03:49 +0000

    Add package http-status-example/1.0.0/http_status_example-1.0.0.tar.gz

---
+++ packages/http-status-example/http_status_example-1.0.0.tar.gz/http_status_example-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md", "r").read()
+
+setup(
+    name="http_status_example",
+    version="1.0.0",
+    description="prints the HTTP status code of either the default url or the url provided by the user",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Russ Nastala",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=["requests~=2.32"],
+    python_requires="~=3.8",
+)
