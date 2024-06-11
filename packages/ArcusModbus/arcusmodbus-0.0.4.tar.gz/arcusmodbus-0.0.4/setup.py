09a2256135b21a6f479add8ebb7c4f47f93f65eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:55 +0000

    Add package ArcusModbus/0.0.4/arcusmodbus-0.0.4.tar.gz

---
+++ packages/ArcusModbus/arcusmodbus-0.0.4.tar.gz/arcusmodbus-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Titan IMX servo controller'
+LONG_DESCRIPTION = 'Package for controlling Arcus Titan IMX servos over Ethernet using Modbus TCP'
+
+
+setup(
+    name ="ArcusModbus",
+    version=VERSION,
+    author="Daniel Curtis",
+    author_email = "dwc00012@mix.wvu.edu",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(include=["ArcusModbus", "ArcusModbus.*"]),
+    py_modules=['ArcusModbus'],
+    install_requires=['pymodbus'],
+    keywords=['python','Arcus Titan IMX'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3"
+    ]
+)
