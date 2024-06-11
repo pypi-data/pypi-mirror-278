ae29f95daeea20fed34c7d0c517a7992757fa34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:22 +0000

    Add package EDAExcelReport/0.1.0/EDAExcelReport-0.1.0.tar.gz

---
+++ packages/EDAExcelReport/EDAExcelReport-0.1.0.tar.gz/EDAExcelReport-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="EDAExcelReport",  
+    version="0.1.0",  
+    author="Rohit Kosamkar",
+    author_email="rohitkosamkar97@gmail.com",
+    description="A package for generating EDA reports",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rohit180497/eda-excel-report",  
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pandas",  
+        "openpyxl",
+        "numpy",
+        "scikit-learn",
+        "datetime"
+    ],
+)
