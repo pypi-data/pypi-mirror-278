e13e17ce23e6acf658c86f0f47046262e9a65546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:50 +0000

    Add package mr-keras-utils/0.1.1/mr_keras_utils-0.1.1.tar.gz

---
+++ packages/mr-keras-utils/mr_keras_utils-0.1.1.tar.gz/mr_keras_utils-0.1.1/src/keras_utils/metrics.py
@@ -0,0 +1,34 @@
+import keras
+from keras import ops
+
+class MetricEveryN(keras.metrics.Metric):
+  """A metric that only runs every `n` batches"""
+  def __init__(self, metric_fn, name='custom_metric', n=10, **kwargs):
+    super().__init__(name=name, **kwargs)
+    self.metric_fn = metric_fn
+    self.n = n
+    self.total = self.add_weight(name='total', initializer='zeros')
+    self.count = self.add_weight(name='count', initializer='zeros')
+    self.batch_counter = self.add_weight(name='batch_counter', initializer='zeros')
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    result = ops.cond(
+      ops.equal(self.batch_counter % self.n, 0),
+      lambda: ops.mean(self.metric_fn(y_true, y_pred)),
+      lambda: 0.0
+    )
+    count = ops.cond(
+      ops.equal(self.batch_counter % self.n, 0),
+      lambda: 1, lambda: 0
+    )
+    self.total.assign_add(result)
+    self.count.assign_add(count)
+    self.batch_counter.assign_add(1)
+
+  def result(self):
+    return self.total / self.count
+
+  def reset_states(self):
+    self.total.assign(0.0)
+    self.count.assign(0.0)
+    self.batch_counter.assign(0.0)
