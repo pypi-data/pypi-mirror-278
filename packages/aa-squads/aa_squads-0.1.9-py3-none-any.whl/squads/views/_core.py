9bb39ace3abe1c5eff3b5e992e9ee4aa9eec459f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:30 +0000

    Add package aa-squads/0.1.9/aa_squads-0.1.9-py3-none-any.whl

---
+++ packages/aa-squads/aa_squads-0.1.9-py3-none-any.whl/squads/views/_core.py
@@ -0,0 +1,21 @@
+from squads.models.groups import Groups, Pending
+
+
+def add_info_to_context(request, context: dict) -> dict:
+    """Add additional information to the context for the view."""
+    pending_count = Pending.objects.filter(user=request.user).count()
+    new_context = {
+        **{
+            "pending_count": pending_count,
+        },
+        **context,
+    }
+    return new_context
+
+
+def check_permission(request, group: Groups):
+    """Check if the user has the permission."""
+    visible = Groups.objects.visible_to(request.user)
+    if group not in visible:
+        return False
+    return True
