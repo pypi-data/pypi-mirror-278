fd3c0aa5e05edb11b2d7ccb6753a0be3039d02f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:03:29 +0000

    Add package rgwml/0.0.13/rgwml-0.0.13-py3-none-any.whl

---
+++ packages/rgwml/rgwml-0.0.13-py3-none-any.whl/rgwml/__init__.py
@@ -0,0 +1,5 @@
+from .__init__(self, name, db_type, host, username, password, database) import DBPreset
+from .__init__(self, config_path), get_preset(self, name) import Config
+from .__init__(self, preset), connect(self), execute_query(self, query) import DatabaseManager
+from .__init__(self, config), mssql_to_df(self, preset_name, query_str), bigquery_to_df(self, preset_name, query_str) import DataManager
+from .__init__(self, preset), execute_query(self, query, project_id) import BigQueryManager
