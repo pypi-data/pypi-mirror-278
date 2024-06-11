159dad1c36205282f2869057bd58df58640c71d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:34 +0000

    Add package loggingA/0.0.6/loggingA-0.0.6-py3-none-any.whl

---
+++ packages/loggingA/loggingA-0.0.6-py3-none-any.whl/loggingA/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2024/5/12
+# @Author  : alan
+# @File    : __init__.py
+import logging
+
+CRITICAL = logging.CRITICAL
+FATAL = logging.FATAL
+ERROR = logging.ERROR
+WARNING = logging.WARNING
+WARN = logging.WARN
+INFO = logging.INFO
+DEBUG = logging.DEBUG
+NOTSET = logging.NOTSET
