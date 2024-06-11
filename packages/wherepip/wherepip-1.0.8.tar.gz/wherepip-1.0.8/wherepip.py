c7c2dd3560c53c2570e92a4b8d902ed78ad86c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 17:04:32 +0000

    Add package wherepip/1.0.8/wherepip-1.0.8.tar.gz

---
+++ packages/wherepip/wherepip-1.0.8.tar.gz/wherepip-1.0.8/wherepip.py
@@ -0,0 +1,30 @@
+#By Python_Fucker On 2024/6/11
+import subprocess
+import sys
+import pkg_resources
+def whereis(s=None):
+    if s==None: 
+     s=sys.argv[1]
+    return __import__(s).__path__[0]
+def fuck(s=None):
+    if s==None: 
+     s=sys.argv[1]
+    subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', s, '-y'])
+def befuck(s=None):
+    if s==None: 
+     s=sys.argv[1]
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', s])
+def so(s=None):
+    if s==None: 
+     s=sys.argv[1]
+    results = []
+    for pkg in pkg_resources.working_set:
+        if s.lower() in pkg.project_name.lower() or s.lower() in pkg.version.lower():
+            results.append(f"{pkg.project_name}=={pkg.version}")
+    return results
+def all():
+    installed_packages = list(pkg_resources.working_set)
+    results = []
+    for package in installed_packages:
+        results.append(f"{package.project_name}=={package.version}")
+    return results
