59599bc439de1d4f50056a764ca79b0917e1c6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:25 +0000

    Add package rgwml/0.0.11/rgwml-0.0.11.tar.gz

---
+++ packages/rgwml/rgwml-0.0.11.tar.gz/rgwml-0.0.11/src/rgwml/docs.py
@@ -0,0 +1,8 @@
+def show_docs():
+    """Prints out all methods and classes in rgwml package with their signatures."""
+    items = [
+        "add_one(number)",
+        "add_two(number)",
+    ]
+    for item in items:
+        print(item)
