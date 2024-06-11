b46e7d83812e67437ecc9329cc443bac210fff96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:04:01 +0000

    Add package testmodule78910/0.0.1/testmodule78910-0.0.1.tar.gz

---
+++ packages/testmodule78910/testmodule78910-0.0.1.tar.gz/testmodule78910-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'testmodule to test uploading'
+LONG_DESCRIPTION = 'blem :)'
+
+# Setting up
+setup(
+    name="testmodule78910",
+    version=VERSION,
+    author="Roteq (Jay F.)",
+    author_email="<feldmanjay@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(where="testmodule"),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+    ]
+)
