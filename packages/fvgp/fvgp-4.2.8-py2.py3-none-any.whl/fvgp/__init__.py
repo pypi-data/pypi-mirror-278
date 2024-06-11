6612d02459a9f4e1d3ad058b0f1562a350e1d556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:03:47 +0000

    Add package fvgp/4.2.8/fvgp-4.2.8-py2.py3-none-any.whl

---
+++ packages/fvgp/fvgp-4.2.8-py2.py3-none-any.whl/fvgp/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for fvGP."""
+
+__author__ = """Marcus Michael Noack"""
+__email__ = 'MarcusNoack@lbl.gov'
+
+from . import _version
+from loguru import logger
+import sys
+from .gp import GP
+from .fvgp import fvGP
+from .gpMCMC import gpMCMC
+
+__all__ = ['GP', 'fvGP']
+__version__ = _version.get_versions()['version']
+
+logger.disable('fvgp')
