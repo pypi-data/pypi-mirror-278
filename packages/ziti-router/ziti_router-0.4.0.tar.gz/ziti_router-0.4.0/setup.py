ed2add9e8453cff4a313c1d46163f4048e9f81a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:03:15 +0000

    Add package ziti-router/0.4.0/ziti_router-0.4.0.tar.gz

---
+++ packages/ziti-router/ziti_router-0.4.0.tar.gz/ziti_router-0.4.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ziti_router',
+    version='0.4.0',
+    author='sadath',
+    author_email='sadathsadu2002@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'ziti-router = ziti_router.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+       install_requires=[
+        'requests',
+        'pylint',
+        'pylint-exit',
+        'pyinstaller',
+        'tqdm',
+        'psutil',
+        'packaging',
+        'pyjwt',
+        'colorama',
+        'jinja2',
+        'distro',
+        'pyyaml',
+        # Add more dependencies as needed
+    ]
+  
+)
