6cbf954862c631a0048f66ca1b2b7faa70e293ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:25 +0000

    Add package jacknote/0.1.5/jacknote-0.1.5.tar.gz

---
+++ packages/jacknote/jacknote-0.1.5.tar.gz/jacknote-0.1.5/jacknote/__init__.py
@@ -0,0 +1,8 @@
+
+    # export AWS_ACCESS_KEY_ID=AKIA4YQKVLRVPO6T3EFE
+    # export AWS_SECRET_ACCESS_KEY=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
+    # export AWS_DEFAULT_REGION=us-east-1
+
+    def main():
+        return "test"
+    
