785cb676449282a73f0e8257d60c71f16bbc7eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:03:47 +0000

    Add package gridpolator/0.4.0/GridPolator-0.4.0.tar.gz

---
+++ packages/gridpolator/GridPolator-0.4.0.tar.gz/GridPolator-0.4.0/GridPolator/__init__.py
@@ -0,0 +1,18 @@
+"""
+
+The main interaction between the user and ``GridPolator`` is through the
+``GridSpectra`` class. The ``GridSpectra`` class is used to store, recall,
+and interpolate a grid of spectra. However, since it is implemented in JAX,
+it is important to also alert the user of the default units that ``GridSpectra``
+uses. The default units are:
+
+* **flux**: ``W m-2 um-1``
+* **wavelength**: ``um``
+* **temperature**: ``K``
+"""
+
+__version__ = '0.4.0'
+
+from GridPolator.grid import GridSpectra
+
+from GridPolator.config import flux_unit, wl_unit, teff_unit
