a2006c7a3d94afe9cae37df8e48c921227982196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 16:04:56 +0000

    Add package pyldk/1.1.6/pyldk-1.1.6-py3-none-manylinux2014_aarch64.whl

---
+++ packages/pyldk/pyldk-1.1.6-py3-none-manylinux2014_aarch64.whl/pyldk/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File     : __init__.py.py
+# @Author   : jade
+# @Date     : 2023/3/15 14:58
+# @Email    : jadehh@1ive.com
+# @Software : Samples
+# @Desc     :
+from pyldk.version import full_version as __version__
+
---
+++ packages/pyldk/pyldk-1.1.6-py3-none-manylinux2014_aarch64.whl/pyldk/version.py
@@ -0,0 +1,1 @@
+full_version  = '1.1.6'
