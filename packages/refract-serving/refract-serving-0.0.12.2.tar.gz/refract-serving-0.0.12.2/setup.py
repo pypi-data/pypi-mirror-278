9af782c98c2d32aca2dd41494b08085d18e66b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:07 +0000

    Add package refract-serving/0.0.12.2/refract-serving-0.0.12.2.tar.gz

---
+++ packages/refract-serving/refract-serving-0.0.12.2.tar.gz/refract-serving-0.0.12.2/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name="refract-serving",
+    version="0.0.12.2",
+    description="Batch prediction recipe",
+    author="Amit Boke",
+    classifiers=["Programming Language :: Python :: 3.8"],
+    packages=find_packages(),
+    include_package_data=True,
+    # install_requires=["refractml", "refractio[local]"],
+    # install_requires=["refractio[snowflake]"],
+    install_requires=[],
+    python_requires=">=3.8,<3.10"
+)
