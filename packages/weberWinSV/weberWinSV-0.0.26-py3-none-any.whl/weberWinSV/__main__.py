28c4ab325bed02379813219fd7e37c194e3eaac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:04:14 +0000

    Add package weberWinSV/0.0.26/weberWinSV-0.0.26-py3-none-any.whl

---
+++ packages/weberWinSV/weberWinSV-0.0.26-py3-none-any.whl/weberWinSV/__main__.py
@@ -0,0 +1,27 @@
+﻿# weberWinSV/__main__.py
+# python -m weberWinSV 入口程序
+
+import sys
+import os
+from .CWinSupervisor import StartCWinSupervisor
+
+
+def _startWinSV(sSettingPyFullFN, sGroupKeyName, sDictVarName):
+    sPythonFileName = os.path.basename(sSettingPyFullFN)
+    # print(f'_startWinSV.sPythonFileName={sPythonFileName}=')
+    StartCWinSupervisor(sSettingPyFullFN, sPythonFileName, sDictVarName, sGroupKeyName)
+
+
+if __name__ == "__main__":
+    # 调用包内的函数或执行其他初始化代码
+    sSettingPyFullFN = 'settingsWinSV.py'
+    sGroupKeyName = 'GroupId4Run'
+    sDictVarName = 'gDictConfigByGroupId'
+    if len(sys.argv) >= 2:
+        sSettingPyFullFN = sys.argv[1]
+        if len(sys.argv) >= 3:
+            sGroupKeyName = sys.argv[2]
+            if len(sys.argv) >= 4:
+                sDictVarName = sys.argv[3]
+    _startWinSV(sSettingPyFullFN, sGroupKeyName, sDictVarName)
+    # print("Hello from my_package!")
