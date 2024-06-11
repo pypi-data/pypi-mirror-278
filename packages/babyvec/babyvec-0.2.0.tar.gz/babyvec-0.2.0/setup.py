78ecb046c6a5dfe1428698995188a2abcd0fa32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:52 +0000

    Add package babyvec/0.2.0/babyvec-0.2.0.tar.gz

---
+++ packages/babyvec/babyvec-0.2.0.tar.gz/babyvec-0.2.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import find_packages, setup
+
+
+requirements = [
+    "faiss-cpu",
+    "nltk",
+    "npy_append_array",
+    "numpy",
+    "torch",
+    "transformers",
+]
+
+setup(
+    name="babyvec",
+    version="v0.2.0",
+    description="Natural language embedding tools",
+    author="Liam Tengelis",
+    author_email="liam@blacktuskdata.com",
+    url="https://github.com/lummm/babyvec",
+    packages=find_packages(),
+    package_data={
+        "": ["*.yaml"],
+        "babyvec": ["py.typed"],
+    },
+    install_requires=requirements,
+    extras_require={
+        "http": [
+            "uvicorn",
+            "fastapi",
+            "pydantic",
+        ],
+    },
+)
