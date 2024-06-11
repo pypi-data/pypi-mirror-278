7b83e063174ee035c10c1838ab83bc898d3c4ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:03:13 +0000

    Add package botterino/0.5.1/botterino-0.5.1.tar.gz

---
+++ packages/botterino/botterino-0.5.1.tar.gz/botterino-0.5.1/Botterino/Loader/loader.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from ..config import roundfile, archivefile
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+yaml.allow_duplicate_keys = True
+
+
+def dump(data, file):
+    with open(file, "w", encoding="utf-8") as f:
+        yaml.dump(data, f)
+
+
+def append(data, file):
+    with open(file, "r", encoding="utf-8") as f:
+        x = yaml.load(f)
+    if not x:
+        dump(data, file)
+        return
+    x.update(data)
+    dump(x, file)
+
+
+def load(file):
+    with open(file, "r", encoding="utf-8") as f:
+        return yaml.load(f)
+
+
+def getRound():
+    x = load(roundfile)
+    if not x:
+        return None
+    k = next(iter(x))
+    top = x.pop(k)
+    if x:
+        dump(x, roundfile)
+    else:
+        open(roundfile, "w", encoding="utf-8").close()
+    y = load(archivefile) or {}
+    y[k] = top
+    dump(y, archivefile)
+    return top
