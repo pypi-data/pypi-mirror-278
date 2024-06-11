5b5b2e4820caf77edef967be9bea26d0b616d5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:03:31 +0000

    Add package bosc/0.0.7/bosc-0.0.7-py3-none-any.whl

---
+++ packages/bosc/bosc-0.0.7-py3-none-any.whl/bosc/__init__.py
@@ -0,0 +1,35 @@
+from bosc.collection import Collection, OrderDirection
+from bosc.database import Database
+from bosc.document import Document
+from bosc.index import Index, IndexType
+from bosc.query.find import And, Eq, Gt, Gte, In, Lt, Lte, Neq, Nin, Or
+from bosc.query.update import Inc, Now, RemoveField, Set
+
+__version__ = "0.0.7"
+__all__ = [
+    # Coore
+    "Database",
+    "Collection",
+    "OrderDirection",
+    # Index
+    "Index",
+    "IndexType",
+    # ODM
+    "Document",
+    # Find Queries
+    "Eq",
+    "Neq",
+    "Gt",
+    "Gte",
+    "Lt",
+    "Lte",
+    "In",
+    "Nin",
+    "And",
+    "Or",
+    # Update Queries
+    "Set",
+    "Inc",
+    "RemoveField",
+    "Now",
+]
