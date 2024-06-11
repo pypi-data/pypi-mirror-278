fe01bb3dcc9e80e6cb94fc7309091520bba3b9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:03:46 +0000

    Add package patchwork-cli/0.0.15/patchwork_cli-0.0.15-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.15-py3-none-any.whl/patchwork/steps/PreparePrompt/typed.py
@@ -0,0 +1,12 @@
+from typing_extensions import Any, Iterable, NotRequired, TypedDict
+
+
+class PreparePromptInputs(TypedDict):
+    prompt_template_file: str
+    prompt_id: str
+    prompt_value_file: NotRequired[str]
+    prompt_values: NotRequired[Iterable[dict[str, Any]]]
+
+
+class PreparePromptOutputs(TypedDict):
+    prompts: Iterable[dict]
