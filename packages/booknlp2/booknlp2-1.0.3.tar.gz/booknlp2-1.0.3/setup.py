db9ec64de2af459417032d8bd616f830dbd695e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:54 +0000

    Add package booknlp2/1.0.3/booknlp2-1.0.3.tar.gz

---
+++ packages/booknlp2/booknlp2-1.0.3.tar.gz/booknlp2-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='booknlp2', 
+	version='1.0.3',
+	packages=find_packages(),
+	py_modules=['booknlp2'],
+	author="Rushikesh Hiray",
+	author_email="rhiray03@gmail.com",
+	include_package_data=True, 
+	license="MIT",
+	install_requires=['transformers',
+		'torch>=1.7.1',
+			'tensorflow>=1.15',
+			'spacy>=3',
+
+                      ],
+
+	)
