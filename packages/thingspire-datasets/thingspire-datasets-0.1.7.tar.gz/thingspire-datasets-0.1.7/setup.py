8729010b2e8f309224d8471e5a546fcf7ad73d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:03:42 +0000

    Add package thingspire-datasets/0.1.7/thingspire-datasets-0.1.7.tar.gz

---
+++ packages/thingspire-datasets/thingspire-datasets-0.1.7.tar.gz/thingspire-datasets-0.1.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="thingspire-datasets",
+    version="0.1.7",
+    author="Inho Kim",
+    author_email="inho@thingspire.com",
+    description="thingspire feature package",
+    long_description="A package that collects and processes weather and "
+                     "special day information by local government and "
+                     "applies it to various projects",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['Bottleneck', 'numpy', 'pandas',
+                      'requests', 'json', 'aiohttp'],
+    keywords=['thingspire', 'thingspire-datasets'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
