eccd7df0b2e3e0c0bc47c3e75ee803a77af51dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:33 +0000

    Add package iaptoolkit-cli/0.0.1a1/iaptoolkit_cli-0.0.1a1-py3-none-any.whl

---
+++ packages/iaptoolkit-cli/iaptoolkit_cli-0.0.1a1-py3-none-any.whl/iaptoolkit_cli/application.py
@@ -0,0 +1,34 @@
+from cleo.application import Application
+
+# from iaptoolkit_cli.commands.curl import CurlCommand_OIDC
+# from iaptoolkit_cli.commands.curl import CurlHeaderString_OIDC
+from iaptoolkit_cli.commands.oidc import TokenCommand_OIDC
+from iaptoolkit_cli.commands.oidc import RequestCommand_OIDC
+# from iaptoolkit_cli.commands.wget import WgetCommand_OIDC
+
+# from iaptoolkit_cli.commands.curl import CurlCommand_OAuth2
+# from iaptoolkit_cli.commands.curl import CurlHeaderString_OAuth2
+# from iaptoolkit_cli.commands.oauth2 import InitCommand_OAuth2
+# from iaptoolkit_cli.commands.oauth2 import TokenCommand_OAuth2
+# from iaptoolkit_cli.commands.oauth2 import RequestCommand_OAuth2
+# from iaptoolkit_cli.commands.wget import WgetCommand_OAuth2
+
+from cleo.application import Application
+
+
+application = Application()
+
+application.add(TokenCommand_OIDC())
+application.add(RequestCommand_OIDC())
+
+# application.add(InitCommand_OAuth2())
+# application.add(TokenCommand_OAuth2())
+# application.add(RequestCommand_OAuth2())
+
+# application.add(CurlCommand())
+# application.add(CurlHeaderString_OIDC())
+# application.add(WgetCommand())
+
+
+if __name__ == "__main__":
+    application.run()
---
+++ packages/iaptoolkit-cli/iaptoolkit_cli-0.0.1a1-py3-none-any.whl/iaptoolkit_cli/commands/oidc.py
@@ -0,0 +1,77 @@
+import sys
+
+import requests
+
+# from cleo.helpers import argument
+# from cleo.helpers import option
+
+from iaptoolkit import IAPToolkit_OIDC
+
+from kvcommon.output import pp
+
+from iaptoolkit_cli.common import IAPToolkitCLICommand
+from iaptoolkit_cli.common import CommonArguments
+from iaptoolkit_cli.common import CommonOptions
+from iaptoolkit_cli.common import get_token_string_oidc
+
+
+class TokenCommand_OIDC(IAPToolkitCLICommand):
+    """
+    TODO: -o arg for output destination instead of stdout (e.g.; File)
+    """
+
+    name = "token_oidc"
+    description = (
+        "Retrieves an IAP auth token using OIDC (for Service Accounts) and "
+        "Application Default Credentials (ADC), and dumps it to stdout"
+    )
+    arguments = [
+        CommonArguments.iap_client_id,
+    ]
+
+    options = [
+        CommonOptions.bypass_cache,
+    ]
+
+    def handle(self):
+        iap_client_id: str = self.argument_str(CommonArguments.iap_client_id.name)
+
+        token: str = get_token_string_oidc(iap_client_id=iap_client_id)
+        sys.stdout.write(token)
+        sys.stdout.flush()
+        return
+
+
+class RequestCommand_OIDC(IAPToolkitCLICommand):
+    name = "request_oidc"
+    description = (
+        "Makes a HTTP Request to the specified IAP-secured url and dumps the response "
+        "using an OIDC token (e.g., for ServiceAccounts)."
+    )
+
+    arguments = [
+        CommonArguments.iap_client_id,
+        CommonArguments.url,
+        CommonArguments.http_method,
+    ]
+    options = [
+        CommonOptions.use_auth_header,
+        CommonOptions.dump_content,
+    ]
+
+    def handle(self):
+        iap_client_id = self.argument_str(CommonArguments.iap_client_id.name)
+        url = self.argument_str(CommonArguments.url.name)
+        http_method = self.argument_str_optional(CommonArguments.http_method.name) or "get"
+        http_method = http_method.lower()
+
+        use_auth_header = self.option_bool(CommonOptions.use_auth_header.name)
+        dump_content = self.option_bool(CommonOptions.dump_content.name)
+
+        headers = dict()
+        iaptk = IAPToolkit_OIDC(google_iap_client_id=iap_client_id)
+        iaptk.get_token_and_add_to_headers(request_headers=headers, use_auth_header=use_auth_header)
+        response = requests.request(method=http_method, url=url, headers=headers)
+        self.line(f"<Response [{response.status_code}]>")
+        if dump_content:
+            pp(response.content)
