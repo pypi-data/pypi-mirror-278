eb95a95e4c000f6aca18fec5ea95f29f483831d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:28 +0000

    Add package reportXlsx/0.4.2/reportXlsx-0.4.2.tar.gz

---
+++ packages/reportXlsx/reportXlsx-0.4.2.tar.gz/reportXlsx-0.4.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+# 确定操作系统
+
+setup(
+    name='reportXlsx',
+    version='0.4.2',
+    packages=find_packages(),
+    package_data={
+        'reportXlsx': ['resources/windows/*.dll','resources/linux/*.dll'],
+    },
+    include_package_data=True,
+    install_requires=[
+        'pandas','openpyxl','pythonnet','xlwings'
+    ],
+    description='这是一个操作xlsx的库,兼容windows和linux',
+    long_description=open('README.md',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/bailulue',
+    author='bailu',
+    author_email='yabailu@chinatelecom.cn'
+)
