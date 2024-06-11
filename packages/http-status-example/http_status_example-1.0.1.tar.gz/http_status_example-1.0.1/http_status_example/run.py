6b07019e038dc2e8ebc47948662ac180098e0f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:04:08 +0000

    Add package http-status-example/1.0.1/http_status_example-1.0.1.tar.gz

---
+++ packages/http-status-example/http_status_example-1.0.1.tar.gz/http_status_example-1.0.1/http_status_example/run.py
@@ -0,0 +1,33 @@
+import requests
+import sys
+URL = r"https://choosealicense.com/"
+
+def get_request(url:str):
+    """"""
+    print(f"checking status for {url} ...")
+    try:
+        response = requests.get(url=url)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.ConnectionError as conn_err:
+        print(f"Connection error occurred: {conn_err}")
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Timeout error occurred: {timeout_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"An error occurred: {req_err}")
+    else:
+        print(response.status_code)
+
+
+def main(url:str):
+    """Main entry point for the script"""    
+    get_request(url)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        url = URL
+    else:
+        url = sys.argv[1]
+    main(url)
