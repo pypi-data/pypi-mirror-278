ab230bb6ad726bed997ab64704861952034241d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:04:06 +0000

    Add package sdk-cloud-dfe/1.0.0/sdk-cloud-dfe-1.0.0.tar.gz

---
+++ packages/sdk-cloud-dfe/sdk-cloud-dfe-1.0.0.tar.gz/sdk-cloud-dfe-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sdk-cloud-dfe",
+    version="1.0.0",
+    author="Integra Notas",
+    author_email="comercial@cloud-dfe.com.br",
+    description="SDK para comunicar com API Integra Notas.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+    "requests",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires=">=3.6",
+)
