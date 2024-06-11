60426dbdbf2803ad1c2342c1714f250e2a002431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:04:23 +0000

    Add package pynrw/1.5.2/pynrw-1.5.2-py3-none-any.whl

---
+++ packages/pynrw/pynrw-1.5.2-py3-none-any.whl/nrw/__init__.py
@@ -0,0 +1,9 @@
+"""Materialien zu den zentralen NRW-Abiturprüfungen im Fach Informatik ab 2018
+implementiert in Python.
+"""
+
+from __future__ import annotations
+
+__version__: Final[str] = "1.5.2"
+
+from typing import Final
