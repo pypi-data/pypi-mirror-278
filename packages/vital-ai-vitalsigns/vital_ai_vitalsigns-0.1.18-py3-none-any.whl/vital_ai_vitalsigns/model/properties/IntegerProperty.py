66ffa499274c96980e6a50309461eee156d6fe2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:04:19 +0000

    Add package vital-ai-vitalsigns/0.1.18/vital_ai_vitalsigns-0.1.18-py3-none-any.whl

---
+++ packages/vital-ai-vitalsigns/vital_ai_vitalsigns-0.1.18-py3-none-any.whl/vital_ai_vitalsigns/model/properties/IntegerProperty.py
@@ -0,0 +1,29 @@
+from vital_ai_vitalsigns.model.properties.IProperty import IProperty
+
+
+class IntegerProperty(IProperty):
+    def __init__(self, value: int):
+        int_value = int(value)
+        super().__init__(int_value)
+
+    def __bool__(self):
+        return bool(self.value)
+
+    def __getattr__(self, attr):
+        return getattr(self.value, attr)
+
+    def __int__(self):
+        return int(self.value)
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.value == other
+        elif isinstance(other, IntegerProperty):
+            return self.value == other.value
+        return NotImplemented
+
+    def __rshift__(self, other):
+        return self == other
+
+    def to_json(self):
+        return {"value": self.value}
---
+++ packages/vital-ai-vitalsigns/vital_ai_vitalsigns-0.1.18-py3-none-any.whl/vital_ai_vitalsigns/service/graph/name_graph.py
@@ -0,0 +1,7 @@
+
+class VitalNameGraph:
+    def __init__(self, graph_uri):
+        self.graph_uri = graph_uri
+
+    def get_graph_uri(self):
+        return self.graph_uri
