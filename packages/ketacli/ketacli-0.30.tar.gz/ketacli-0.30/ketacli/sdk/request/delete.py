b6acbf72976c1e8af25398439c991695f3d15781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 16:05:11 +0000

    Add package ketacli/0.30/ketacli-0.30.tar.gz

---
+++ packages/ketacli/ketacli-0.30.tar.gz/ketacli-0.30/ketacli/sdk/request/delete.py
@@ -0,0 +1,28 @@
+from .asset_map import get_request_path
+import json
+from ..util import template_render
+
+
+def delete_asset_request(asset_type, name=None, data=None, **kwargs):
+    path, method, example = get_request_path(asset_type, "delete")
+    example = json.dumps(example, ensure_ascii=False)
+
+    if name is not None and "name" not in kwargs:
+        kwargs.update({"name": name})
+    path = template_render(path, **kwargs)
+    example = template_render(example, **kwargs)
+    example = json.loads(example)
+    if method == "create":
+        method = "post"
+    elif method == "update":
+        method = "put"
+    if not data:
+        data = example
+    return {
+        "path": path,
+        "query_params": {},
+        # list 操作用不到的内容
+        "method": method,
+        "data": data,
+        "custom_headers": {},
+    }
