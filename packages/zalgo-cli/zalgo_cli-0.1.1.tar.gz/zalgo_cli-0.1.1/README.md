c2a790301710a8b43143a224e158623865a930b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:03:42 +0000

    Add package zalgo-cli/0.1.1/zalgo_cli-0.1.1.tar.gz

---
+++ packages/zalgo-cli/zalgo_cli-0.1.1.tar.gz/zalgo_cli-0.1.1/README.md
@@ -0,0 +1,80 @@
+# zalgo-cli
+
+A simple command line tool to generate zalgo text.
+
+- [zalgo-cli](#zalgo-cli)
+  - [Demo](#demo)
+  - [Installation](#installation)
+    - [pipx](#pipx)
+    - [pip](#pip)
+  - [Usage](#usage)
+  - [Develop](#develop)
+  - [Credits](#credits)
+
+## Demo
+
+```
+$ zalgo test -n 20
+
+ṯe͕sͫť	t̿e̵s͍t̐	t̜e̼s̗tͨ	ẗe̮s̼tͥ	
+t͋e̞śt̪	t̊e̎s̮t͈	ṯe͊s̗t̍	t̄e̬śt͗	
+t͕e͓s͜t͕	tͯe̙s͙t͍	t̀e̊s̏t̲	t̰ẽs̕t̗	
+t̘e͐s̞t̿	ẗeͣs͆t̸	t͙e͉s̑t̤	t̀e͙s̐t̋	
+t͆e̟s̈́tͦ	t̛e͏s̛t͕	t̆eͮs̠tͮ	t͋e̱s͎tͦ	
+```
+
+## Installation
+
+### pipx
+
+This is the recommended installation method.
+
+```
+$ pipx install zalgo-cli
+```
+
+### [pip](https://pypi.org/project/zalgo-cli/)
+
+```
+$ pip install zalgo-cli
+```
+
+## Usage
+
+You can use either `zalgo` or `zalgo-cli` to run the program.
+
+```
+$ zalgo --help
+
+usage: zalgo [-h] [-a int] [-l int] [-n int] [-o] [-V] [str]
+
+Generate Zalgo text
+
+positional arguments:
+  str                   Initial string to Zalgo-fy. If not provided, read from
+                        stdin (default: None)
+
+options:
+  -h, --help            show this help message and exit
+  -a int, --adds-per-char int
+                        Number of additions per character (default: 1)
+  -l int, --char-limit int
+                        Character limit [0 for no limit] (default: 0)
+  -n int, --amount int  Amount of Zalgo text to generate (default: 1)
+  -o, -1, --one-per-line
+                        Output one Zalgo-fied string per line (default: False)
+  -V, --version         show program's version number and exit
+
+```
+
+## Develop
+
+```
+$ git clone https://github.com/tddschn/zalgo-cli.git
+$ cd zalgo-cli
+$ poetry install
+```
+
+## Credits
+
+Development of this project was based on https://github.com/n-1x/zalgo-cli .
---
+++ packages/zalgo-cli/zalgo_cli-0.1.1.tar.gz/zalgo_cli-0.1.1/zalgo_cli/cli.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+Author : Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
+Date   : 2024-06-10
+Purpose: Generate Zalgo text
+"""
+
+import argparse
+import random
+import sys
+from zalgo_cli import __version__
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Generate Zalgo text',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('string',
+                        metavar='str',
+                        nargs='?',
+                        help='Initial string to Zalgo-fy. If not provided, read from stdin')
+
+    parser.add_argument('-a',
+                        '--adds-per-char',
+                        metavar='int',
+                        type=int,
+                        default=1,
+                        help='Number of additions per character')
+
+    parser.add_argument('-l',
+                        '--char-limit',
+                        metavar='int',
+                        type=int,
+                        default=0,
+                        help='Character limit [0 for no limit]')
+
+    parser.add_argument('-n',
+                        '--amount',
+                        metavar='int',
+                        type=int,
+                        default=1,
+                        help='Amount of Zalgo text to generate')
+
+    parser.add_argument('-o', '-1',
+                        '--one-per-line',
+                        action='store_true',
+                        help='Output one Zalgo-fied string per line')
+
+    parser.add_argument(
+        '-V', '--version',
+        action='version',
+        version=f'%(prog)s {__version__}',
+    )
+    return parser.parse_args()
+
+def zalgo(string, adds_per_char):
+    """Take the given string and add <adds_per_char> unicode combining characters after each character."""
+    result = ''
+
+    for char in string:
+        for _ in range(adds_per_char):
+            rand_bytes = random.randint(0x300, 0x36f).to_bytes(2, 'big')
+            char += rand_bytes.decode('utf-16be')
+        result += char
+
+    return result
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    if args.string:
+        string = args.string
+    else:
+        string = sys.stdin.read()
+
+    adds_per_char = args.adds_per_char
+    char_limit = args.char_limit
+    amount_wanted = args.amount
+    one_per_line = args.one_per_line
+
+    if char_limit != 0:
+        adds_per_char = (char_limit - len(string)) // len(string)
+
+    for i in range(amount_wanted):
+        print(zalgo(string, adds_per_char), end='\n' if one_per_line else '\t')
+
+        if not one_per_line and ((i + 1) % 4 == 0):
+            print()
+
+    if not one_per_line:
+        print()
+
+if __name__ == '__main__':
+    main()
