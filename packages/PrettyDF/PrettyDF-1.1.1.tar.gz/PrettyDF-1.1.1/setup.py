cc5dd036cfe7e67faeee3fc444c5464750373be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 03:03:39 +0000

    Add package PrettyDF/1.1.1/PrettyDF-1.1.1.tar.gz

---
+++ packages/PrettyDF/PrettyDF-1.1.1.tar.gz/PrettyDF-1.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+dir = Path(__file__).parent
+README = (dir / "README.md").read_text()
+
+setup(
+    name='PrettyDF',
+    version='1.1.1',
+    packages=find_packages(),
+    author='DawnSaju',
+    description='Pretty Print DataFrames',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author_email = "dawnsajubusiness@gmail.com",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+
+    install_requires=[
+        'pandas',
+        'tabulate'
+    ],
+    entry_points={
+        'console_scripts': [
+            'prettydf = PrettyDF:hello'
+        ]
+    }
+)
+
