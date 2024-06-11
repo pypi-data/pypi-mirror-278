2f192081876d01f297257572e54ed5f494d18f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 15:03:37 +0000

    Add package robustintelligence/2.9.0rc3/robustintelligence-2.9.0rc3-py3-none-any.whl

---
+++ packages/robustintelligence/robustintelligence-2.9.0rc3-py3-none-any.whl/ri/__init__.py
@@ -0,0 +1,4 @@
+from .client import RIClient as Client
+from .client import FirewallClient
+from .apiclient.models import *
+from .fwclient.models import *
---
+++ packages/robustintelligence/robustintelligence-2.9.0rc3-py3-none-any.whl/ri/apiclient/api/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa
+
+# import apis into api package
+from ri.apiclient.api.generative_model_testing_api import GenerativeModelTestingApi
