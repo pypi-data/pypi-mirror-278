58972a4f7b5a1e6f2c034bfafe17e3600608d132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:48 +0000

    Add package toggl-api-wrapper/0.1.0/toggl_api_wrapper-0.1.0-py3-none-any.whl

---
+++ packages/toggl-api-wrapper/toggl_api_wrapper-0.1.0-py3-none-any.whl/toggl_api/modules/workspace.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from .meta import TogglCachedEndpoint
+from .models import TogglWorkspace
+
+
+class WorkspaceEndpoint(TogglCachedEndpoint):
+    def get_workspace(self, *, refresh: bool = False) -> Optional[TogglWorkspace]:
+        return self.request("", refresh=refresh)  # type: ignore[return-value]
+
+    @property
+    def model(self) -> type[TogglWorkspace]:
+        return TogglWorkspace
+
+    @property
+    def endpoint(self) -> str:
+        return super().endpoint + f"workspaces/{self.workspace_id}"
