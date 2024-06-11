1e0bdaf6cba48835ab22a887cffc55be9eb5d1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 17:04:19 +0000

    Add package ocr-toolkits/0.0.2/ocr-toolkits-0.0.2.tar.gz

---
+++ packages/ocr-toolkits/ocr-toolkits-0.0.2.tar.gz/ocr-toolkits-0.0.2/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(*paths):
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+requirements = [
+    "Pillow",
+    "jellyfish",
+    "opencv-python",
+    "numpy"
+]
+
+setup(
+    name="ocr-toolkits",
+    version="0.0.2",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={
+        'ocr-tools': [
+            'font/AKbalthom KhmerLer Regular.ttf',
+            'font/Khmer MEF1 Regular.ttf',
+            'font/Khmer OS Battambang Regular.ttf',
+            'font/Khmer OS Muol Light Regular.ttf',
+            'font/Khmer OS Siemreap Regular.ttf'
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/MetythornPenn/ocr-toolkits.git',
+    license='Apache Software License 2.0',
+    author = 'Metythorn Penn',
+    author_email = 'metythorn@gmail.com',
+    keywords='ocr-toolkits',
+    description='Ocr_tools is a Python library that generates synthetic images containing Khmer text and other important toolbox',
+    install_requires=requirements,
+    long_description=(read('README.md')),
+    long_description_content_type='text/markdown',
+	classifiers= [
+		'Natural Language :: English',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+	],
+)
