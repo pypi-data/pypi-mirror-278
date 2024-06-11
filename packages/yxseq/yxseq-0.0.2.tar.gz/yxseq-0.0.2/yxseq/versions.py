8c75f065691eaaceab4aa76418ac24620da4999c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:03:57 +0000

    Add package yxseq/0.0.2/yxseq-0.0.2.tar.gz

---
+++ packages/yxseq/yxseq-0.0.2.tar.gz/yxseq-0.0.2/yxseq/versions.py
@@ -0,0 +1,19 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. add cli",
+            "2. add fq2fa cmd",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. init",
+        ],
+    },
+]
