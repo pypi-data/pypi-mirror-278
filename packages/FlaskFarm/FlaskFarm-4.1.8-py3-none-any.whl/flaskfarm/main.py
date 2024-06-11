05d32f8f2012970e8ead51387805bf2228d3be0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 17:04:20 +0000

    Add package FlaskFarm/4.1.8/FlaskFarm-4.1.8-py3-none-any.whl

---
+++ packages/FlaskFarm/FlaskFarm-4.1.8-py3-none-any.whl/flaskfarm/main.py
@@ -0,0 +1,23 @@
+﻿try:
+    from gevent import monkey;monkey.patch_all()
+    print('[MAIN] gevent mokey patch!!')
+except:
+    print('[MAIN] gevent not installed!!')
+import os
+import sys
+import traceback
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib'))
+
+try:
+    from framework import frame
+
+    # flaskfarm.main : 패키지로 실행. 패키지로 celry 실행 체크
+    if __name__ in ['__main__', 'flaskfarm.main'] and sys.argv[0].endswith('celery') == False:
+        frame.start()
+    else:  
+        app = frame.app
+        celery = frame.celery
+except Exception as exception:
+    print(str(exception))  
+    print(traceback.format_exc())
