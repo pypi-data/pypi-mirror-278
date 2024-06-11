0639fcbc304ccfdf897eca09af7135296a54f859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:03:45 +0000

    Add package ufpy/0.1.2/ufpy-0.1.2.tar.gz

---
+++ packages/ufpy/ufpy-0.1.2.tar.gz/ufpy-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+from ufpy import __version__
+
+with open('README.md', 'r', encoding='utf-8') as mdf:
+    long_description = mdf.read()
+
+install_requires = [
+
+]
+
+organization_name = 'honey-team'
+author, author_email = 'bleudev', 'aitiiigg1@gmail.com'
+project_name = 'ufpy'
+github_url = f'https://github.com/{organization_name}/{project_name}'
+
+setup(
+    name=project_name,
+    version=__version__,
+    author=author,
+    author_email=author_email,
+    description='Ufpy (Useful Python) - package for Python with some useful features.',
+    download_url=f'{github_url}/releases/tag/{__version__}',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=github_url,
+    packages=[project_name, f'{project_name}.typ'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12',
+    ],
+    zip_safe=False,
+    python_requires=">=3.12",
+    install_requires=install_requires
+)
