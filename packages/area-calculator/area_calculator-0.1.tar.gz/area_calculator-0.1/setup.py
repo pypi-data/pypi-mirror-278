65979338378cad0072691211c3e3b0ed67c39388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 16:05:30 +0000

    Add package area-calculator/0.1/area_calculator-0.1.tar.gz

---
+++ packages/area-calculator/area_calculator-0.1.tar.gz/area_calculator-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='area_calculator',  # 模块名
+    version='0.1',  # 版本号
+    packages=find_packages(),  # 自动找到所有包和子包
+    author='Name',  # 作者
+)
