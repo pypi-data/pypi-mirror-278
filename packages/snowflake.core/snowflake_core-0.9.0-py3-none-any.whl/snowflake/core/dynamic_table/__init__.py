689c6e3347af3178f927eb30f8e3a5299bb5ee5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 14:03:44 +0000

    Add package snowflake.core/0.9.0/snowflake_core-0.9.0-py3-none-any.whl

---
+++ packages/snowflake.core/snowflake_core-0.9.0-py3-none-any.whl/snowflake/core/dynamic_table/__init__.py
@@ -0,0 +1,21 @@
+from ..dynamic_table._generated.models import (
+    DownstreamLag,
+    DynamicTable,
+    DynamicTableClone,
+    DynamicTableColumn,
+    TargetLag,
+    UserDefinedLag,
+)
+from ._dynamic_table import DynamicTableCollection, DynamicTableResource
+
+
+__all__ = [
+    "DownstreamLag",
+    "DynamicTable",
+    "DynamicTableClone",
+    "DynamicTableResource",
+    "DynamicTableCollection",
+    "DynamicTableColumn",
+    "TargetLag",
+    "UserDefinedLag",
+]
