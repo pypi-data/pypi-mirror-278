47681c9ae2c9935acbb41fcffa29e88a66cca551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 04:04:11 +0000

    Add package cg/60.9.8/cg-60.9.8-py3-none-any.whl

---
+++ packages/cg/cg-60.9.8-py3-none-any.whl/cg/cli/delete/base.py
@@ -0,0 +1,26 @@
+"""CLI for deleting records in statusDB """
+
+import logging
+
+import click
+
+from cg.cli.delete.case import delete_case
+from cg.cli.delete.cases import delete_cases
+from cg.cli.delete.observations import (
+    delete_available_observations,
+    delete_observations,
+)
+
+LOG = logging.getLogger(__name__)
+
+
+@click.group()
+def delete():
+    """delete database records in CG."""
+    LOG.info("Running CG delete")
+
+
+delete.add_command(delete_case)
+delete.add_command(delete_cases)
+delete.add_command(delete_observations)
+delete.add_command(delete_available_observations)
