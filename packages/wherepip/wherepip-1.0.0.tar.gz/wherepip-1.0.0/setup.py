4106b44ea358e05e3def3a95af02972ac4769c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 16:05:21 +0000

    Add package wherepip/1.0.0/wherepip-1.0.0.tar.gz

---
+++ packages/wherepip/wherepip-1.0.0.tar.gz/wherepip-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='wherepip',
+    version='1.0.0',
+    description='高效python库管理工具,功能有搜索、安装、卸载、获取库所在目录（python学霸公众号）',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Python学霸',
+    author_email='python@xueba.com',
+    py_modules=['wherepip'],
+    install_requires=['pkg_resources'],
+    entry_points={
+        'console_scripts': [
+            'where=wherepip:where',
+            'fuck=wherepip:fuck',
+            'befuck=wherepip:befuck',
+            'so=wherepip:so',
+            'all=wherepip:all'
+        ]
+    }
+)
