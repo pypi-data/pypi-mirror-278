78575fbf21af86facade86b8f5ec7d14b6956f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 05:03:09 +0000

    Add package sgzenity/1.0.4/sgzenity-1.0.4.tar.gz

---
+++ packages/sgzenity/sgzenity-1.0.4.tar.gz/sgzenity-1.0.4/src/sgzenity/__init__.py
@@ -0,0 +1,13 @@
+from .sgszenity import (
+    calendar,
+    color_selection,
+    entry,
+    error,
+    file_selection,
+    message,
+    password,
+    question,
+    scale,
+    sglist,
+    warning,
+)
