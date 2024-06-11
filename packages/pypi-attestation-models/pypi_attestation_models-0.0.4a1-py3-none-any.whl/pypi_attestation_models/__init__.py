59478205bbc4ba24add48c42a2e6f2968e6bd80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-11 06:03:24 +0000

    Add package pypi-attestation-models/0.0.4a1/pypi_attestation_models-0.0.4a1-py3-none-any.whl

---
+++ packages/pypi-attestation-models/pypi_attestation_models-0.0.4a1-py3-none-any.whl/pypi_attestation_models/__init__.py
@@ -0,0 +1,27 @@
+"""The `pypi-attestation-models` APIs."""
+
+__version__ = "0.0.4a1"
+
+from ._impl import (
+    Attestation,
+    AttestationPayload,
+    ConversionError,
+    InvalidAttestationError,
+    TransparencyLogEntry,
+    VerificationError,
+    VerificationMaterial,
+    pypi_to_sigstore,
+    sigstore_to_pypi,
+)
+
+__all__ = [
+    "Attestation",
+    "AttestationPayload",
+    "ConversionError",
+    "InvalidAttestationError",
+    "TransparencyLogEntry",
+    "VerificationError",
+    "VerificationMaterial",
+    "pypi_to_sigstore",
+    "sigstore_to_pypi",
+]
