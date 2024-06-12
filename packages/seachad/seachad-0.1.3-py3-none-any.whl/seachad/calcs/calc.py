1324faa711132387db6e60ef51b2311571622bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:04:26 +0000

    Add package seachad/0.1.3/seachad-0.1.3-py3-none-any.whl

---
+++ packages/seachad/seachad-0.1.3-py3-none-any.whl/seachad/calcs/calc.py
@@ -0,0 +1,9 @@
+from seachad.calcs.consts import PI
+
+
+def sum(num1: float, num2: float) -> float:
+    return num1 + num2
+
+
+def area_circle(radio: float) -> float:
+    return radio * radio * PI
