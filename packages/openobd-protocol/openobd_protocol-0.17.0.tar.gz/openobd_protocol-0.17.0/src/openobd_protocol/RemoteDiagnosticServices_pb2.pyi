c8bac55eb1967383c53c2f7381c81fe4eb535dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:06 +0000

    Add package openobd-protocol/0.17.0/openobd_protocol-0.17.0.tar.gz

---
+++ packages/openobd-protocol/openobd_protocol-0.17.0.tar.gz/openobd_protocol-0.17.0/src/openobd_protocol/RemoteDiagnosticServices_pb2.pyi
@@ -0,0 +1,8 @@
+from openobd_protocol import Authentication_pb2 as _Authentication_pb2
+from openobd_protocol import BasicResponse_pb2 as _BasicResponse_pb2
+from openobd_protocol import BusConfiguration_pb2 as _BusConfiguration_pb2
+from openobd_protocol import Status_pb2 as _Status_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
