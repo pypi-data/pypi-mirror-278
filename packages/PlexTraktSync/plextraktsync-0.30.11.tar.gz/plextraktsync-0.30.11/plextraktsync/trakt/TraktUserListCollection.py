53c56e0457afc9e8f696a239cb67b05988903dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:07:13 +0000

    Add package PlexTraktSync/0.30.11/plextraktsync-0.30.11.tar.gz

---
+++ packages/PlexTraktSync/plextraktsync-0.30.11.tar.gz/plextraktsync-0.30.11/plextraktsync/trakt/TraktUserListCollection.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from collections import UserList
+from typing import TYPE_CHECKING
+
+from plextraktsync.factory import logging
+from plextraktsync.trakt.TraktUserList import TraktUserList
+
+if TYPE_CHECKING:
+    from plextraktsync.media.Media import Media
+    from plextraktsync.trakt.types import TraktLikedList, TraktPlayable
+
+
+class TraktUserListCollection(UserList):
+    logger = logging.getLogger(__name__)
+
+    @property
+    def is_empty(self):
+        return not len(self)
+
+    def add_to_lists(self, m: Media):
+        # Skip movie editions
+        # https://support.plex.tv/articles/multiple-editions/#:~:text=Do%20Multiple%20Editions%20work%20with%20watch%20state%20syncing%3F
+        if m.plex.edition_title is not None:
+            return
+        for tl in self:
+            tl.add(m)
+
+    def load_lists(self, liked_lists: list[TraktLikedList]):
+        for liked_list in liked_lists:
+            self.add_list(liked_list["listid"], liked_list["listname"])
+
+    def add_watchlist(self, items: list[TraktPlayable]):
+        tl = TraktUserList.from_watchlist(items)
+        self.append(tl)
+        return tl
+
+    def add_list(self, list_id: int, list_name: str):
+        tl = TraktUserList.from_trakt_list(list_id, list_name)
+        self.append(tl)
+        return tl
