7c9c4475f52df5c326386de89ce2fdc684a4dfdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 19:04:02 +0000

    Add package bucketbase/1.3.0/bucketbase-1.3.0-py3-none-any.whl

---
+++ packages/bucketbase/bucketbase-1.3.0-py3-none-any.whl/bucketbase/__init__.py
@@ -0,0 +1,8 @@
+from bucketbase.cached_immutable_bucket import CachedImmutableBucket
+from bucketbase.errors import DeleteError
+from bucketbase.errors import DeleteError
+from bucketbase.file_lock import FileLockForPath
+from bucketbase.fs_bucket import AppendOnlyFSBucket, FSBucket
+from bucketbase.ibucket import IBucket, ShallowListing, S3_NAME_SAFE_RE, S3_NAME_CHARS_NO_SEP
+from bucketbase.memory_bucket import MemoryBucket
+from bucketbase.minio_bucket import MinioBucket
---
+++ packages/bucketbase/bucketbase-1.3.0-py3-none-any.whl/bucketbase/cached_immutable_bucket.py
@@ -0,0 +1,54 @@
+import io
+from pathlib import Path, PurePosixPath
+from typing import Iterable, Union, BinaryIO
+
+from streamerate import slist
+
+from bucketbase.errors import DeleteError
+from bucketbase.fs_bucket import AppendOnlyFSBucket
+from bucketbase.ibucket import ShallowListing, IBucket, ObjectStream
+
+
+class CachedImmutableBucket(IBucket):
+    def __init__(self, cache: IBucket, main: IBucket) -> None:
+        self._cache = cache
+        self._main = main
+
+    def get_object(self, name: PurePosixPath | str) -> bytes:
+        try:
+            return self._cache.get_object(name)
+        except FileNotFoundError:
+            _content = self._main.get_object(name)
+            self._cache.put_object(name, _content)
+            return _content
+
+    def get_object_stream(self, name: PurePosixPath | str) -> ObjectStream:
+        try:
+            return self._cache.get_object_stream(name)
+        except FileNotFoundError:
+            with self._main.get_object_stream(name) as stream:
+                self._cache.put_object_stream(name, stream)
+            return self._cache.get_object_stream(name)
+
+    def put_object(self, name: PurePosixPath | str, content: Union[str, bytes, bytearray]) -> None:
+        raise io.UnsupportedOperation("put_object is not supported for CachedImmutableMinioObjectStorage")
+
+    def put_object_stream(self, name: PurePosixPath | str, stream: BinaryIO) -> None:
+        raise io.UnsupportedOperation("put_object_stream is not supported for CachedImmutableMinioObjectStorage")
+
+    def list_objects(self, prefix: PurePosixPath | str = "") -> slist[PurePosixPath]:
+        return self._main.list_objects(prefix)
+
+    def shallow_list_objects(self, prefix: PurePosixPath | str = "") -> ShallowListing:
+        return self._main.shallow_list_objects(prefix)
+
+    def exists(self, name: PurePosixPath | str) -> bool:
+        return self._cache.exists(name) or self._main.exists(name)
+
+    def remove_objects(self, names: Iterable[PurePosixPath | str]) -> slist[DeleteError]:
+        raise io.UnsupportedOperation("remove_objects is not supported for CachedImmutableMinioObjectStorage")
+
+    @classmethod
+    def build_from_fs(cls, cache_root: Path, main: IBucket) -> "CachedImmutableBucket":
+        cache_bucket = AppendOnlyFSBucket.build(cache_root)
+        return CachedImmutableBucket(cache=cache_bucket, main=main)
