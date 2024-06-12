59183c9b5f6ad4c3df786d3dc637a028fdabe54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 19:04:37 +0000

    Add package theostekno/1.4/theostekno-1.4.tar.gz

---
+++ packages/theostekno/theostekno-1.4.tar.gz/theostekno-1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='theostekno',
+    version='1.4',
+    packages=find_packages(),
+    author='Emre Tekno',
+    author_email='theostekno@gmail.com',
+    description='A simple random number generator library',
+    long_description='Your long description here',
+    long_description_content_type='text/markdown',
+    url='https://github.com/emretecno/theos-ai',
+    keywords=['random', 'number', 'generator', 'library'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6'
+)
