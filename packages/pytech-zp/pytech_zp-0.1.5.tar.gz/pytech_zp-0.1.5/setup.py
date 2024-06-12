4f83dc8c8d50d49314fb5470635d3f33f9b32360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:04:59 +0000

    Add package pytech-zp/0.1.5/pytech_zp-0.1.5.tar.gz

---
+++ packages/pytech-zp/pytech_zp-0.1.5.tar.gz/pytech_zp-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+  
+setup(  
+    name='pytech-zp',  
+    version='0.1.5',  
+    author='Peng Zhao',
+    author_email='zhaokehan86@163.com',
+    description='Python function package of technical indicators and patterns implemented by C++',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'pytech': ['*.pyd']},
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    license=open('LICENSE').read(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pyodbc'],
+    # 其他必要信息，如 author, description, license 等  
+)
