f1936e103ccbba9cf3e0b2e9873579bdbf43d5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:09 +0000

    Add package quickQrLib/0.2.38/quickQrLib-0.2.38.tar.gz

---
+++ packages/quickQrLib/quickQrLib-0.2.38.tar.gz/quickQrLib-0.2.38/quickQrLib/auth_util/authenticate_jwt.py
@@ -0,0 +1,30 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.state import token_backend
+from rest_framework.exceptions import AuthenticationFailed
+from auth_n_auth.models import AppUser
+
+class CustomJWTAuthentication(JWTAuthentication):
+    def authenticate(self, request, url):
+        print(f"Request in CustomJWTAuthentication: {request}")
+        header = self.get_header(request)
+        if header is None:
+            raise AuthenticationFailed("Authorization credentials were not provided")
+
+        raw_token = self.get_raw_token(header)
+        if raw_token is None:
+            raise AuthenticationFailed("Authorization credentials were not provided")
+        try:
+            validated_token = token_backend.decode(raw_token, verify=True)
+        except Exception as e:
+            raise InvalidToken(e)
+        
+        # return validated_token.get("id",None), validated_token
+
+        try:
+            # Retrieve the user using the `emp_id`
+            user = AppUser.objects.get(id=id)
+        except AppUser.DoesNotExist:
+            raise AuthenticationFailed("User not found")
+
+        return (user, validated_token)  # Return the user and token
