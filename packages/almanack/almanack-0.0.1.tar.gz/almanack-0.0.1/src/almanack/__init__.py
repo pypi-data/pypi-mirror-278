cd312152a16a11e5c0567795e86b5b0e4ac3f146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:04:48 +0000

    Add package almanack/0.0.1/almanack-0.0.1.tar.gz

---
+++ packages/almanack/almanack-0.0.1.tar.gz/almanack-0.0.1/src/almanack/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py for software gardening almanack python package
+
+from .book import read
+
+# note: version placeholder is updated during build
+# by poetry-dynamic-versioning.
+__version__ = "0.0.1"
