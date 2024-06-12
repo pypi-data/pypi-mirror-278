c7781c9883e2a977dcfee0ffc8ac7af26f543d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:04:39 +0000

    Add package fa-cli/1.1.3/fa-cli-1.1.3.tar.gz

---
+++ packages/fa-cli/fa-cli-1.1.3.tar.gz/fa-cli-1.1.3/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="fa-cli",
+    version="1.1.3",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "typer[all]",
+        "rich",
+        "gitpython"
+    ],
+    entry_points={
+        "console_scripts": [
+            "fa=fa_cli.main:app",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
