f7877eeed6b2c524679fd30fbc09070b47213cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 19:05:01 +0000

    Add package edgegraph/0.4.0/edgegraph-0.4.0-py3-none-any.whl

---
+++ packages/edgegraph/edgegraph-0.4.0-py3-none-any.whl/edgegraph/structure/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Holds the classes that form graphs.
+"""
+
+from .base import BaseObject
+from .vertex import Vertex
+from .universe import Universe
+from .link import Link
+from .twoendedlink import TwoEndedLink
+from .undirectededge import UnDirectedEdge
+from .directededge import DirectedEdge
+
