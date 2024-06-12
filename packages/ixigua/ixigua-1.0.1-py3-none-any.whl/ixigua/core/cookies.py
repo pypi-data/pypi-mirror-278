401d7501dbcfe8c6819defc470de4c77f641a1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:05:18 +0000

    Add package ixigua/1.0.1/ixigua-1.0.1-py3-none-any.whl

---
+++ packages/ixigua/ixigua-1.0.1-py3-none-any.whl/ixigua/core/cookies.py
@@ -0,0 +1,12 @@
+"""
+使用该cookies，可以获取页面的`_SSR_HYDRATED_DATA`
+
+参考：https://github.com/soimort/you-get/blob/29f513821df4b0ec3ed0b514a0897f8c336b51e7/src/you_get/extractors/ixigua.py
+"""
+
+cookies = {
+    'MONITOR_WEB_ID': '7892c49b-296e-4499-8704-e47c1b15123',
+    '__ac_nonce': '060d88ff000a75e8d17eb',
+    '__ac_signature': '_02B4Z6wo100f01kX9ZpgAAIDAKIBBQUIPYT5F2WIAAPG2ad',
+    'ttwid': '1%7CcIsVF_3vqSIk4XErhPB0H2VaTxT0tdsTMRbMjrJOPN8%7C1624806049%7C08ce7dd6f7d20506a41ba0a331ef96a6505d96731e6ad9f6c8c709f53f227ab1'
+}
---
+++ packages/ixigua/ixigua-1.0.1-py3-none-any.whl/ixigua/utils/api.py
@@ -0,0 +1,29 @@
+from webrequests import WebRequest
+
+
+def get_series_list(pseries_id, start=0, end=None, limit=30):
+    """
+    根据合集ID，获取该合集的视频列表
+
+    https://www.ixigua.com/api/videov2/pseries_more_v2?pSeriesId=6988400843246535199&rank=990&tailCount=30
+    rank -- skip
+    tailCount -- limit
+
+    Refer: https://www.ixigua.com
+    """
+    url = 'https://www.ixigua.com/api/videov2/pseries_more_v2'
+
+    headers = {'Referer': 'https://www.ixigua.com'}
+
+    for rank in range(start, end, limit):
+        payload = {
+            'pSeriesId': pseries_id,
+            'rank': rank,
+            'tailCount': end - start + 1 if limit > (end - start + 1) else limit,
+        }
+
+        response = WebRequest.get_response(url, params=payload, headers=headers)
+        data = response.json()['data']
+
+        for item in data:
+            yield item
