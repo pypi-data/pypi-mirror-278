33f48ce97732e901dec00182621b2b483dc70822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:05:11 +0000

    Add package jupyter-server-proxy/3.2.4/jupyter_server_proxy-3.2.4.tar.gz

---
+++ packages/jupyter-server-proxy/jupyter_server_proxy-3.2.4.tar.gz/jupyter_server_proxy-3.2.4/jupyterlab-server-proxy/lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the jupyterlab-server-proxy extension.
+ */
+declare const extension: JupyterFrontEndPlugin<void>;
+export default extension;
