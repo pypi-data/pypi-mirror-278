6032a96c2b310c05d310bc4b12e3fac5c32e397c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:05:43 +0000

    Add package anonymity-api/0.4.0/anonymity_api-0.4.0.tar.gz

---
+++ packages/anonymity-api/anonymity_api-0.4.0.tar.gz/anonymity_api-0.4.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'anonymity_api',
+    version= '0.4.0',
+    packages= find_packages(),
+    install_requires=[
+        
+    ],
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst'
+)
