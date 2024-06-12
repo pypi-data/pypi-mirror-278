91e94ebc5c6ddb1b2832153b58b63eaa7b3aedbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:04:33 +0000

    Add package pythonLibrary-Test/0.0.1/pythonLibrary_Test-0.0.1.tar.gz

---
+++ packages/pythonLibrary-Test/pythonLibrary_Test-0.0.1.tar.gz/pythonLibrary_Test-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+	
+
+setup(
+	name = 'pythonLibrary_Test',
+	version = '0.0.1',	
+	packages = find_packages(),
+	install_requires = [
+	#Add dependencies here.
+	#e.g. "numpy >= 1.11.1'
+	],
+)
