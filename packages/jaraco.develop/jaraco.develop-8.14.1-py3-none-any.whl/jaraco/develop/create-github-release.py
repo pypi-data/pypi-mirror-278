576615cea3685f3e922d8ac0a625aa15622d8621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:04:51 +0000

    Add package jaraco.develop/8.14.1/jaraco.develop-8.14.1-py3-none-any.whl

---
+++ packages/jaraco.develop/jaraco.develop-8.14.1-py3-none-any.whl/jaraco/develop/create-github-release.py
@@ -0,0 +1,10 @@
+import autocommand
+
+from . import github
+from . import repo
+
+
+@autocommand.autocommand(__name__)
+def run(project: github.Repo = github.Repo.detect()):
+    md = repo.get_project_metadata()
+    project.create_release(tag=f'v{md.version}')
---
+++ packages/jaraco.develop/jaraco.develop-8.14.1-py3-none-any.whl/jaraco/develop/print-meta.py
@@ -0,0 +1,8 @@
+import autocommand
+from build.util import project_wheel_metadata
+
+
+@autocommand.autocommand(__name__)
+def main(path='.', field='Requires-Dist'):
+    for spec in project_wheel_metadata(path).get_all(field):
+        print(spec)
