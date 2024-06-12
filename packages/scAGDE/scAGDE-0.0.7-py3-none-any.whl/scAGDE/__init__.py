764df348d114537a36b7121ede9ca9de0b7ac5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 19:04:30 +0000

    Add package scAGDE/0.0.7/scAGDE-0.0.7-py3-none-any.whl

---
+++ packages/scAGDE/scAGDE-0.0.7-py3-none-any.whl/scAGDE/__init__.py
@@ -0,0 +1,11 @@
+"""
+@Project ：code 
+@File    ：__init__.py.py
+@IDE     ：PyCharm 
+@Author  ：Hao Gaoyang
+@Date    ：2024/6/12 11:36
+@Content ：
+"""
+import scAGDE.trainer
+
+Trainer = scAGDE.trainer.Trainer
