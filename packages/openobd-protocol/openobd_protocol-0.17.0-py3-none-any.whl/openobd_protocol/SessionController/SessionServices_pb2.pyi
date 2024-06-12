0f8da1cc0c9512cac7a7696e9280d8ca1ff4cd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:06 +0000

    Add package openobd-protocol/0.17.0/openobd_protocol-0.17.0-py3-none-any.whl

---
+++ packages/openobd-protocol/openobd_protocol-0.17.0-py3-none-any.whl/openobd_protocol/SessionController/SessionServices_pb2.pyi
@@ -0,0 +1,7 @@
+from openobd_protocol import Status_pb2 as _Status_pb2
+from openobd_protocol.SessionController import Session_pb2 as _Session_pb2
+from openobd_protocol import BasicResponse_pb2 as _BasicResponse_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
---
+++ packages/openobd-protocol/openobd_protocol-0.17.0-py3-none-any.whl/openobd_protocol/UserInterface/UserInterfaceServices_pb2.pyi
@@ -0,0 +1,6 @@
+from openobd_protocol.UserInterface import UserInterface_pb2 as _UserInterface_pb2
+from openobd_protocol import Status_pb2 as _Status_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
