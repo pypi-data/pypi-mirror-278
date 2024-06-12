b144653641c124691fd9bfa7edff6cbd683af56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 18:05:07 +0000

    Add package escriptorium-collate/0.1.3/escriptorium_collate-0.1.3.tar.gz

---
+++ packages/escriptorium-collate/escriptorium_collate-0.1.3.tar.gz/escriptorium_collate-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="escriptorium-collate",
+    version="0.1.3",
+    author="Osama Eshera",
+    author_email="osama.eshera@gmail.com",
+    description="A Python library for collating eScriptorium documents.",
+    url="https://github.com/oeshera/escriptorium-collate",
+    packages=find_packages(),
+    install_requires=[
+        "escriptorium-connector",
+        "minineedle",
+        "nltk",
+        "pydantic",
+    ],
+    setup_requires=["pytest-runner", "flake8"],
+    tests_require=["pytest"],
+)
