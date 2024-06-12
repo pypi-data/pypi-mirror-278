4a1d6f5d8bb27bde9edb8fe94289b6ad59a65193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:04:47 +0000

    Add package dooray-api-wrapper/0.0.1/dooray_api_wrapper-0.0.1-py3-none-any.whl

---
+++ packages/dooray-api-wrapper/dooray_api_wrapper-0.0.1-py3-none-any.whl/dooray-api-wrapper/dooray/project.py
@@ -0,0 +1,41 @@
+from typing import Dict, List, Union, Optional
+
+from src.common import dooray_request
+from src.const.const import Scope, Type, State
+from src.structure import response_result, response_header
+
+
+def get_project_list(
+    page: int = 0,
+    size: int = 0,
+    scope: Scope = Scope.PUBLIC,
+    type: Type = Type.PUBLIC,
+    state: State = State.ACTIVE,
+):
+    end_point = f"/project/v1/projects"
+    params = {
+        "page": page,
+        "size": size,
+        "scope": scope.value,
+        "type": type.value,
+        "state": state.value,
+    }
+    return dooray_request.dooray_get(end_point, params)
+
+
+def get_project(project_id: str):
+    end_point = f"/project/v1/projects/{project_id}"
+    return dooray_request.dooray_get(end_point)
+
+
+def response(
+    data: Dict,
+) -> Optional[
+    List[Union[response_header.ResponseHeader, response_result.ProjectResult]]
+]:
+    header = data["header"]
+    result = {"result": data["result"]}
+    return [
+        response_header.ResponseHeader(**header),
+        response_result.ProjectResult(**result),
+    ]
---
+++ packages/dooray-api-wrapper/dooray_api_wrapper-0.0.1-py3-none-any.whl/dooray-api-wrapper/dooray/wiki.py
@@ -0,0 +1,52 @@
+from typing import Dict, List, Union, Optional
+
+from src.common import dooray_request
+from src.const.const import Scope, Type, State
+from src.structure import response_result, response_header
+
+
+def get_wiki_list(page: int = 0, size: int = 0) -> Optional[response_result.WikiResult]:
+    """접근 가능한 위키 목록을 조회합니다."""
+    end_point = f"/wiki/v1/wikis"
+    params = {
+        "page": page,
+        "size": size,
+    }
+    response = dooray_request.dooray_get(end_point, params)
+    if response is None:
+        return None
+
+    header = response["header"]
+    result = {"result": response["result"]}
+    return response_result.WikiResult(**result)
+
+
+def get_wiki_sub_pages(
+    wiki_id: str, parentPageId: Optional[str] = None
+) -> Optional[response_result.WikiPageResult]:
+    """특정 위키 페이지의 하위 페이지들을 조회합니다."""
+    end_point = f"/wiki/v1/wikis/{wiki_id}/pages"
+    params = {
+        "parentPageId": parentPageId,
+    }
+    response = dooray_request.dooray_get(end_point, params)
+    if response is None:
+        return None
+
+    header = response["header"]
+    result = {"result": response["result"]}
+    return response_result.WikiPageResult(**result)
+
+
+def get_wiki_page(
+    wiki_id: str, pageId: str
+) -> Optional[response_result.WikiPageResultItem]:
+    """특정 위키 페이지를 조회합니다."""
+    end_point = f"/wiki/v1/wikis/{wiki_id}/pages/{pageId}"
+    response = dooray_request.dooray_get(end_point)
+    if response is None:
+        return None
+
+    header = response["header"]
+    result = response["result"]
+    return response_result.WikiPageResultItem(**result)
