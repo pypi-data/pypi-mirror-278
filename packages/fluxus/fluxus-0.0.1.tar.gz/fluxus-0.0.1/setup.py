88f7af5cc262a59e25015c0a3aa0f7f8c1c8ef49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:05:02 +0000

    Add package fluxus/0.0.1/fluxus-0.0.1.tar.gz

---
+++ packages/fluxus/fluxus-0.0.1.tar.gz/fluxus-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="fluxus",
+    version="0.0.1",
+    author="BCG X",
+    #author_email="your.email@example.com",
+    description="Coming soon!",
+    # long_description=open("README.md").read(),
+    # long_description_content_type="text/markdown",
+    url="https://github.com/BCG-X-Official",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
