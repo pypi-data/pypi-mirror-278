0ffb8959c3c7275901c2236560a3ac1878754cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:06:55 +0000

    Add package cloudsmith-cli/1.2.4/cloudsmith_cli-1.2.4-py2.py3-none-any.whl

---
+++ packages/cloudsmith-cli/cloudsmith_cli-1.2.4-py2.py3-none-any.whl/cloudsmith_cli/cli/commands/__init__.py
@@ -0,0 +1,25 @@
+"""CLI/Commands - Import all commands."""
+
+from . import (
+    check,
+    copy,
+    delete,
+    dependencies,
+    docs,
+    entitlements,
+    help_,
+    list_,
+    login,
+    metrics,
+    move,
+    policy,
+    push,
+    quarantine,
+    quota,
+    repos,
+    resync,
+    status,
+    tags,
+    upstream,
+    whoami,
+)
