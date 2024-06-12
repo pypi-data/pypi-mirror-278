2ac9fbd3835b832ed9480ae7d1c9c3ac29d1641c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 17:04:51 +0000

    Add package nextline/0.7.11/nextline-0.7.11-py3-none-any.whl

---
+++ packages/nextline/nextline-0.7.11-py3-none-any.whl/nextline/events.py
@@ -0,0 +1,94 @@
+import datetime
+from dataclasses import dataclass
+from typing import Optional
+
+from nextline.types import PromptNo, RunNo, TaskNo, ThreadNo, TraceCallNo, TraceNo
+
+
+@dataclass
+class Event:
+    pass
+
+
+@dataclass
+class OnStartTrace(Event):
+    started_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    thread_no: ThreadNo
+    task_no: Optional[TaskNo]
+
+
+@dataclass
+class OnEndTrace(Event):
+    ended_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+
+
+@dataclass
+class OnStartTraceCall(Event):
+    started_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    trace_call_no: TraceCallNo
+    file_name: str
+    line_no: int
+    frame_object_id: int
+    event: str
+
+
+@dataclass
+class OnEndTraceCall(Event):
+    ended_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    trace_call_no: TraceCallNo
+
+
+@dataclass
+class OnStartCmdloop(Event):
+    started_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    trace_call_no: TraceCallNo
+
+
+@dataclass
+class OnEndCmdloop(Event):
+    ended_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    trace_call_no: TraceCallNo
+
+
+@dataclass
+class OnStartPrompt(Event):
+    started_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    trace_call_no: TraceCallNo
+    prompt_no: PromptNo
+    prompt_text: str
+    file_name: str
+    line_no: int
+    frame_object_id: int
+    event: str
+
+
+@dataclass
+class OnEndPrompt(Event):
+    ended_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    trace_call_no: TraceCallNo
+    prompt_no: PromptNo
+    command: str
+
+
+@dataclass
+class OnWriteStdout(Event):
+    written_at: datetime.datetime
+    run_no: RunNo
+    trace_no: TraceNo
+    text: str
