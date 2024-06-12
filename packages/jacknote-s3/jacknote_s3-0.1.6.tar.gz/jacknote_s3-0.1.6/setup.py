5e38aa7afe7b19891e5dcd240b32b330afe8821e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:04:57 +0000

    Add package jacknote-s3/0.1.6/jacknote_s3-0.1.6.tar.gz

---
+++ packages/jacknote-s3/jacknote_s3-0.1.6.tar.gz/jacknote_s3-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jacknote_s3",
+    version="0.1.6",
+    packages=find_packages(),
+    install_requires=[],
+    author="jdev",
+    author_email="jdev@radioframenetworks.com",
+    description="Jacknote",
+    keywords="",
+    url="https://radioframenetworks.com.com/kacknote",
+)
