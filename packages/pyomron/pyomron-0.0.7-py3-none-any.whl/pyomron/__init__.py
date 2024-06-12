31042cf3fcc9b264991086248f36ffb4e7141f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:04:47 +0000

    Add package pyomron/0.0.7/pyomron-0.0.7-py3-none-any.whl

---
+++ packages/pyomron/pyomron-0.0.7-py3-none-any.whl/pyomron/__init__.py
@@ -0,0 +1,16 @@
+# type: ignore[attr-defined]
+"""Python API for acquisition and control of OMRON G3PW Power Controller."""
+
+import sys
+
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
