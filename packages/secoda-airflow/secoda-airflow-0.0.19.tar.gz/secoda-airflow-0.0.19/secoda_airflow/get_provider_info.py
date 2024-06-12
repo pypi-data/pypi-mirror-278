f955c478d727b51537b0b6fb2d6c56f5ba008d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:04:40 +0000

    Add package secoda-airflow/0.0.19/secoda-airflow-0.0.19.tar.gz

---
+++ packages/secoda-airflow/secoda-airflow-0.0.19.tar.gz/secoda-airflow-0.0.19/secoda_airflow/get_provider_info.py
@@ -0,0 +1,46 @@
+def get_provider_info():
+    return {
+        "name": "Secoda",
+        "description": "`Secoda <https://www.secoda.com/>`__\n",
+        "integrations": [
+            {
+                "integration-name": "Secoda",
+                "external-doc-url": "https://www.secoda.com/",
+                "tags": ["service"],
+            }
+        ],
+        "connection-types": [
+            {
+                "hook-class-name": "secoda_airflow.hooks.SecodaHook",
+                "connection-type": "secoda",
+                "connection-name": "Secoda Connection",
+                "connection-fields": [
+                    {
+                        "label": "Host",
+                        "key": "host",
+                        "type": "string",
+                        "description": "Host for the Secoda service",
+                    },
+                    {
+                        "label": "API Key",
+                        "key": "password",
+                        "type": "password",
+                        "description": "API key for the Secoda service",
+                    },
+                    {
+                        "label": "Integration ID",
+                        "key": "extra__integration_id",
+                        "type": "string",
+                        "description": "Integration ID for the Secoda service",
+                    },
+                ],
+            }
+        ],
+        "hooks": [
+            {
+                "integration-name": "Secoda",
+                "python-modules": ["secoda_airflow.hooks"],
+            }
+        ],
+        "package-name": "secoda-airflow",
+    }
