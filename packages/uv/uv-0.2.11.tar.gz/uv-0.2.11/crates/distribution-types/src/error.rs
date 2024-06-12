e77602e8fc72f4fab65a98ba622e54220c154971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 18:05:20 +0000

    Add package uv/0.2.11/uv-0.2.11.tar.gz

---
+++ packages/uv/uv-0.2.11.tar.gz/uv-0.2.11/crates/distribution-types/src/error.rs
@@ -0,0 +1,34 @@
+use url::Url;
+
+use pep508_rs::VerbatimUrl;
+use uv_normalize::PackageName;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error(transparent)]
+    Utf8(#[from] std::string::FromUtf8Error),
+
+    #[error(transparent)]
+    WheelFilename(#[from] distribution_filename::WheelFilenameError),
+
+    #[error("Unable to extract file path from URL: {0}")]
+    MissingFilePath(Url),
+
+    #[error("Could not extract path segments from URL: {0}")]
+    MissingPathSegments(Url),
+
+    #[error("Distribution not found at: {0}")]
+    NotFound(Url),
+
+    #[error("Unsupported scheme `{0}` on URL: {1} ({2})")]
+    UnsupportedScheme(String, String, String),
+
+    #[error("Requested package name `{0}` does not match `{1}` in the distribution filename: {2}")]
+    PackageNameMismatch(PackageName, PackageName, String),
+
+    #[error("Only directories can be installed as editable, not filenames: `{0}`")]
+    EditableFile(VerbatimUrl),
+}
