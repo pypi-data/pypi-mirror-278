27ffb525afa704b80f79a069e5660a7a1fc6554f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 17:04:51 +0000

    Add package yxseq/0.0.3/yxseq-0.0.3.tar.gz

---
+++ packages/yxseq/yxseq-0.0.3.tar.gz/yxseq-0.0.3/yxseq/versions.py
@@ -0,0 +1,25 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. debug",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. add cli",
+            "2. add fq2fa cmd",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. init",
+        ],
+    },
+]
