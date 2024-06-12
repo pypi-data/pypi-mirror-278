5d96445ee7ab8352ba0bad391234a9b8501796af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:05:09 +0000

    Add package pytest-common-subject/1.1.1/pytest_common_subject-1.1.1-py3-none-any.whl

---
+++ packages/pytest-common-subject/pytest_common_subject-1.1.1-py3-none-any.whl/pytest_common_subject/__init__.py
@@ -0,0 +1,19 @@
+import pkg_resources
+
+from .exceptions import DeferredCommonSubjectRvalUsage
+from .fixtures import precondition_fixture
+from .mixins import (
+    CommonSubjectTestMixin,
+    AsyncCommonSubjectTestMixin,
+    WithCommonSubjectDeferred,
+)
+
+__version__ = pkg_resources.get_distribution('pytest-common-subject').version
+
+__all__ = [
+    'DeferredCommonSubjectRvalUsage',
+    'precondition_fixture',
+    'CommonSubjectTestMixin',
+    'AsyncCommonSubjectTestMixin',
+    'WithCommonSubjectDeferred',
+]
