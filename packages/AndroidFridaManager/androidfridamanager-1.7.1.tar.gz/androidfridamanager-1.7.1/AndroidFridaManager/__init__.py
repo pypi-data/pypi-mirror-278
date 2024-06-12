1f01b7374d1565329c55eb38fc6fe417ecc3874f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:03:54 +0000

    Add package AndroidFridaManager/1.7.1/androidfridamanager-1.7.1.tar.gz

---
+++ packages/AndroidFridaManager/androidfridamanager-1.7.1.tar.gz/androidfridamanager-1.7.1/AndroidFridaManager/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .FridaManager import FridaManager, main
+from .job import Job, FridaBasedException
+from .job_manager import JobManager
+
+__version__ = "1.7.1"
