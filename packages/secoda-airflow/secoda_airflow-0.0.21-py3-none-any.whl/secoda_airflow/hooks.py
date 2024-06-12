b4df9d4be175a33c404c43c551e02b742844eb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:04:39 +0000

    Add package secoda-airflow/0.0.21/secoda_airflow-0.0.21-py3-none-any.whl

---
+++ packages/secoda-airflow/secoda_airflow-0.0.21-py3-none-any.whl/secoda_airflow/hooks.py
@@ -0,0 +1,68 @@
+import requests
+
+from airflow.exceptions import AirflowException
+from airflow.hooks.base import BaseHook
+
+
+class SecodaHook(BaseHook):
+    conn_name_attr = "secoda_conn_id"
+    default_conn_name = "secoda_default"
+    conn_type = "secoda"
+    hook_name = "Secoda Hook"
+
+    def __init__(self, context, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        secoda_conn_id = self.default_conn_name
+        custom_conn_id = context["dag"].params.get("secoda_conn_id")
+        if custom_conn_id:
+            secoda_conn_id = custom_conn_id
+
+        conn = self.get_connection(secoda_conn_id)
+        self.api_key = conn.password
+        self.host = conn.host or "https://api.secoda.com"
+        self.integration_id = conn.extra_dejson.get("integration_id")
+
+        if not self.api_key:
+            raise AirflowException("No API key provided")
+
+        if not self.integration_id:
+            raise AirflowException("No integration ID provided")
+
+    @property
+    def sanitized_host(self) -> str:
+        if self.host.endswith("/"):
+            return self.host[:-1]
+
+        return self.host
+
+    def post(self, endpoint: str, data: dict) -> requests.Response:
+        return requests.post(
+            f"{self.sanitized_host}{endpoint}",
+            headers={"Authorization": f"Bearer {self.api_key}"},
+            json=data,
+        )
+
+    def post_task_success(self, context):
+        return self.post(
+            f"/integration/run_logs/{self.integration_id}/ingest/",
+            context,
+        )
+
+    def post_dag_success(self, context):
+        return self.post(
+            f"/integration/run_logs/{self.integration_id}/ingest/",
+            context,
+        )
+
+    def post_task_failure(self, context):
+        return self.post(
+            f"/integration/run_logs/{self.integration_id}/ingest/",
+            context,
+        )
+
+    def post_dag_failure(self, context):
+        return self.post(
+            f"/integration/run_logs/{self.integration_id}/ingest/",
+            context,
+        )
