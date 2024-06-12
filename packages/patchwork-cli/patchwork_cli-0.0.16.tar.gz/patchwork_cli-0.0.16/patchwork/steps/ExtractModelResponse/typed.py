e4030b2b29cce09725033cd97ae0b8683b56cc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:04:42 +0000

    Add package patchwork-cli/0.0.16/patchwork_cli-0.0.16.tar.gz

---
+++ packages/patchwork-cli/patchwork_cli-0.0.16.tar.gz/patchwork_cli-0.0.16/patchwork/steps/ExtractModelResponse/typed.py
@@ -0,0 +1,10 @@
+from typing_extensions import Iterable, NotRequired, TypedDict
+
+
+class ExtractModelResponseInputs(TypedDict):
+    openai_responses: Iterable[str]
+    response_partitions: NotRequired[dict[str, list[str]]]
+
+
+class ExtractModelResponseOutputs(TypedDict):
+    extracted_responses: list[dict[str, str]]
