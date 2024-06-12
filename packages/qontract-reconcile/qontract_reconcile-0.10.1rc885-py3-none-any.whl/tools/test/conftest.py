cb60be669339e5b29aff1b87169fa3f646c8606a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 19:04:34 +0000

    Add package qontract-reconcile/0.10.1rc885/qontract_reconcile-0.10.1rc885-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc885-py3-none-any.whl/tools/test/conftest.py
@@ -0,0 +1,37 @@
+from collections.abc import (
+    Callable,
+    MutableMapping,
+)
+from typing import Any
+
+import pytest
+from pydantic import BaseModel
+from pydantic.error_wrappers import ValidationError
+
+from reconcile.utils.models import data_default_none
+
+
+class GQLClassFactoryError(Exception):
+    pass
+
+
+@pytest.fixture
+def gql_class_factory() -> (
+    Callable[
+        [type[BaseModel], MutableMapping[str, Any] | None],
+        BaseModel,
+    ]
+):
+    """Create a GQL class from a fixture and set default values to None."""
+
+    def _gql_class_factory(
+        klass: type[BaseModel], data: MutableMapping[str, Any] | None = None
+    ) -> BaseModel:
+        try:
+            return klass(**data_default_none(klass, data or {}))
+        except ValidationError as e:
+            msg = "[gql_class_factory] Your given data does not match the class ...\n"
+            msg += "\n".join([str(m) for m in list(e.raw_errors)])
+            raise GQLClassFactoryError(msg) from e
+
+    return _gql_class_factory
