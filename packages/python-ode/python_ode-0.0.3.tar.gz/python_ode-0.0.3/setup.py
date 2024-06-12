7ec9f06d45dc747314078468621c265d937f6e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 17:04:51 +0000

    Add package python-ode/0.0.3/python_ode-0.0.3.tar.gz

---
+++ packages/python-ode/python_ode-0.0.3.tar.gz/python_ode-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='python-ode',
+    packages=find_packages(include=['ode']),
+    version='0.0.3',
+    description='A framework to drive a clean code',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='v2SoftwareHouse',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
