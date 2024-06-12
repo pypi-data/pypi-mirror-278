fb802a39c156aeabe08e0d7606b0eca0596d6988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:04:38 +0000

    Add package url-validatorIndx/1.1.2/url_validatorIndx-1.1.2.tar.gz

---
+++ packages/url-validatorIndx/url_validatorIndx-1.1.2.tar.gz/url_validatorIndx-1.1.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools # type: ignore
+
+# Reads the content of your README.md into a variable to be used in the setup below
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # name='anutoolbox123',                           # should match the package folder
+    # packages=['anutoolbox123'],  
+    name='url_validatorIndx',                           # should match the package folder
+    packages=['url_validatorIndx'],                     # should match the package folder
+    version='1.1.2',                                # important for updates
+    license='MIT',                                  # should match your chosen license
+    description='Testing installation of Package',
+    long_description=long_description,              # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    author='Rishabh K',
+    author_email='rshb.kapoor@gmail.com',
+    # url='https://github.com/mike-huls/toolbox_public', 
+    # project_urls = {                                # Optional
+    #     "Bug Tracker": "https://github.com/mike-huls/toolbox_public/issues"
+    # },
+    install_requires=['requests','pyarmor','pyarmor-webui','pycryptodome'],                  # list all packages that your package uses
+    keywords=["pypi", "url_validatorIndx", "tutorial"], #descriptive meta-data
+    classifiers=[                                   # https://pypi.org/classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
