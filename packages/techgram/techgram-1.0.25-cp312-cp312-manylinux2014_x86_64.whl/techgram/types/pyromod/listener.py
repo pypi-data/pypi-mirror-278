21bdfcf24869431f09815de8db97bbd9767e11bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:04:35 +0000

    Add package techgram/1.0.25/techgram-1.0.25-cp312-cp312-manylinux2014_x86_64.whl

---
+++ packages/techgram/techgram-1.0.25-cp312-cp312-manylinux2014_x86_64.whl/techgram/types/pyromod/listener.py
@@ -0,0 +1,18 @@
+from asyncio import Future
+from dataclasses import dataclass
+from typing import Callable
+
+import pyrogram
+
+from .identifier import Identifier
+from .listenerTypes import ListenerTypes
+
+
+@dataclass
+class Listener:
+    listener_type: ListenerTypes
+    filters: "pyrogram.filters.Filter"
+    unallowed_click_alert: bool
+    identifier: Identifier
+    future: Future = None
+    callback: Callable = None
