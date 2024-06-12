18201f7e371d6165be5e8d216106253c4f1b642d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 18:04:31 +0000

    Add package bioinformatics-script/0.1.0/bioinformatics_script-0.1.0-py3-none-any.whl

---
+++ packages/bioinformatics-script/bioinformatics_script-0.1.0-py3-none-any.whl/AnnotationCheckerWithStructure/__init__.py
@@ -0,0 +1,5 @@
+# bioinformatics_script/__init__.py
+
+from .main import main
+
+__all__ = ["main"]
---
+++ packages/bioinformatics-script/bioinformatics_script-0.1.0-py3-none-any.whl/AnnotationCheckerWithStructure/main.py
@@ -0,0 +1,7 @@
+# bioinformatics_script/main.py
+
+def main():
+    print("Hello, Bioinformatics!")
+
+if __name__ == "__main__":
+    main()
