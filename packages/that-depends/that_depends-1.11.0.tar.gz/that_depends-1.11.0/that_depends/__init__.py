6eda642e2106776165f498411a31973f22eefeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:04:59 +0000

    Add package that-depends/1.11.0/that_depends-1.11.0.tar.gz

---
+++ packages/that-depends/that_depends-1.11.0.tar.gz/that_depends-1.11.0/that_depends/__init__.py
@@ -0,0 +1,6 @@
+from that_depends import providers
+from that_depends.container import BaseContainer
+from that_depends.injection import Provide, inject
+
+
+__all__ = ["providers", "BaseContainer", "inject", "Provide"]
