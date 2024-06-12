a1810be8bd53fd0b38adb01f8d597501829ccacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 19:04:59 +0000

    Add package pyerualjetwork/2.0.6/pyerualjetwork-2.0.6.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-2.0.6.tar.gz/pyerualjetwork-2.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.0.6",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Advanced python deep learning library. New features: BINARY INJECTION (OLD) NOW ADDED NEW DIRECT FEATURE INJECTION. AND 'standard_scaler' func. Important Note: If there are any data smaller than 0 among the input data of the entry model, import plan_bi; otherwise, import plan_di. (Documentation in desc. Examples in GİTHUB: https://github.com/HCB06/PyerualJetwork)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
