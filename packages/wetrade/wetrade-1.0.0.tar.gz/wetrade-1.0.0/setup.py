50b159bc22d7079641519b074ec858b95c86758d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:04:02 +0000

    Add package wetrade/1.0.0/wetrade-1.0.0.tar.gz

---
+++ packages/wetrade/wetrade-1.0.0.tar.gz/wetrade-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.rst', 'r') as f:
+  long_description = f.read()
+
+setuptools.setup(
+  name = 'wetrade',
+  version = '1.0.0',
+  author = 'Mason Krause',
+  description = 'An E-Trade python library built for active stock trading',
+  long_description = long_description,
+  packages = setuptools.find_packages(),
+  include_package_data = True,
+  python_requires = '>=3.7',
+  install_requires = [
+    'authlib',
+    'playwright',
+    'urllib3==1.26.16',
+    'xmltodict',
+    'pytz',
+    'pyotp==2.9.0',
+    'google-cloud-logging', 
+    'google-cloud-storage', 
+    'google-cloud-secret-manager',
+    'polars', 
+    'pandas', 
+    'pyarrow'],
+    extras_require={
+      'dev': [
+        'pytest',
+        'pytest-timeout',
+        'sphinx',
+        'sphinx_rtd_theme']},)
