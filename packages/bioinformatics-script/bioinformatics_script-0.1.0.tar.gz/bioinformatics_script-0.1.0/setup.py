624980c18e181ac382aca06be6faedbe2e5d3180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 18:04:31 +0000

    Add package bioinformatics-script/0.1.0/bioinformatics_script-0.1.0.tar.gz

---
+++ packages/bioinformatics-script/bioinformatics_script-0.1.0.tar.gz/bioinformatics_script-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="bioinformatics_script",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=2.0.3"
+    ],
+    entry_points={
+        'console_scripts': [
+            'bioinformatics_script=bioinformatics_script.main:main',
+        ],
+    },
+    author="Andreas Bachler",
+    author_email="Andy.Bachler@gmail.com",
+    description="A simple bioinformatics script.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/yourusername/bioinformatics-script",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
