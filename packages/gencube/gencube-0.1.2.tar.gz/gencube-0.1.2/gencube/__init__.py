99164188ff0e722d184326ea66f5e30c1dc424f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:06:21 +0000

    Add package gencube/0.1.2/gencube-0.1.2.tar.gz

---
+++ packages/gencube/gencube-0.1.2.tar.gz/gencube-0.1.2/gencube/__init__.py
@@ -0,0 +1,14 @@
+
+## 설명 / 삭제 예정 - 모두 필요한가? from gget
+# Add and format time stamp in logging messages
+#import logging
+#logging.basicConfig(
+#    format="%(asctime)s %(levelname)s %(message)s",
+#    level=logging.INFO,
+#    datefmt="%c",
+#)
+# Mute numexpr threads info
+#logging.getLogger("numexpr").setLevel(logging.WARNING)
+#__author__ = """Keun Hong Son"""
+#__email__ = 'newhong@snu.ac.kr'
+#__version__ = '0.1.2'
---
+++ packages/gencube/gencube-0.1.2.tar.gz/gencube-0.1.2/gencube/__main__.py
@@ -0,0 +1,11 @@
+"""
+## 필요한가? from gget
+from .main import main
+
+main()
+"""
+def main():
+    print("GenCube is running")
+
+if __name__ == "__main__":
+    main()
