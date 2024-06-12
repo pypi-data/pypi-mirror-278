b08abd3b9e6518bbe15d3cd2fa304a76e7791df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:04:33 +0000

    Add package sila2-feature-lib/2024.24a2/sila2_feature_lib-2024.24a2.tar.gz

---
+++ packages/sila2-feature-lib/sila2_feature_lib-2024.24a2.tar.gz/sila2_feature_lib-2024.24a2/tests/test_import.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+class TestImport(TestCase):
+    def test_import(self):
+        """Test if the function get_xml can be imported from the package. I.e. package was installed correctly."""
+        from sila2_feature_lib import get_xml
