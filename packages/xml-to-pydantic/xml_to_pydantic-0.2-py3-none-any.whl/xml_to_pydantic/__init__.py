51a74cc4b1fbd2828d5534d8a3ca1a4def3a0e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:05 +0000

    Add package xml-to-pydantic/0.2/xml_to_pydantic-0.2-py3-none-any.whl

---
+++ packages/xml-to-pydantic/xml_to_pydantic-0.2-py3-none-any.whl/xml_to_pydantic/__init__.py
@@ -0,0 +1,12 @@
+from .model import ConfigDict, XmlBaseModel, XmlField, XmlModelError, XmlParsingError
+
+__version__ = "0.2"
+
+__all__ = [
+    "__version__",
+    "ConfigDict",
+    "XmlBaseModel",
+    "XmlField",
+    "XmlModelError",
+    "XmlParsingError",
+]
