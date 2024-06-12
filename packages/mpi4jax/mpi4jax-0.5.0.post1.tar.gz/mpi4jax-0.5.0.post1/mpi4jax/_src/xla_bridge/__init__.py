a73138bc9543863df7d9fe7c5b13e7dacf7dab6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:05:24 +0000

    Add package mpi4jax/0.5.0.post1/mpi4jax-0.5.0.post1.tar.gz

---
+++ packages/mpi4jax/mpi4jax-0.5.0.post1.tar.gz/mpi4jax-0.5.0.post1/mpi4jax/_src/xla_bridge/__init__.py
@@ -0,0 +1,41 @@
+import os
+from jax.lib import xla_client
+
+from . import mpi_xla_bridge
+from . import mpi_xla_bridge_cpu
+
+try:
+    from . import mpi_xla_bridge_gpu
+except ImportError:
+    HAS_GPU_EXT = False
+else:
+    HAS_GPU_EXT = True
+
+try:
+    from . import mpi_xla_bridge_xpu
+except ImportError:
+    HAS_XPU_EXT = False
+else:
+    HAS_XPU_EXT = True
+
+# setup logging
+
+
+def _is_truthy(str_val):
+    return str_val.lower() in ("true", "1", "on")
+
+
+mpi_xla_bridge.set_logging(_is_truthy(os.getenv("MPI4JAX_DEBUG", "")))
+
+
+# register custom call targets
+for name, fn in mpi_xla_bridge_cpu.cpu_custom_call_targets.items():
+    xla_client.register_custom_call_target(name, fn, platform="cpu")
+
+if HAS_GPU_EXT:
+    for name, fn in mpi_xla_bridge_gpu.gpu_custom_call_targets.items():
+        xla_client.register_custom_call_target(name, fn, platform="CUDA")
+
+if HAS_XPU_EXT:
+    for name, fn in mpi_xla_bridge_xpu.xpu_custom_call_targets.items():
+        xla_client.register_custom_call_target(name, fn, platform="SYCL")
