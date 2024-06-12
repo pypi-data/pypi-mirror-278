7c340dcb1b3560399d7eedd80c9dd891078b7663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 17:04:39 +0000

    Add package my-plotly-package/0.1.1/my_plotly_package-0.1.1.tar.gz

---
+++ packages/my-plotly-package/my_plotly_package-0.1.1.tar.gz/my_plotly_package-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_plotly_package',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'plotly>=5.0.0',
+        'pandas>=1.0.0'
+    ],
+    author='Diogo Antonio',
+    author_email='diogoantoniodejesus@hotmail.com',
+    description='Objetivo da criação dessas funções são para que reduza o tempo na contrução e estilização de indicadores para Futuros Dashboards utilizando poucas linhas de codigo.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Diogoa83/biblioteca_python_dash',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+
+
+# pypi-AgEIcHlwaS5vcmcCJGQ5M2NjYTQ1LTY2MzAtNDU4Yi04NTI3LWY0ZmNmZmYzNDVjYQACKlszLCJmMTdlNmI3My1iY2JhLTRmODgtYTNjYi02YzBmYzViZTY0M2QiXQAABiDvSI1zxMXLWmLgBOwygUd93exaGfXNrBwacC7o-eypxg
+
