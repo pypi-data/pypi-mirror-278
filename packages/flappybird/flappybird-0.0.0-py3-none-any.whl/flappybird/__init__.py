50b76efcb13c42afe7b71219c038b4da405d7f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 15:04:34 +0000

    Add package flappybird/0.0.0/flappybird-0.0.0-py3-none-any.whl

---
+++ packages/flappybird/flappybird-0.0.0-py3-none-any.whl/flappybird/__init__.py
@@ -0,0 +1,7 @@
+"""flappybird - Flappy bird, but every sprite is its own window."""
+from __future__ import annotations
+
+
+def greet(name: str) -> str:
+    """Returns a greeting."""
+    return f"Hello {name}!"
