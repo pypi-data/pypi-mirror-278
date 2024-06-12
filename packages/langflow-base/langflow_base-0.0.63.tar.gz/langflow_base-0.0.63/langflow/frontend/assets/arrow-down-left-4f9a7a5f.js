aabd409085970dff7957bf5b62f71bcd66a61056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 16:04:40 +0000

    Add package langflow-base/0.0.63/langflow_base-0.0.63.tar.gz

---
+++ packages/langflow-base/langflow_base-0.0.63.tar.gz/langflow_base-0.0.63/langflow/frontend/assets/arrow-down-left-4f9a7a5f.js
@@ -0,0 +1,6 @@
+import{ee as e}from"./index-43654cc3.js";/**
+ * @license lucide-react v0.394.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("ArrowDownLeft",[["path",{d:"M17 7 7 17",key:"15tmo1"}],["path",{d:"M17 17H7V7",key:"1org7z"}]]);export{t as default};
