2b02d9e58547f5e86f61fe53ae3d6ab2f68924a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:29 +0000

    Add package InvManagement/0.5/invmanagement-0.5.tar.gz

---
+++ packages/InvManagement/invmanagement-0.5.tar.gz/invmanagement-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='InvManagement',
+    version='0.5',
+    packages=find_packages(),
+    description='A simple inventory management system',
+    author='Kyle Tiongson, Hans Neil Emnacin',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
+
