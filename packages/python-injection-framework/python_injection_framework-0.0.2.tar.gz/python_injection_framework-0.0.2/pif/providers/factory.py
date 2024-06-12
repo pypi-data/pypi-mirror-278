b1907e4fb738406930074cbb9c128f22de2feb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:04:56 +0000

    Add package python-injection-framework/0.0.2/python_injection_framework-0.0.2.tar.gz

---
+++ packages/python-injection-framework/python_injection_framework-0.0.2.tar.gz/python_injection_framework-0.0.2/pif/providers/factory.py
@@ -0,0 +1,33 @@
+#                  _   _                 _     _
+#    ___  ___ ___ | |_| |_ ______ _  ___| |__ / |
+#   / __|/ __/ _ \| __| __|_  / _` |/ __| '_ \| |
+#   \__ \ (_| (_) | |_| |_ / / (_| | (__| | | | |
+#   |___/\___\___/ \__|\__/___\__,_|\___|_| |_|_|
+#
+#        Zac Scott (github.com/scottzach1)
+#
+#  https://github.com/scottzach1/python-injector-framework
+
+import functools
+from typing import Callable, TypeVar
+
+from pif.providers.provider import Provider
+from pif.providers.util import intercept_args
+
+__all__ = ("Factory",)
+
+T = TypeVar("T")
+
+
+class Factory(Provider):
+    """
+    Generate a new instance every call.
+    """
+
+    __slots__ = ("_func", "_depends")
+
+    def __init__(self, func: Callable[..., T], *args, **kwargs):
+        self._func = functools.partial(intercept_args(func), *args, **kwargs)
+
+    def _evaluate(self) -> T:
+        return self._func()
