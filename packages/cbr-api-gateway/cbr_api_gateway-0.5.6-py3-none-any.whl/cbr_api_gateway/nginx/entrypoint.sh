9fb5b5b54acfd39a0526a83ade9f612a3cc9b7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 18:04:31 +0000

    Add package cbr-api-gateway/0.5.6/cbr_api_gateway-0.5.6-py3-none-any.whl

---
+++ packages/cbr-api-gateway/cbr_api_gateway-0.5.6-py3-none-any.whl/cbr_api_gateway/nginx/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Starting CBR NGINX API Gateway:"
+# Substitute environment variables in the template and create the final nginx.conf
+envsubst '${FLASK_APP_HOST} ${FASTAPI_APP_HOST}' < ./nginx.conf.template > /etc/nginx/nginx.conf
+
+# Start NGINX
+#cat /etc/nginx/nginx.conf
+nginx -g 'daemon off;'
+
