97853ca4551cd55adff17556f7a8db9c90ef35eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:09 +0000

    Add package spid-cie-oidc/1.4.5/spid_cie_oidc-1.4.5.tar.gz

---
+++ packages/spid-cie-oidc/spid_cie_oidc-1.4.5.tar.gz/spid_cie_oidc-1.4.5/setup.py
@@ -0,0 +1,69 @@
+import re
+import os
+import re
+import sys
+
+from glob import glob
+from setuptools import setup, find_packages
+
+SRC_FOLDER = '.'
+PKG_NAME = 'spid_cie_oidc'
+
+INSTALL_REQUIRES = [
+    "Django>=4.0,<5.0",
+    "cryptojwt>=1.8.2",
+    "pydantic>=1.8.2,<2.0",
+    "pytz>=2021.3",
+    "aiohttp>3.9,<3.10",
+    "requests>=2.20,<3.0",
+    "pydantic[email]",
+    "djagger"
+]
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as readme:
+    README = readme.read()
+
+with open(f'{SRC_FOLDER}{os.path.sep}{PKG_NAME}/__init__.py', 'r') as fd:
+    VERSION = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(), re.MULTILINE
+    ).group(1)
+
+LICENSE = "License :: OSI Approved :: Apache Software License"
+
+setup(
+    name=PKG_NAME,
+    version=VERSION,
+    description="SPID/CIE OIDC Federation Entity",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Giuseppe De Marco',
+    author_email='demarcog83@gmail.com',
+    license=LICENSE,
+    url=f"https://github.com/peppelinux/{PKG_NAME.replace('_', '-')}",
+    packages=[PKG_NAME, ],
+    package_dir={f"{PKG_NAME}": f"{SRC_FOLDER}/{PKG_NAME}"},
+
+    package_data={
+        f"{PKG_NAME}": [
+            i.replace(
+                f'{SRC_FOLDER}{os.path.sep}{PKG_NAME}{os.path.sep}', ''
+            )
+            for i in glob(
+                f'{SRC_FOLDER}{os.path.sep}{PKG_NAME}{os.path.sep}**',
+                recursive=True
+            )
+            if i and '__pycache__' not in i
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        LICENSE,
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules"],
+    install_requires=INSTALL_REQUIRES,
+    zip_safe=False,
+)
---
+++ packages/spid-cie-oidc/spid_cie_oidc-1.4.5.tar.gz/spid_cie_oidc-1.4.5/spid_cie_oidc/entity/schemas/rp_metadata.py
@@ -0,0 +1,46 @@
+from enum import Enum
+from typing import List, Optional
+from pydantic import BaseModel, HttpUrl, root_validator
+from .jwks import JwksSpid
+
+
+class GrantTypeSupported(str, Enum):
+    refresh_token = "refresh_token"  # nosec - B105
+    authorization_code = "authorization_code"
+
+
+class RPMetadata(BaseModel):
+    organization_name: str
+    redirect_uris: List[HttpUrl]
+    response_types = ["code"]
+    grant_types: List[GrantTypeSupported]
+    client_id: HttpUrl
+    # TODO: Could be specified in multiple languages
+    client_name: str
+
+    signed_jwks_uri: Optional[HttpUrl]
+    jwks_uri: Optional[HttpUrl]
+    jwks: Optional[JwksSpid]
+
+    @root_validator(pre=False)
+    def validate(cls, values):
+        jwks_there = False
+        for i in ("jwks_uri", "jwks", "signed_jwks_uri"):
+            if values.get(i):
+                jwks_there = True
+                break
+
+        if not jwks_there:
+            raise ValueError(
+                "one of signed_jwks_uri or jwks_uri or jwks must be set"
+            )
+        return values
+
+
+class RPMetadataSpid(RPMetadata):
+    pass
+
+
+class RPMetadataCie(RPMetadataSpid):
+    application_type = "web"
+    tls_client_certificate_bound_access_tokens: Optional[bool]
