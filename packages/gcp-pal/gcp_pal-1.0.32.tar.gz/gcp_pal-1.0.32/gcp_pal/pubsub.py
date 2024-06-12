b2efe7178136638be7c97fab220710cb2e772a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:05:30 +0000

    Add package gcp-pal/1.0.32/gcp_pal-1.0.32.tar.gz

---
+++ packages/gcp-pal/gcp_pal-1.0.32.tar.gz/gcp_pal-1.0.32/gcp_pal/pubsub.py
@@ -0,0 +1,49 @@
+import os
+import json
+
+from gcp_pal.utils import log, ClientHandler, ModuleHandler, get_default_arg
+
+
+class PubSub:
+    """
+    Class for pushing a payload to a topic
+
+    Examples:
+    - `PubSub("topic").publish("data")` -> Publish `"data"` to "topic"
+    - `PubSub("topic").publish({"key": "value"})` -> Publish `{"key": "value"}` to "topic"
+    """
+
+    def __init__(self, topic: str, project=None):
+        """
+        Args:
+        - topic (str): Name of the topic
+        - project (str): Project ID
+        """
+        self.topic_id = topic
+        self.project = project or get_default_arg("project")
+
+        self.pubsub = ModuleHandler("google.cloud").please_import(
+            "pubsub_v1", who_is_calling="PubSub"
+        )
+        self.publisher = ClientHandler(self.pubsub.PublisherClient).get()
+
+        self.topic_path = self.publisher.topic_path(self.project, topic)
+
+    def __repr__(self):
+        return f"PubSub({self.topic_id})"
+
+    def publish(self, data) -> None:
+        if isinstance(data, dict):
+            data = json.dumps(data)
+        try:
+            publish_future = self.publisher.publish(
+                self.topic_path, data.encode("utf-8")
+            )
+            result = publish_future.result()
+        except Exception as e:
+            log(f"PubSub - An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    PubSub("test_topic").publish("data")
+    PubSub("test_topic").publish({"key": "value"})
