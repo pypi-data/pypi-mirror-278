8919acc8b5efc71814415b415a06c5b15d84cf2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:13 +0000

    Add package diive/0.77.0/diive-0.77.0.tar.gz

---
+++ packages/diive/diive-0.77.0.tar.gz/diive-0.77.0/diive/pkgs/flux/common.py
@@ -0,0 +1,37 @@
+from typing import Literal
+
+# Names of flux variables and their base variable in EddyPro,
+# names are different depending on output file (_full_output_ or _fluxnet_)
+basevars_fluxnetfile = {
+    'FC': 'CO2',
+    'FH2O': 'H2O',
+    'LE': 'H2O',
+    'ET': 'H2O',
+    'H': 'T_SONIC',
+    'FN2O': 'N2O',
+    'FCH4': 'CH4',
+}
+
+basevars_fulloutputfile = {
+    'co2_flux': 'co2',
+    'h2o_flux': 'h2o',
+    'LE': 'h2o',
+    'ET': 'h2o',
+    'H': 'sonic_temperature',
+    'n2o_flux': 'n2o',
+    'ch4_flux': 'ch4',
+}
+
+
+def detect_basevar(fluxcol: str,
+                   filetype: Literal['EDDYPRO-FLUXNET-CSV-30MIN', 'EDDYPRO-FULL-OUTPUT-CSV-30MIN']) -> str:
+    """Detect name of base variable that was used to calculate
+    the respective flux."""
+    if filetype == 'EDDYPRO-FLUXNET-CSV-30MIN':
+        basevar = basevars_fluxnetfile[fluxcol]
+    elif filetype == 'EDDYPRO-FULL-OUTPUT-CSV-30MIN':
+        basevar = basevars_fulloutputfile[fluxcol]
+    else:
+        raise Exception(f"(!) Filetype {filetype} is not defined. No basevar could be detected for {fluxcol}.")
+    print(f"Detected base variable {basevar} for {fluxcol}.")
+    return basevar
