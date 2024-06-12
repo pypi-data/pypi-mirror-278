d3ff2c5541f9184e7a32605fb4cd311395eec1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 14:05:17 +0000

    Add package langtrace-python-sdk/2.1.14/langtrace_python_sdk-2.1.14-py3-none-any.whl

---
+++ packages/langtrace-python-sdk/langtrace_python_sdk-2.1.14-py3-none-any.whl/langtrace_python_sdk/__init__.py
@@ -0,0 +1,32 @@
+"""
+Copyright (c) 2024 Scale3 Labs
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from langtrace_python_sdk import langtrace
+from langtrace_python_sdk.utils.with_root_span import (
+    with_langtrace_root_span,
+    with_additional_attributes,
+)
+
+from langtrace_python_sdk.utils.prompt_registry import get_prompt_from_registry
+from langtrace_python_sdk.utils.with_root_span import SendUserFeedback
+
+__all__ = [
+    "langtrace",
+    "with_langtrace_root_span",
+    "with_additional_attributes",
+    "get_prompt_from_registry",
+    "SendUserFeedback",
+]
