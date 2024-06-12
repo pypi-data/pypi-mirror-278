1962c54b6b80716335101c3111890fc5f035359b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 19:05:04 +0000

    Add package theostekno/0.1/theostekno-0.1.tar.gz

---
+++ packages/theostekno/theostekno-0.1.tar.gz/theostekno-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='theostekno',
+    version='0.1',
+    py_modules=['theos_tekno'],
+    author='Emre Tekno',
+    author_email='theostekno@gmail.com',
+    description='A simple random number generator library'
+)
