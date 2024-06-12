539793286de7aa265a226777919e1739f2e097f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-12 20:05:45 +0000

    Add package k-lights-interface/0.1.23/k_lights_interface-0.1.23-py3-none-any.whl

---
+++ packages/k-lights-interface/k_lights_interface-0.1.23-py3-none-any.whl/k_lights_interface/k_device_names.py
@@ -0,0 +1,16 @@
+play_device_name = "K Play"
+play_pro_device_name = "K Play Pro"
+epos_300_lamphead_device_name = "Epos300 driver"
+epos_300_controller_device_name = "Epos300 controller"
+epos_600_lamphead_device_name = "Epos600 lamphead"
+epos_600_controller_device_name = "Epos600 controller"
+valid_device_names = [play_device_name, play_pro_device_name, epos_300_lamphead_device_name, epos_300_controller_device_name, epos_600_controller_device_name, epos_600_lamphead_device_name]
+
+
+
+
+btle_adv_name_play = "Play"
+btle_adv_name_play_pro = "Play Pro"
+btle_adv_name_epos_300_controller = "Epos 300"
+btle_adv_name_epos_600_controller = "Epos 600"
+valid_btle_adv_names = [btle_adv_name_play, btle_adv_name_play_pro, btle_adv_name_epos_300_controller, btle_adv_name_epos_600_controller]
