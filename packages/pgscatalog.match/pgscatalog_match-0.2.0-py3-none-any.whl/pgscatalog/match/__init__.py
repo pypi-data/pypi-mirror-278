fd512d8e823765ec9e4f5ffa039782c9b1ea2737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 16:06:02 +0000

    Add package pgscatalog.match/0.2.0/pgscatalog_match-0.2.0-py3-none-any.whl

---
+++ packages/pgscatalog.match/pgscatalog_match-0.2.0-py3-none-any.whl/pgscatalog/match/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+from .lib import VariantFrame
+from .lib import ScoringFileFrame, match_variants
+from .lib import MatchResult, MatchResults
+from .lib import PlinkScoreFiles
+
+
+log_fmt = "%(name)s: %(asctime)s %(levelname)-8s %(message)s"
+logging.basicConfig(format=log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    "VariantFrame",
+    "ScoringFileFrame",
+    "MatchResult",
+    "MatchResults",
+    "PlinkScoreFiles",
+    "match_variants",
+]
+
+__version__ = "0.2.0"
