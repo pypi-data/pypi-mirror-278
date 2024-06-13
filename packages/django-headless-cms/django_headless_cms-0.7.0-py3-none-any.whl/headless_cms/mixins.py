a3307224ad0f8ba88d4ede58cb04f61d7819ce53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:05:04 +0000

    Add package django-headless-cms/0.7.0/django_headless_cms-0.7.0-py3-none-any.whl

---
+++ packages/django-headless-cms/django_headless_cms-0.7.0-py3-none-any.whl/headless_cms/mixins.py
@@ -0,0 +1,74 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.settings import api_settings as rest_framework_settings
+
+from headless_cms.serializers import HashModelSerializer
+from headless_cms.settings import headless_cms_settings
+
+
+class CMSSchemaMixin:
+    """
+    Mixin to include views in the OpenAPI schema documentation for the CMS.
+
+    This mixin is used to dynamically generate documentation for the OpenAPI schema,
+    ensuring that only views extending this mixin will be included in the generated
+    schema. It also provides default CMS permission classes for Django REST Framework
+    views.
+
+    Attributes:
+        permission_classes (list): A list of permission classes that includes the default
+            CMS permission class followed by the default permission classes from Django
+            REST Framework settings.
+
+    Example:
+        This mixin can be used in a Django REST Framework view to include the view in
+        the OpenAPI schema documentation and enforce CMS-specific permission policies.
+
+        .. code-block:: python
+
+            from rest_framework.views import APIView
+            from headless_cms.mixins import CMSSchemaMixin
+
+            class MyCMSView(CMSSchemaMixin, APIView):
+                # Your view implementation here
+                pass
+    """
+
+    permission_classes = [
+        headless_cms_settings.DEFAULT_CMS_PERMISSION_CLASS
+    ] + rest_framework_settings.DEFAULT_PERMISSION_CLASSES
+
+    def __init_subclass__(cls, **kwargs):
+        cls.__doc__ = cls.__doc__ or " "
+
+
+class HashModelMixin:
+    """
+    A mixin that adds hash-related actions to a Django REST Framework viewset.
+
+    This mixin provides actions to retrieve hash data for individual model instances or a list of
+    model instances.
+    """
+
+    def get_serializer_class(self):
+        if self.action in ["hashes", "hash"]:
+            return HashModelSerializer
+        return super().get_serializer_class()
+
+    @action(detail=True, methods=["GET"])
+    def hash(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=["GET"])
+    def hashes(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
