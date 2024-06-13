8fdac6ef9345892d4e1bcc0216c6fabf6506d6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 15:05:28 +0000

    Add package cgt/0.4.3/cgt-0.4.3.tar.gz

---
+++ packages/cgt/cgt-0.4.3.tar.gz/cgt-0.4.3/cgt/tests/test_conversions.py
@@ -0,0 +1,18 @@
+from .. import example
+import pytest
+
+@pytest.fixture
+def example_framework():
+    yield example(10)
+
+def test_one_row_and_cycles_random(example_framework):
+    fw, _ = example_framework
+    instance = fw.random_instance()
+    assert instance == fw.cycles(fw.one_row(instance))
+
+def test_same_canonical_instance_random(example_framework):
+    fw, _ = example_framework
+    canonical_instance = fw.canonical_instance(fw.random_instance())
+    genome = fw._genome_coset(fw.cycles(canonical_instance))
+    for instance in genome:
+        assert canonical_instance == fw.canonical_instance(instance)
---
+++ packages/cgt/cgt-0.4.3.tar.gz/cgt-0.4.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
+
+setuptools.setup(
+    name="cgt",
+    version= readfile("VERSION").strip(),
+    author="Joshua Stevenson",
+    author_email="joshua.stevenson@utas.edu.au",
+    description="Tools for representing genomes in sage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/js51/Circular-genome-tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    package_data={'cgt': ['*.pickle']},
+    include_package_data=True,
+)
