fe6e1dc111420e1bea45265a5cb331f9de9cc591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 06:04:48 +0000

    Add package tarsier/0.5.95/tarsier-0.5.95.tar.gz

---
+++ packages/tarsier/tarsier-0.5.95.tar.gz/tarsier-0.5.95/tarsier/ocr/__init__.py
@@ -0,0 +1,4 @@
+from .ocr_service import GoogleVisionOCRService, OCRService
+from .types import ImageAnnotatorResponse
+
+__all__ = ["OCRService", "GoogleVisionOCRService", "ImageAnnotatorResponse"]
