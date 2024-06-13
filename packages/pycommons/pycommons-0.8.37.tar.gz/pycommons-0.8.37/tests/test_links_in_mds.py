f7c3c91824613d4aa3ab8050b705ff340c0de952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 14:05:34 +0000

    Add package pycommons/0.8.37/pycommons-0.8.37.tar.gz

---
+++ packages/pycommons/pycommons-0.8.37.tar.gz/pycommons-0.8.37/tests/test_links_in_mds.py
@@ -0,0 +1,28 @@
+"""Test the links in the README.md files."""
+
+from pycommons.dev.tests.links_in_md import check_links_in_md
+from pycommons.io.path import file_path
+
+
+def test_links_in_readme_md() -> None:
+    """Test the links in the README.md file."""
+    check_links_in_md(file_path(__file__).up(
+        2).resolve_inside("README.md"))
+
+
+def test_links_in_contributing_md() -> None:
+    """Test the links in the CONTRIBUTING.md file."""
+    check_links_in_md(file_path(__file__).up(
+        2).resolve_inside("CONTRIBUTING.md"))
+
+
+def test_links_in_security_md() -> None:
+    """Test the links in the SECURITY.md file."""
+    check_links_in_md(file_path(__file__).up(
+        2).resolve_inside("SECURITY.md"))
+
+
+def test_links_in_license() -> None:
+    """Test the links in the LICENSE file."""
+    check_links_in_md(file_path(__file__).up(
+        2).resolve_inside("LICENSE"))
