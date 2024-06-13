eb160ed2d5aaaf7cfac176af08c175bc616d86ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 16:05:37 +0000

    Add package yolo-auto-agmentation/1.0.2/yolo_auto_agmentation-1.0.2.tar.gz

---
+++ packages/yolo-auto-agmentation/yolo_auto_agmentation-1.0.2.tar.gz/yolo_auto_agmentation-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yolo_auto_agmentation',
+    version='1.0.2',
+    description='enter dataset path with train, val, test. Then automatically augment every images to ready YOLO object detection train',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Na-I-Eon',
+    author_email='112fkdldjs@naver.com',
+    url='https://github.com/Nyan-SouthKorea/yolo_auto_augmentation',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python', 'tqdm', 'albumentations', 'natsort'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
