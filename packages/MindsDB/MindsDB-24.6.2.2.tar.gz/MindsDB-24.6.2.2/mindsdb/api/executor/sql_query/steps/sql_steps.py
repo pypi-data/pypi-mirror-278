59071f5bcce12cd16fc6ce82de6f923302c52fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:06:30 +0000

    Add package MindsDB/24.6.2.2/MindsDB-24.6.2.2.tar.gz

---
+++ packages/MindsDB/MindsDB-24.6.2.2.tar.gz/MindsDB-24.6.2.2/mindsdb/api/executor/sql_query/steps/sql_steps.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+from mindsdb_sql.planner.steps import (
+    LimitOffsetStep,
+    DataStep,
+)
+
+from mindsdb.api.executor.sql_query.result_set import ResultSet
+
+from .base import BaseStepCall
+
+
+class LimitOffsetStepCall(BaseStepCall):
+
+    bind = LimitOffsetStep
+
+    def call(self, step):
+        step_data = self.steps_data[step.dataframe.step_num]
+
+        step_data2 = ResultSet()
+        for col in step_data.columns:
+            step_data2.add_column(col)
+
+        df = step_data.get_raw_df()
+
+        if isinstance(step.offset, int):
+            df = df[step.offset:]
+        if isinstance(step.limit, int):
+            df = df[:step.limit]
+
+        step_data2.add_raw_df(df)
+
+        return step_data2
+
+
+class DataStepCall(BaseStepCall):
+
+    bind = DataStep
+
+    def call(self, step):
+        # create resultset
+        df = pd.DataFrame(step.data)
+        return ResultSet().from_df(df, database='', table_name='')
