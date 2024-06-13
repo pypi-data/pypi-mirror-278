5914ad1b38b6a101e4579eec60f7fdd7f9ebd43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 07:05:26 +0000

    Add package find-and-replace-commit-check/0.8.0/find-and-replace-commit-check-0.8.0.tar.gz

---
+++ packages/find-and-replace-commit-check/find-and-replace-commit-check-0.8.0.tar.gz/find-and-replace-commit-check-0.8.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup()
+
+
+# from setuptools import setup
+
+# setup(
+#     name="find-and-replace-template-commit-check",
+#     version="0.8.0",
+#     description="A tool to find and replace templates in commit messages",
+#     packages=["find_and_replace_template_commit_check"],
+#     author="Your Name",
+#     author_email="your.email@example.com",
+#     url="https://github.com/yourusername/find-and-replace-template-commit-check",
+# )
