976948dd15023d9ad681b2f72987ee196d176795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 16:05:08 +0000

    Add package modelscope-studio/0.3.1/modelscope_studio-0.3.1-py3-none-any.whl

---
+++ packages/modelscope-studio/modelscope_studio-0.3.1-py3-none-any.whl/modelscope_studio/__init__.py
@@ -0,0 +1,7 @@
+from .components import ModelScopeChatbot as Chatbot
+from .components import ModelScopeFlow as Flow
+from .components import ModelScopeMarkdown as Markdown
+from .components import ModelScopeMultimodalInput as MultimodalInput
+from .components import ModelScopeWaterfallGallery as WaterfallGallery
+from .external import load
+from .version import __version__
