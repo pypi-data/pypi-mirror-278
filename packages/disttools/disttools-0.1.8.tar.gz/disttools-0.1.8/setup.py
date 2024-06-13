a01c8001898743cceee78bf2b382535ae0f8033c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 15:06:03 +0000

    Add package disttools/0.1.8/disttools-0.1.8.tar.gz

---
+++ packages/disttools/disttools-0.1.8.tar.gz/disttools-0.1.8/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from setuptools import setup
+import os
+
+setup(name='disttools',
+	version='0.1.8',
+	description='release distribution tools',
+	url='http://github.com/jeppeter/disttools',
+	author='jeppeter Wang',
+	author_email='jeppeter@gmail.com',
+	license='MIT',
+	packages=['disttools'],
+	zip_safe=False)
