b3d3a5ba55bc16707dbe123891a3573ac92d9973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 15:06:08 +0000

    Add package AnnotationCheckerWithStructure/0.1.4/annotationcheckerwithstructure-0.1.4.tar.gz

---
+++ packages/AnnotationCheckerWithStructure/annotationcheckerwithstructure-0.1.4.tar.gz/annotationcheckerwithstructure-0.1.4/setup.py
@@ -0,0 +1,35 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="AnnotationCheckerWithStructure",
+    version="0.1.4",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        "biopython==1.79",
+        "BCBio-GFF==0.7.5",
+        "pandas==1.3.5",
+        "matplotlib==3.5.1",
+        "seaborn==0.11.2"
+    ],
+    entry_points={
+        'console_scripts': [
+            'AnnotationCheckerWithStructure=main:main',
+        ],
+    },
+    author="Andreas Bachler",
+    author_email="Andy.Bachler@example.com",
+    description="A simple bioinformatics script.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Andy-B-123/AnnotationCheckerWithStructure",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
