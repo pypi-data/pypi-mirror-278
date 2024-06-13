d2f5144658f23bbcde61c70462993541936d7aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:35 +0000

    Add package hyperfocus/1.0.0/hyperfocus-1.0.0.tar.gz

---
+++ packages/hyperfocus/hyperfocus-1.0.0.tar.gz/hyperfocus-1.0.0/src/hyperfocus/console/commands/add.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import click
+
+from hyperfocus.services.session import get_current_session
+from hyperfocus.termui import formatter
+from hyperfocus.termui import printer
+from hyperfocus.termui import prompt
+from hyperfocus.termui.components import SuccessNotification
+
+
+@click.command(help="Add task to current working day")
+@click.argument("title", metavar="<title>", type=click.STRING)
+@click.option("-d", "--details", "add_details", is_flag=True, help="add task details")
+def add(title: str, add_details: bool) -> None:
+    session = get_current_session()
+
+    details = prompt.prompt("Task details") if add_details else ""
+
+    task = session.daily_tracker.create_task(title=title, details=details)
+    printer.echo(
+        SuccessNotification(
+            text=f"{formatter.task(task=task, show_prefix=True)} created",
+        )
+    )
---
+++ packages/hyperfocus/hyperfocus-1.0.0.tar.gz/hyperfocus-1.0.0/src/hyperfocus/termui/icons.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+
+NEW_DAY = "✨"
+DETAILS = "■"
+NO_DETAILS = "□"
+TASK_STATUS = "⬢"
+LIST = "•"
+SUCCESS = "✔"
+INFO = "ℹ"  # noqa: RUF001
+WARNING = "▼"
+ERROR = "✘"
+NOTIFICATION = ">"
+PROMPT = "?"
+PROGRESSBAR = "▮"
+PROGRESSBAR_EMPTY = "▯"
+HISTORY_NODE = "├──"
+HISTORY_END_NODE = "└──"
