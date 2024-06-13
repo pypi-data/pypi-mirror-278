40e1dea7213a3cadc5e07d86115185c32c9b5c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:08:57 +0000

    Add package flowbyte/0.2.4/flowbyte-0.2.4-py3-none-any.whl

---
+++ packages/flowbyte/flowbyte-0.2.4-py3-none-any.whl/flowbyte/log.py
@@ -0,0 +1,39 @@
+
+
+
+class Log:
+    status: str
+    message: str
+    message_color: str
+
+
+
+    def __init__(self, message: str, status:str):
+        self.message = message
+        self.status = status
+
+    
+
+    def get_status_color(self, status: str):
+        """
+        Convert the color string to the corresponding ANSI color code
+        """
+        colors = {
+            "regular": "\033[30m",  # black
+            "fail": "\033[31m",     # red
+            "success": "\033[32m",  # green
+            "warning": "\033[33m",  # yellow
+            "info": "\033[34m",     # blue
+        }
+
+        return colors.get(status, colors["regular"])
+        
+
+
+    
+    def print_message(self):
+        """
+        Print the message in the specified color
+        """
+        color = self.get_status_color(self.status)
+        print(color + self.message + "\033[0m")
