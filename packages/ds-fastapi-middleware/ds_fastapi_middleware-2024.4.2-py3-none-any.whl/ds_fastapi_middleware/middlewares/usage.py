e33ac417d2e6508a7b6ddc5af42ede43f16629e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:08:55 +0000

    Add package ds-fastapi-middleware/2024.4.2/ds_fastapi_middleware-2024.4.2-py3-none-any.whl

---
+++ packages/ds-fastapi-middleware/ds_fastapi_middleware-2024.4.2-py3-none-any.whl/ds_fastapi_middleware/middlewares/usage.py
@@ -0,0 +1,82 @@
+"""
+Usage middleware for tracing service usage in fastApi applications.
+
+Usage metrics is collected and written to storage system for
+the purpose of billing customer.
+
+Example:
+>>> from fastapi import FastAPI
+>>> from ds_fastapi_middleware.middlewares import UsageMiddleware
+
+>>> app = FastAPI()
+>>> app.add_middleware(
+>>>     UsageMiddleware,
+>>>     product_id="product_id",
+>>>     memory_mb=1024,
+>>>     queue_name="queue_name",
+>>> )
+"""
+
+import datetime
+import typing
+from uuid import UUID
+
+from fastapi import FastAPI, Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from ds_fastapi_middleware import models
+from ds_fastapi_middleware.utils.usage import write_usage
+
+
+class UsageMiddleware(BaseHTTPMiddleware):
+    def __init__(
+        self,
+        app: FastAPI,
+        product_id: UUID,
+        memory_mb: int,
+        queue_name: str,
+    ):
+        """
+        Usage middleware for tracing service usage in fastApi applications.
+
+        @param product_id: Product UUID.
+        @param memory_mb: Memory in megabytes as integer.
+        @param queue_name: Queue name to write the message.
+        """
+        self.product_id = product_id
+        self.memory_mb = memory_mb
+        self.queue_name = queue_name
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next: typing.Callable):
+        """
+        Function to collect usage data and write to Cost database.
+        @param request: Request object.
+        @param call_next: Function to call next.
+        @return: Response.
+        """
+        start_time = datetime.datetime.utcnow()
+        response = await call_next(request)
+        end_timestamp = datetime.datetime.utcnow()
+
+        if not hasattr(request.state, "context"):
+            # Context object does not exist, authorization
+            # never happened.
+            return response
+
+        ctx = request.state.context
+        tenant_id = ctx.tenant_id
+        logger = ctx.logger
+
+        usage = models.UsagePayload(
+            product_id=self.product_id,
+            tenant_id=tenant_id,
+            memory_mb=self.memory_mb,
+            start_time=start_time,
+            end_time=end_timestamp,
+            workflow=request.url.path,
+        )
+        logger.info("Writing usage.")
+        write_usage(usage=usage, queue_name=self.queue_name)
+        response.headers["X-Usage-Id"] = str(usage.id)
+        return response
---
+++ packages/ds-fastapi-middleware/ds_fastapi_middleware-2024.4.2-py3-none-any.whl/tests/test_middleware_ctx.py
@@ -0,0 +1,27 @@
+import uuid
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from ds_fastapi_middleware import ContextMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(ContextMiddleware)
+
+
+@app.get("/hello")
+async def hello():
+    return {"message": "Hello World"}
+
+
+client = TestClient(app)
+
+
+def test_ctx():
+    response = client.get("/hello")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+    assert response.headers["x-request-id"] is not None
+    uuid.UUID(response.headers["x-request-id"])
