e6e99a745f0c2d0cd3be4a303a679c1d8e3aeb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 06:04:39 +0000

    Add package flymyai/0.1.2/flymyai-0.1.2.tar.gz

---
+++ packages/flymyai/flymyai-0.1.2.tar.gz/flymyai-0.1.2/setup.py
@@ -0,0 +1,16 @@
+import pathlib
+from setuptools import setup, find_packages
+
+_BASE_PATH = pathlib.Path(__file__).parent
+
+setup(
+    name="flymyai",
+    version="0.1.2",
+    packages=find_packages(where=str(_BASE_PATH)),
+    package_dir={"": str(_BASE_PATH)},
+    include_package_data=True,
+    license="",
+    author="oleg",
+    author_email="lyerhd@gmail.com",
+    description="",
+)
