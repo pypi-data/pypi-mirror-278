5f908ef6cee2b48d59ae2cede7c014c57377e191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 17:04:54 +0000

    Add package alga/0.4.0/alga-0.4.0.tar.gz

---
+++ packages/alga/alga-0.4.0.tar.gz/alga-0.4.0/README.md
@@ -0,0 +1,13 @@
+Alga
+====
+
+A command line utility for controlling a LG webOS TV over the network.
+
+It is still in it's early stages of development but the implemented functionality should work.
+
+Work yet to do:
+---------------
+
+* Add tests for websocket communication
+* Add way of adding TV without having to set environment variables
+* Add CI for releasing versions to PyPI when tags are added
---
+++ packages/alga/alga-0.4.0.tar.gz/alga-0.4.0/src/alga/client.py
@@ -0,0 +1,53 @@
+import json
+import ssl
+from collections.abc import Iterator
+from contextlib import contextmanager
+from os import getenv
+from typing import Any, Optional, cast
+
+from websocket import WebSocket
+
+from alga.payloads import get_hello_data
+
+
+HOSTNAME = getenv("ALGA_HOST", "lgwebostv")
+KEY = getenv("ALGA_KEY", "")
+
+
+@contextmanager
+def new(
+    perform_handshake: bool = True, timeout: int = 3
+) -> Iterator[WebSocket]:  # pragma: no cover
+    connection = WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+    connection.connect(f"wss://{HOSTNAME}:3001/", suppress_origin=True, timeout=timeout)  # type: ignore[no-untyped-call]
+
+    if perform_handshake:
+        connection.send(json.dumps(get_hello_data(KEY)))
+        response = json.loads(connection.recv())
+        if "client-key" not in response["payload"]:
+            raise Exception(
+                f"Something went wrong with performing a handshake. Response: {response}"
+            )
+
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+
+def request(
+    uri: str, data: Optional[dict[str, Any]] = None
+) -> dict[str, Any]:  # pragma: no cover
+    with new() as connection:
+        request: dict[str, Any] = {"type": "request", "uri": uri}
+
+        if data:
+            request.update(payload=data)
+
+        connection.send(json.dumps(request))
+
+        raw_response = connection.recv()
+        response = json.loads(raw_response)
+
+        assert response.get("payload", {}).get("returnValue") is True
+        return cast(dict[str, Any], response["payload"])
