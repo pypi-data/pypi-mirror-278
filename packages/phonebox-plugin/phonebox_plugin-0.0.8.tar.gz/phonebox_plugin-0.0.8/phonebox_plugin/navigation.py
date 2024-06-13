ab4aa38b1238332c470110570852e4d4eb8c1287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 04:05:28 +0000

    Add package phonebox-plugin/0.0.8/phonebox_plugin-0.0.8.tar.gz

---
+++ packages/phonebox-plugin/phonebox_plugin-0.0.8.tar.gz/phonebox_plugin-0.0.8/phonebox_plugin/navigation.py
@@ -0,0 +1,14 @@
+from extras.plugins import PluginMenuItem
+
+menu_items = (
+    PluginMenuItem(
+        link='plugins:phonebox_plugin:list_view',
+        link_text='Numbers',
+        buttons=()
+    ),
+    PluginMenuItem(
+        link='plugins:phonebox_plugin:voice_circuit_list_view',
+        link_text='Voice Circuits',
+        buttons=()
+    ),
+)
