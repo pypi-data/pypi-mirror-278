696226b9942c7f15045ddf7c96679a1302280268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 16:05:39 +0000

    Add package GPT-SoVITS-Infer/0.2.5/gpt_sovits_infer-0.2.5-py3-none-any.whl

---
+++ packages/GPT-SoVITS-Infer/gpt_sovits_infer-0.2.5-py3-none-any.whl/gpt_sovits/infer/__init__.py
@@ -0,0 +1,9 @@
+from gpt_sovits.infer.inference import GPTSoVITSInference
+from gpt_sovits.infer.inference_pool import GPTSoVITSInferencePool
+from gpt_sovits.infer.interface import GPTSoVITSInferenceSimple
+
+__all__ = [
+    "GPTSoVITSInference",
+    "GPTSoVITSInferencePool",
+    "GPTSoVITSInferenceSimple",
+]
