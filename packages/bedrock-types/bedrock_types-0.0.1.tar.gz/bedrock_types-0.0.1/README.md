1c510e56c7219fefaad840fcb7383361dd273afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 16:06:08 +0000

    Add package bedrock-types/0.0.1/bedrock_types-0.0.1.tar.gz

---
+++ packages/bedrock-types/bedrock_types-0.0.1.tar.gz/bedrock_types-0.0.1/README.md
@@ -0,0 +1,1 @@
+# Amazon Bedrock Converse API Data Types
---
+++ packages/bedrock-types/bedrock_types-0.0.1.tar.gz/bedrock_types-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="bedrock_types",
+    version="0.1",
+    description="Bedrock Converse API Data Types",
+    url="https://github.com/hustshawn/bedrock-types",
+    author="Shawn Zhang",
+    author_email="hustshawn@gmail.com",
+    license="MIT",
+    packages=["bedrock_types"],
+    zip_safe=False,
+)
