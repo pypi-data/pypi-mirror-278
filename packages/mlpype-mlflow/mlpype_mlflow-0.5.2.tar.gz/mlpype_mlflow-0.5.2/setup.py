45e2ce8d667b087c9eb4f5d96f5c84d2858f550e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 16:05:29 +0000

    Add package mlpype-mlflow/0.5.2/mlpype_mlflow-0.5.2.tar.gz

---
+++ packages/mlpype-mlflow/mlpype_mlflow-0.5.2.tar.gz/mlpype_mlflow-0.5.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_namespace_packages, setup
+
+if __name__ == "__main__":
+    version = "0.5.2"
+
+    deps = [
+        f"mlpype-base=={version}",
+        "mlflow>=1.30.0",
+        "GitPython>=3.1.27",
+    ]
+    strict_deps = [s.replace(">=", "==") for s in deps]
+
+    setup(
+        name="mlpype-mlflow",
+        install_requires=deps,
+        extras_require={"dev": strict_deps, "strict": strict_deps},
+        packages=find_namespace_packages(include=["mlpype.*"]),
+        version=version,
+    )
