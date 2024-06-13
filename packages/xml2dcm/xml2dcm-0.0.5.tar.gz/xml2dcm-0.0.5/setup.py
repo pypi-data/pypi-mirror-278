b9374691d5156528426787bdcd9fbab5a4057369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 15:05:53 +0000

    Add package xml2dcm/0.0.5/xml2dcm-0.0.5.tar.gz

---
+++ packages/xml2dcm/xml2dcm-0.0.5.tar.gz/xml2dcm-0.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='xml2dcm',
+    version='0.0.5',
+    description='Packages to convert XML files to DICOM format written by SNUH VitalLab ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='seunghojun',
+    author_email='danuri.jun@gmail.com',
+    url='https://github.com/SeungHoJUN/XML2DCM',
+    install_requires=['pybase64', 'xml-python', 'numpy', 'pydicom', 'datetime', 'traceback2'],
+    keywords=['xml2dcm', 'XML', 'Dicom', 'ECG Dicom', 'SNUH'],
+    python_requires='>=3.11',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
