d9e38d595963bc43ddd90c6836b13d3b87ae352f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:11 +0000

    Add package odoo13-addon-photovoltaic-mgmt-extended/13.0.1.1.5/odoo13-addon-photovoltaic_mgmt_extended-13.0.1.1.5.tar.gz

---
+++ packages/odoo13-addon-photovoltaic-mgmt-extended/odoo13-addon-photovoltaic_mgmt_extended-13.0.1.1.5.tar.gz/odoo13-addon-photovoltaic_mgmt_extended-13.0.1.1.5/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Photovoltaic Management Extended",
+    'version': '13.0.1.1.5',
+    'depends': [
+        'photovoltaic_mgmt',
+        'photovoltaic_participant_liquidations',
+        'stock'
+    ],
+    'author': "Librecoop",
+    'category': 'Sales',
+    'description': """
+    This module extendes the functionality of the Photovoltaic Management module
+    """,
+    "installable": True,
+    "auto_install": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/contract_participation.xml",
+        "views/photovoltaic_power_station.xml",
+        "wizard/account_allocation_state_update_wizard_view.xml",
+        "views/account_allocation.xml",
+        "views/res_partner.xml",
+        "views/res_partner_interest.xml"
+    ],
+}
