d66fe527a76a198956b16452952efdd35222f3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:24 +0000

    Add package pylipsum/0.0a0/pylipsum-0.0a0.tar.gz

---
+++ packages/pylipsum/pylipsum-0.0a0.tar.gz/pylipsum-0.0a0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pylipsum',
+      version='0.0a',
+      description='Lorem lipsum as py library',
+      packages=['pylipsum'],
+      author_email='diassuikimbek@aol.com',
+      zip_safe=False)
