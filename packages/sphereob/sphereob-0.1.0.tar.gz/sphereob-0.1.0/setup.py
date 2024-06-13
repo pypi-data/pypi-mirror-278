3602d93d5cca5ba2053d14324a151ce78529d3cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 04:05:51 +0000

    Add package sphereob/0.1.0/sphereob-0.1.0.tar.gz

---
+++ packages/sphereob/sphereob-0.1.0.tar.gz/sphereob-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sphereob',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A description of your project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/sphereob',  # Update with your GitHub URL
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'PyQt5',
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'qdarkstyle',
+    ],
+    entry_points={
+        'console_scripts': [
+            'sphereob=sphereob.GUI.sphere_overburden_gui:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
