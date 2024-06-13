b8f5ce4137bd91c5a0daf40d84dcf40d41297a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:06:07 +0000

    Add package stsci.image/2.3.9/stsci_image-2.3.9.tar.gz

---
+++ packages/stsci.image/stsci_image-2.3.9.tar.gz/stsci_image-2.3.9/stsci/image/__init__.py
@@ -0,0 +1,9 @@
+from importlib.metadata import PackageNotFoundError, version
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = 'unknown'
+
+from ._image import *
+from .combine import *
