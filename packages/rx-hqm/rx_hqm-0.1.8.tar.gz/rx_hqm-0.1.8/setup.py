3c6bd1f67464e6536bd6ae0865d57ea0445550a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:10:11 +0000

    Add package rx-hqm/0.1.8/rx_hqm-0.1.8.tar.gz

---
+++ packages/rx-hqm/rx_hqm-0.1.8.tar.gz/rx_hqm-0.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+        name            ='rx_hqm',
+        version         ='0.1.8',
+        author          ='Qi Shi',
+        description     ='',
+        packages        = find_packages(),
+        package_data    = {'hqm_data' : ['*.json', 'double_swap/*.json']},
+        install_requires= open('requirements.txt').read()
+        )
