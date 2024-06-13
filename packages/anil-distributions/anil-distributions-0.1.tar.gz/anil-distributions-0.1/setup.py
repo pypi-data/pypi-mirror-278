1ba09eb860fd7adc8c716cdc178bdc80323611de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 04:05:47 +0000

    Add package anil-distributions/0.1/anil-distributions-0.1.tar.gz

---
+++ packages/anil-distributions/anil-distributions-0.1.tar.gz/anil-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='anil-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['anil-distributions'],
+      author='Anil Kumar Avadhanula',
+      zip_safe=False)
