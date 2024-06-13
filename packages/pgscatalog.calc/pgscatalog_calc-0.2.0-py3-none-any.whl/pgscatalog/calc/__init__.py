2df2d883b9e2e8597caa7a03168c368d590dc3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 16:06:03 +0000

    Add package pgscatalog.calc/0.2.0/pgscatalog_calc-0.2.0-py3-none-any.whl

---
+++ packages/pgscatalog.calc/pgscatalog_calc-0.2.0-py3-none-any.whl/pgscatalog/calc/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+from .lib import (
+    PolygenicScore,
+    AggregatedPGS,
+    AdjustResults,
+    AdjustArguments,
+)
+from .lib import PopulationType, PrincipalComponents
+
+log_fmt = "%(name)s: %(asctime)s %(levelname)-8s %(message)s"
+logging.basicConfig(format=log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    "PolygenicScore",
+    "PrincipalComponents",
+    "PopulationType",
+    "AggregatedPGS",
+    "AdjustArguments",
+    "AdjustResults",
+]
+
+__version__ = "0.2.0"
