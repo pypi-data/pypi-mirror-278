8ce5b6464a0642513a7407231efb160a9c09106f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:04:33 +0000

    Add package duyll/0.5.1/duyll-0.5.1.tar.gz

---
+++ packages/duyll/duyll-0.5.1.tar.gz/duyll-0.5.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "duyll",
+    version= "0.5.1",
+    packages=["duyll/basicsr/data", 
+              "duyll/basicsr/utils",
+              "duyll/basicsr/models",
+              "duyll/pretrained_weights",
+              "duyll",
+              ],
+    install_requires=[]
+
+)
