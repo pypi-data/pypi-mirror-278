76d14ef2875427568a9c1d16c4fd9fd2540dca69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 06:04:43 +0000

    Add package streaming-stats/0.1/streaming_stats-0.1.tar.gz

---
+++ packages/streaming-stats/streaming_stats-0.1.tar.gz/streaming_stats-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open(file="README.md", mode="r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='streaming_stats',
+    version='0.1',
+    author="Aditya Raj",
+    author_email="adityaraj867604@gmail.com",
+    description='Statistics calculator of a list in O(1) space complexity',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ed21b006/RunningStats',  # Adjust as necessary
+    py_modules=['running_stats'],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
