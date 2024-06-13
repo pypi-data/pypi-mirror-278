e70f5797e8bda18212e2252f040a61b434f4c4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 06:04:42 +0000

    Add package metaerg/2.5.9/metaerg-2.5.9.tar.gz

---
+++ packages/metaerg/metaerg-2.5.9.tar.gz/metaerg-2.5.9/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='metaerg',
+    version='2.5.9',
+    packages=setuptools.find_packages(where='src'),
+    url='https://github.com/kinestetika/MetaErg',
+    license='MIT',
+    author='Marc Strous',
+    author_email='mstrous@ucalgary.ca',
+    description='Annotation of genomes and contigs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.10',
+                 'Topic :: Scientific/Engineering :: Bio-Informatics'],
+    keywords='repeat-regions genes functions taxonomy',
+    project_urls={'Source': 'https://github.com/kinestetika/MetaErg'},
+    package_dir={'': 'src'},
+    package_data={'': ['functional_gene_data']},
+    python_requires='>=3.10',
+    # versions for keras and tensorflow are limited because deepsig is currently not compatible with later versions
+    install_requires=['keras == 2.15', 'tensorflow == 2.15', 'biopython', 'ncbi-datasets-pylib', 'pandas', 'httpx', 'virtualenv', 'h5py', 'pyarrow', 'openpyxl', 'tqdm'],
+    extras_require={  # Optional
+        'dev': ['setuptools', 'build', 'twine'],
+        'test': []
+    },
+    entry_points={  # Optional
+        'console_scripts': [
+            'metaerg=metaerg.main:main',
+        ],
+    }
+)
