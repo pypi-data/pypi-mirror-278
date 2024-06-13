635d2f075574f8e2bf713b151fad347ba989e7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 15:06:14 +0000

    Add package quipudigital/0.1.1/quipudigital-0.1.1.tar.gz

---
+++ packages/quipudigital/quipudigital-0.1.1.tar.gz/quipudigital-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quipudigital',
+    version='0.1.1',
+    package_data={'mathlib': ['assets/*.*']},  # Include all files in the assets folder
+    packages=find_packages(),
+    author='jgomezz',
+    author_email="jgomezz@gmail.com",
+    description='Una biblioteca de visualización de Quipus con Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jgomezz/quipudigital',
+    
+)
+
+
