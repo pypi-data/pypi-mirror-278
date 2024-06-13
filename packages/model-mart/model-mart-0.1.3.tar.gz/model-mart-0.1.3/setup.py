a7b6702c8c3c3af628565274ce0b4064d2c20c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:32 +0000

    Add package model-mart/0.1.3/model-mart-0.1.3.tar.gz

---
+++ packages/model-mart/model-mart-0.1.3.tar.gz/model-mart-0.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = None
+
+setuptools.setup(
+    name="model-mart",  # Replace with your own name
+    version="0.1.3",
+    author="SoutherLea",
+    author_email="lizhengnan@stonewise.cn",
+    description="sdk help users to use model market",
+    long_description="sdk help users to use model market",
+    long_description_content_type="text/markdown",
+    url="https://gitlab.stonewise.cn/mlsys/model-mart.git",
+    install_requires=[
+        'minio>=7.2.7',
+        'Requests>=2.31.0',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
