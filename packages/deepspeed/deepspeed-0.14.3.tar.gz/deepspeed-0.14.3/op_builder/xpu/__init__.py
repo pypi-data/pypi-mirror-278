70adf2553a743ce1ede47510ca91c2ebd4441e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:04:31 +0000

    Add package deepspeed/0.14.3/deepspeed-0.14.3.tar.gz

---
+++ packages/deepspeed/deepspeed-0.14.3.tar.gz/deepspeed-0.14.3/op_builder/xpu/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+
+from .cpu_adam import CPUAdamBuilder
+from .cpu_adagrad import CPUAdagradBuilder
+from .fused_adam import FusedAdamBuilder
+from .async_io import AsyncIOBuilder
+from .packbits import PackbitsBuilder
