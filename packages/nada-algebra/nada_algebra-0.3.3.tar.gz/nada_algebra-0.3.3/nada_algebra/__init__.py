f9a5d8ba68c1be6272610d194af09c8c9d8c6ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 04:05:29 +0000

    Add package nada-algebra/0.3.3/nada_algebra-0.3.3.tar.gz

---
+++ packages/nada-algebra/nada_algebra-0.3.3.tar.gz/nada_algebra-0.3.3/nada_algebra/__init__.py
@@ -0,0 +1,16 @@
+"""This is the __init__.py module"""
+
+from nada_algebra.array import NadaArray
+from nada_algebra.funcs import *
+from nada_algebra.types import (
+    Rational,
+    SecretRational,
+    PublicBoolean,
+    SecretBoolean,
+    public_rational,
+    rational,
+    secret_rational,
+    get_log_scale,
+    reset_log_scale,
+    set_log_scale,
+)
