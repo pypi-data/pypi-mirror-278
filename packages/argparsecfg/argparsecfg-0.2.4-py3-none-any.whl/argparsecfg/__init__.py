6b7826f2cf41df51ec3b776d9b2b7c42bc4f9642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:08:41 +0000

    Add package argparsecfg/0.2.4/argparsecfg-0.2.4-py3-none-any.whl

---
+++ packages/argparsecfg/argparsecfg-0.2.4-py3-none-any.whl/argparsecfg/__init__.py
@@ -0,0 +1,20 @@
+from .core import (
+    ArgumentParserCfg,
+    add_args_from_dc,
+    add_argument_metadata,
+    create_dc_obj,
+    create_parser,
+    field_argument,
+    parse_args,
+)
+
+
+__all__ = [
+    "ArgumentParserCfg",
+    "add_args_from_dc",
+    "add_argument_metadata",
+    "create_dc_obj",
+    "create_parser",
+    "field_argument",
+    "parse_args",
+]
