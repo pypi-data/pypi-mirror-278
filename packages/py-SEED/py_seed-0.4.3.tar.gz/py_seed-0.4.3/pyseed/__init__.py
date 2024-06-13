7f215a22d415c2757b7bc7c93a466e558d23822d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 04:05:27 +0000

    Add package py-SEED/0.4.3/py_seed-0.4.3.tar.gz

---
+++ packages/py-SEED/py_seed-0.4.3.tar.gz/py_seed-0.4.3/pyseed/__init__.py
@@ -0,0 +1,7 @@
+# Local Imports
+from pyseed.seed_client_base import (  # noqa
+    SEEDOAuthReadOnlyClient,
+    SEEDOAuthReadWriteClient,
+    SEEDReadOnlyClient,
+    SEEDReadWriteClient,
+)
---
+++ packages/py-SEED/py_seed-0.4.3.tar.gz/py_seed-0.4.3/tests/test_utils.py
@@ -0,0 +1,26 @@
+"""
+SEED Platform (TM), Copyright (c) Alliance for Sustainable Energy, LLC, and other contributors.
+See also https://github.com/seed-platform/py-seed/main/LICENSE
+"""
+
+
+# Imports from Third Party Modules
+import unittest
+from pathlib import Path
+
+# Local Imports
+from pyseed.utils import read_map_file
+
+
+class UtilsTest(unittest.TestCase):
+    def test_mapping_file(self):
+        mappings = read_map_file(Path("tests/data/test-seed-data-mappings.csv"))
+        assert len(mappings) == 14
+
+        expected = {
+            "from_field": "Sq. Ft",
+            "from_units": "ft**2",
+            "to_field": "gross_floor_area",
+            "to_table_name": "PropertyState",
+        }
+        assert mappings[5] == expected
