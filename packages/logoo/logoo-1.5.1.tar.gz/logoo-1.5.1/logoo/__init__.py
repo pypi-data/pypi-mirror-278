938190bcef5c4b5715a4ea8aee24a8234bc5ce84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 17:04:42 +0000

    Add package logoo/1.5.1/logoo-1.5.1.tar.gz

---
+++ packages/logoo/logoo-1.5.1.tar.gz/logoo-1.5.1/logoo/__init__.py
@@ -0,0 +1,10 @@
+import logging
+from collections import namedtuple
+
+from .logger import Logger
+from .primary_logger import PrimaryLogger
+
+__version__ = "1.5.1"
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=1, minor=5, micro=1, releaselevel="final", serial=0)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
