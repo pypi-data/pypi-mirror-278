0ed35ca083db4d42ad13b954ddea99a0853ad49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:10:10 +0000

    Add package PyDEG/0.1.2/pydeg-0.1.2.tar.gz

---
+++ packages/PyDEG/pydeg-0.1.2.tar.gz/pydeg-0.1.2/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="PyDEG",
+    version='0.1.2',
+    python_requires=">=3.9.0",
+    license="MIT",
+    description="A python implementation of differential expression analysis.",
+    author="labjc",
+    author_email="jakec26384@gmail.com",
+    packages=find_packages()
+)
