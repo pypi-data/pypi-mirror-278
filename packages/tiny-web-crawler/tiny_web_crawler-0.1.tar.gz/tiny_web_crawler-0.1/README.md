665a514fbde020e2e930e29bb985445874190bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:04:49 +0000

    Add package tiny-web-crawler/0.1/tiny_web_crawler-0.1.tar.gz

---
+++ packages/tiny-web-crawler/tiny_web_crawler-0.1.tar.gz/tiny_web_crawler-0.1/README.md
@@ -0,0 +1,12 @@
+# A Tiny Web Crawler
+A web crawler written in python.
+
+# Install Requirements
+`pip install validators beautifulsoup4 lxml`
+
+### Python version: Python 3.6.3 :: Anaconda, Inc.
+
+# Run
+`python crawler.py`
+
+Starts crawling from `root_url` until `max_link` urls are crawled. Saves the output to `out.json`.
---
+++ packages/tiny-web-crawler/tiny_web_crawler-0.1.tar.gz/tiny_web_crawler-0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tiny-web-crawler',
+    version='0.1',
+    author='Indrajith Indraprastham',
+    author_email='indr4jith@gmail.com',
+    description='A simple web crawler.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/indrajithi/tiny-web-crawler',
+    packages=find_packages(),
+    install_requires=[
+        'validators',
+        'beautifulsoup4',
+        'lxml',
+        'colorama',
+        'requests'
+    ],
+    extras_require={
+        'dev': [
+            'mypy',
+            'pytest',
+            'responses'
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'tiny-web-crawler=crawler.crawler:main',
+        ],
+    },
+)
