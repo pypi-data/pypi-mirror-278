150971c5ff11d907ceb5f04f6f860010bde2a9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:07:07 +0000

    Add package vikivision/1.0.0/vikivision-1.0.0-py3-none-any.whl

---
+++ packages/vikivision/vikivision-1.0.0-py3-none-any.whl/vikivission/__init__.py
@@ -0,0 +1,11 @@
+from .Vission import speak_fast, get_image_hash, describe_image_with_gemini, ImageDescription, capture_and_process_image, stlit
+
+__all__ = [
+    "speak_fast",
+    "get_image_hash",
+    "describe_image_with_gemini",
+    "ImageDescription",
+    "capture_and_process_image",
+    "stlit"
+]
+
