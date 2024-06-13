6d2972003023ef3dafa6a5de1f58048e922b2bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 14:05:28 +0000

    Add package vlamalib/0.0.2/vlamalib-0.0.2.tar.gz

---
+++ packages/vlamalib/vlamalib-0.0.2.tar.gz/vlamalib-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Vlamaz AI utils package'
+LONG_DESCRIPTION = 'A package that contains useful functions for AI and Data Science.'
+
+# Setting up
+setup(
+    name="vlamalib",
+    version=VERSION,
+    author="Vlamaz (Vladyslav Mazurkevych)",
+    author_email="<srvlamaz@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy', 'requests'],
+    keywords=['python', 'machine learning', 'data', 'llama3', 'ai'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
