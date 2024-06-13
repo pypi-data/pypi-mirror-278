32a2b2c6b27e119543bf727182629c72196c742b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:33 +0000

    Add package rk-hadmisid-study/0.1.8/rk_hadmisid_study-0.1.8.tar.gz

---
+++ packages/rk-hadmisid-study/rk_hadmisid_study-0.1.8.tar.gz/rk_hadmisid_study-0.1.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+import glob
+
+setup(
+        name            ='rk_hadmisid_study',
+        version         ='0.1.8',
+        author          ='xuzh',
+        description     ='All tools and scripts used in the study of the hadron mis-ID.',
+        packages        = find_packages(),
+        package_data    = {'misID_data' : ['model/*/*/*/*.csv', 'model/*/*/*/*.json']},
+        install_requires= open('requirements.txt').read()
+        )
