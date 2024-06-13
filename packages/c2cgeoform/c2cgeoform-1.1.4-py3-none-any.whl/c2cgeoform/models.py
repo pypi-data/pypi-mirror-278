e790842f662cef757c9ffe49f9b57e66e4db295e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 17:04:48 +0000

    Add package c2cgeoform/1.1.4/c2cgeoform-1.1.4-py3-none-any.whl

---
+++ packages/c2cgeoform/c2cgeoform-1.1.4-py3-none-any.whl/c2cgeoform/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, LargeBinary, Text
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+from sqlalchemy.ext.declarative import declarative_base
+from zope.sqlalchemy import register
+from .ext import colander_ext
+
+DBSession = scoped_session(sessionmaker())
+register(DBSession)
+Base = declarative_base()
+
+
+class FileData():
+    id = Column(Integer, primary_key=True)
+    filename = Column(Text, nullable=True)
+    data = Column(LargeBinary, nullable=False, info={
+        'colanderalchemy': {
+            'typ': colander_ext.BinaryData()
+        }})
