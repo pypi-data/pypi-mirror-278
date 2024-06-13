fa9b82fbd01ac884fc1b3ea6a6fa6cff95bb3e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 04:05:48 +0000

    Add package py-filesearcher/0.2.0/py_filesearcher-0.2.0-py3-none-any.whl

---
+++ packages/py-filesearcher/py_filesearcher-0.2.0-py3-none-any.whl/py_filesearcher/readers/word.py
@@ -0,0 +1,16 @@
+import pathlib
+import typing
+
+import docx
+
+from py_filesearcher.readers.base import BaseReader
+
+
+class WordReader(BaseReader):
+    def __init__(self):
+        super().__init__()
+
+    def read(self, file_path: typing.Union[pathlib.Path, typing.IO[bytes]]) -> str:
+        document = docx.Document(file_path)
+        content = '\n'.join([p.text for p in document.paragraphs])
+        return content
