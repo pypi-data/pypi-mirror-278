8afdd8d5e7b9b6921939f9684ec4501244263e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:04:40 +0000

    Add package xfem/2.1.2305.dev16/xfem-2.1.2305.dev16-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/xfem/xfem-2.1.2305.dev16-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/xfem/_version.pyi
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from builtins import object as VERSION_TUPLE
+__all__ = ['VERSION_TUPLE', 'version', 'version_tuple']
+__version__: str = '2.1.2305.dev16'
+__version_tuple__: tuple = (2, 1, 2305, 'dev16')
+version: str = '2.1.2305.dev16'
+version_tuple: tuple = (2, 1, 2305, 'dev16')
