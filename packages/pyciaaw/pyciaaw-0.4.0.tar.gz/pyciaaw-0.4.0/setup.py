3100f4ad08d14f555ab207058faac962f7789b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:43 +0000

    Add package pyciaaw/0.4.0/pyciaaw-0.4.0.tar.gz

---
+++ packages/pyciaaw/pyciaaw-0.4.0.tar.gz/pyciaaw-0.4.0/setup.py
@@ -0,0 +1,35 @@
+r"""Setup."""
+import platform
+from setuptools import setup, Extension
+
+libraries = None
+library_dirs = None
+runtime_library_dirs = None
+extra_objects = None
+
+if platform.system() == "Linux":
+    libraries = ["cciaaw"]
+    library_dirs = ["./src/pyciaaw"]
+    runtime_library_dirs = ["$ORIGIN"]
+if platform.system() == "Windows":
+    extra_objects = ["./src/pyciaaw/libcciaaw.dll.a"]
+if platform.system() == "Darwin":
+    libraries = ["cciaaw"]
+    library_dirs = ["./src/pyciaaw"]
+    runtime_library_dirs = ["@loader_path"]
+
+if __name__ == "__main__":
+
+    mod_version = Extension(name="pyciaaw.version",
+                        sources=["./src/pyciaaw/cpy_ciaaw_version.c"],
+                        libraries=libraries,
+                        library_dirs=library_dirs,
+                        runtime_library_dirs=runtime_library_dirs,
+                        extra_objects=extra_objects)
+    mod_saw_2021 = Extension(name="pyciaaw.saw_2021",
+                        sources=["./src/pyciaaw/cpy_ciaaw_saw_2021.c"],
+                        libraries=libraries,
+                        library_dirs=library_dirs,
+                        runtime_library_dirs=runtime_library_dirs,
+                        extra_objects=extra_objects)
+    setup(ext_modules=[mod_version, mod_saw_2021])
