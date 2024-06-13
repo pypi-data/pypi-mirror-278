b64e9c078c4ee63226ca53ba2260128ac3566ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:39 +0000

    Add package django-codenerix/4.0.98/django_codenerix-4.0.98-py2.py3-none-any.whl

---
+++ packages/django-codenerix/django_codenerix-4.0.98-py2.py3-none-any.whl/codenerix/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "4.0.98"
+
+__authors__ = [
+    "Juanmi Taboada",
+    "Juan Soler Ruiz",
+]
+
+__authors_email__ = [
+    "juanmi@juanmitaboada.com",
+    "soleronline@gmail.com",
+]
+
+__all__ = ["__version__", "__authors__", "__authors_email__"]
