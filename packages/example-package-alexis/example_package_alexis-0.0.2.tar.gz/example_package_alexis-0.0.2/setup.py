f817b0c460adc925aa0c86d48ff8d094c62dcdb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 14:05:28 +0000

    Add package example-package-alexis/0.0.2/example_package_alexis-0.0.2.tar.gz

---
+++ packages/example-package-alexis/example_package_alexis-0.0.2.tar.gz/example_package_alexis-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+setup(
+    name='example_package_alexis',
+    version='0.0.2',
+    description='Esta es la descripción del paquete',
+    long_description=readme.read(),
+    author='Alexis Barria',
+    author_email='alexis.barria.c@outlook.cl',
+    classifiers=[],
+    license='MIT',
+    include_package_data=True
+)
