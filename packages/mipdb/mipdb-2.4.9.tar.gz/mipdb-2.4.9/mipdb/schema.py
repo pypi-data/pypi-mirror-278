d660bc974fd1bfa4869acb9c4a9b06b381c6ef55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 17:04:43 +0000

    Add package mipdb/2.4.9/mipdb-2.4.9.tar.gz

---
+++ packages/mipdb/mipdb-2.4.9.tar.gz/mipdb-2.4.9/mipdb/schema.py
@@ -0,0 +1,29 @@
+import sqlalchemy as sql
+
+from mipdb.database import DataBase
+from mipdb.exceptions import UserInputError
+
+
+class Schema:
+    name: str
+    schema: sql.MetaData
+
+    def __init__(self, name) -> None:
+        self.name = name
+        self._validate_schema_name()
+        self.schema = sql.MetaData(schema=self.name)
+
+    def __repr__(self) -> str:
+        return f"Schema(name={self.name})"
+
+    def create(self, db: DataBase):
+        db.create_schema(self.name)
+
+    def drop(self, db: DataBase):
+        db.drop_schema(self.name)
+
+    def _validate_schema_name(self):
+        if '"' in self.name:
+            raise UserInputError(
+                f"Data model's name: {self.name} contains prohibited character double quotes (\")"
+            )
