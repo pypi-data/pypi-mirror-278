7f903e88db69ca6caf8f4aab5eda3e226900ad19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 05:06:32 +0000

    Add package al-microsoft-teams/1.0.3/al-microsoft-teams-1.0.3.tar.gz

---
+++ packages/al-microsoft-teams/al-microsoft-teams-1.0.3.tar.gz/al-microsoft-teams-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='al-microsoft-teams',
+    version='1.0.3',
+    packages=['microsoft_teams'],
+    install_requires=[
+        'requests',
+        'msal',
+        'alsacommonutils'
+    ],
+    author='Rashelle Woudberg',
+    author_email='rashelle@al.co.za',
+    description='A Python wrapper to send messages to Microsoft Teams.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AutumnLeafIT/al-aws-notification-modules',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
