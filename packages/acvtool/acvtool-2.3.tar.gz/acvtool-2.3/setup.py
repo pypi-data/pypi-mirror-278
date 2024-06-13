475b220970b4fd4a4b9462e71833b1733fed4ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 06:04:44 +0000

    Add package acvtool/2.3/acvtool-2.3.tar.gz

---
+++ packages/acvtool/acvtool-2.3.tar.gz/acvtool-2.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='acvtool',
+    version='2.3',
+    author='Aleksandr Pilgun',
+    author_email='alexand.pilgun@gmail.com',
+    description="ACVTool is an instrumentation-based tool to measure and visualize instruction coverage for Android apps.",
+    url='https://github.com/pilgun/acvtool',
+    packages=['acvtool'],
+    install_requires=[
+        'PyYAML==6.0.1',
+        'Chameleon==4.5.4',
+        'javaobj-py3==0.4.4',
+        'six==1.12.0',
+        'androguard==4.0.2',
+        'pyaxml==0.0.5',
+        'typing_extensions==4.7.1',
+        'setuptools==70.0.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'acv=acvtool.acvtool:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
