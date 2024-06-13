701e1a411884c4d43c122fe13274f98bf84ffe94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 15:05:33 +0000

    Add package ofnodes/2.2.1/ofnodes-2.2.1-py3-none-any.whl

---
+++ packages/ofnodes/ofnodes-2.2.1-py3-none-any.whl/ofnodes/__init__.py
@@ -0,0 +1,35 @@
+__docformat__ = "restructuredtext"
+
+import logging
+from ofnodes.nodes.singlynode import SinglyNode
+from ofnodes.structures.singlylinkedlist import SinglyLinkedList
+from ofnodes.structures.randomaccessarray import RandomAccessArray
+from ofnodes.structures.stack import Stack
+
+
+
+__doc__ = """
+ofnodes - an illustration of node-based objects and algorithms in Python
+========================================================================
+**ofnodes** is a Python Package providing examples of the Author's ability
+to implement data structures and algorithms in Python. It is not a substitute
+for built-in or other third-party implementations of the same data structures
+and algorithms. It aims to illustrate the author's ability to produce a
+distributable package of objects behind a thoughtful user interface.
+
+Main Features
+-------------
+Here is what ofnodes can do:
+    - implement a node object with a unidirectional pointer
+    - implement and manipulate a linked list object of unidirectional,
+    i.e., 'singly linked' nodes.
+
+Included in the library is a Tail descriptor designed to manage the tail attribute of
+linked data structures. While the primary purpose of the Tail descriptor is to
+facilitate efficient management of the data structure's tail node, its implementation
+also aligns with best practices for preventing cyclic dependencies within linked data
+structures. By adhering to these design principles, the library promotes safe and
+reliable usage of linked data structures, reducing the risk of unintended cyclic
+references and associated issues.
+"""
+__all__ = ['SinglyNode','SinglyLinkedList', 'RandomAccessArray', 'Stack']
---
+++ packages/ofnodes/ofnodes-2.2.1-py3-none-any.whl/ofnodes/components/nodes/mixins.py
@@ -0,0 +1,28 @@
+# ofnodes/components/nodes/mixins.py
+#from ofnodes.nodes.singlynode import SinglyNode
+class AddMixin:
+    __slots__ = ()
+    def __add__(self, other):
+        """Add data to the node's data attribute.
+
+        Args:
+            other: The data to add to the node's data attribute.
+
+        Raises:
+            ValueError: If the provided data is invalid or if the node is not an instance of SinglyNode.
+
+        Notes:
+            This method adds the provided data to the node's data attribute after performing
+            any necessary validation.
+
+        Returns:
+            None
+
+        Examples:  TODO
+
+        """
+        if other:
+            validated_data = other  # TODO: validate data
+            self._data = self._data + validated_data
+            return
+        raise ValueError("Invalid data to add to SinglyNode.data")
