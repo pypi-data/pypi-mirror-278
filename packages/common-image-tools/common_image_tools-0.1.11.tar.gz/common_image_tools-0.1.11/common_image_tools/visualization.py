579742478dd2445b6cfee9cfef1bb680bebc9abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 03:09:42 +0000

    Add package common-image-tools/0.1.11/common_image_tools-0.1.11.tar.gz

---
+++ packages/common-image-tools/common_image_tools-0.1.11.tar.gz/common_image_tools-0.1.11/common_image_tools/visualization.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import cv2
+
+
+def draw_points_shape(img, roi_points, color):
+    for v in range(1, len(roi_points)):
+        cv2.line(img, roi_points[v], roi_points[v - 1], color, 2)
+
+    cv2.line(img, roi_points[0], roi_points[-1], color, 2)
+
+    return img
