51570ab2d62f8e57b826117baea689020690cf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 04:06:08 +0000

    Add package python-ics/915.14/python_ics-915.14-cp310-cp310-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/python-ics/python_ics-915.14-cp310-cp310-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/ics/structures/mac_sec_flags_t.py
@@ -0,0 +1,27 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+
+
+class Nameless8987(ctypes.Structure):
+    _pack_ = 1
+    _fields_ = [
+        ('en', ctypes.c_uint32, 1),
+        ('reserved', ctypes.c_uint32, 31),
+    ]
+
+
+
+class mac_sec_flags_t(ctypes.Union):
+    _pack_ = 1
+    _anonymous_  = ('Nameless8987',)
+    _fields_ = [
+        ('Nameless8987', Nameless8987),
+        ('flags_32b', ctypes.c_uint32),
+    ]
+
+
+_MACSecFlags = mac_sec_flags_t
+MACSecFlags_t = mac_sec_flags_t
+
