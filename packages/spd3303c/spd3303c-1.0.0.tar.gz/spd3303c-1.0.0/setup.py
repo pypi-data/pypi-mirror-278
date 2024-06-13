b50a0f92b5a1a0f07dc1250e8399c3c6cc3407f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 06:05:16 +0000

    Add package spd3303c/1.0.0/spd3303c-1.0.0.tar.gz

---
+++ packages/spd3303c/spd3303c-1.0.0.tar.gz/spd3303c-1.0.0/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+requirements = ["numpy", "pyvisa-py", "click", "python-vxi11", "zeroconf", "psutil"]
+
+setup(
+    name="spd3303c",
+    version="1.0.0",
+    description="Python API for SigLent SPD3303X/E/C power supplies.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=["spd3303c"],
+    license="MIT",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=requirements,
+    author="Kai Geissdoerfer",
+    author_email="kai.geissdoerfer@tu-dresden.de",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest>=3.9"],
+    entry_points={"console_scripts": ["spd-ctrl=spd3303x.cli:cli"]},
+)
