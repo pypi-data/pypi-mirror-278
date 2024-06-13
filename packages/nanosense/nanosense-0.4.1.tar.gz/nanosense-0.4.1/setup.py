104de1132cd78cff37fc780e7e4d917415defe0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-13 15:06:25 +0000

    Add package nanosense/0.4.1/nanosense-0.4.1.tar.gz

---
+++ packages/nanosense/nanosense-0.4.1.tar.gz/nanosense-0.4.1/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nanosense',
+    version='0.4.1',
+    description='A comprehensive package for nanosense data analysis and visualization.',
+    author='Shankar Dutt',
+    author_email='shankar.dutt@anu.edu.au',
+    url='https://github.com/shankardutt/nanosense',
+    packages=find_packages(include=['nanosense', 'nanosense.*']),
+    include_package_data=True,
+    package_data={
+        'nanosense': ['icons.icns', 'image_1.jpg'],
+    },
+    install_requires=[
+        'PySide6',
+        'cryptography',
+        'matplotlib',
+        'neo',
+        'numpy',
+        'pyabf',
+        'scipy',
+        'joblib',
+        'bottleneck',
+        'ruptures',
+        'pywavelets',
+        'detecta',
+        'hmmlearn',
+        'scikit-learn',
+        'h5py',
+        'seaborn',
+        'pandas',
+        'tabulate',
+        'sktime',
+        'lightgbm',
+        'torch',
+        'torchvision',
+        'tensorflow',
+        'numexpr',
+        'uncertainties',
+        'pyqtgraph',
+        'pynvml',
+        'psutil'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    entry_points={
+        'console_scripts': [
+            'nanosense=nanosense.nanosense:main',
+        ],
+    },
+)
