83925246bd75736d5dee4a2f0fca257edac893e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:01 +0000

    Add package ewhs-api-python/1.2.0/ewhs_api_python-1.2.0-py3-none-any.whl

---
+++ packages/ewhs-api-python/ewhs_api_python-1.2.0-py3-none-any.whl/ewhs/resources.py
@@ -0,0 +1,44 @@
+from .mixins import ListResourceMixin, DeleteResourceMixin, DetailResourceMixin, \
+    UpdateResourceMixin, CreateResourceMixin
+
+
+class Resource(object):
+    resource = None
+
+    def __init__(self, api):
+        self._api = api
+
+
+class CRUDResource(
+    ListResourceMixin,
+    DeleteResourceMixin,
+    DetailResourceMixin,
+    UpdateResourceMixin,
+    CreateResourceMixin,
+    Resource
+):
+    pass
+
+
+class Shipment(ListResourceMixin, DetailResourceMixin, Resource):
+    resource = 'shipments'
+
+
+class Order(ListResourceMixin, DetailResourceMixin, CreateResourceMixin, UpdateResourceMixin, Resource):
+    resource = 'orders'
+
+
+class Article(ListResourceMixin, DetailResourceMixin, CreateResourceMixin, UpdateResourceMixin, Resource):
+    resource = 'articles'
+
+
+class Webhook(ListResourceMixin, DetailResourceMixin, CreateResourceMixin, UpdateResourceMixin, Resource):
+    resource = 'webhooks'
+
+
+class Stock(ListResourceMixin, Resource):
+    resource = 'stock'
+
+
+class ShippingMethod(ListResourceMixin, DetailResourceMixin, Resource):
+    resource = 'shippingmethods'
