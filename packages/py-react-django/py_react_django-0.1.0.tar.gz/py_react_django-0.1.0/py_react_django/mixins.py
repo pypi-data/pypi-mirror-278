1f825c6754c25eb0d7b8514aa59f9c0db565fb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:35 +0000

    Add package py-react-django/0.1.0/py_react_django-0.1.0.tar.gz

---
+++ packages/py-react-django/py_react_django-0.1.0.tar.gz/py_react_django-0.1.0/py_react_django/mixins.py
@@ -0,0 +1,19 @@
+class ReactMixin:
+    """
+    Export Props for React App.
+    html include react_props.html
+    """
+    app_root = ''
+
+    def get_app_root(self):
+        return self.app_root
+
+    @staticmethod
+    def get_props_data():
+        return {}
+
+    def get_context_data(self, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['props'] = self.get_props_data()
+        context['app_root'] = self.get_app_root()
+        return context
