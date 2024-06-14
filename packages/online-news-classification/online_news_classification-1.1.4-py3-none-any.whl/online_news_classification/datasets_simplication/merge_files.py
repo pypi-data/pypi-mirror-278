47a4162ea5eb7e57c81d3bbfa08948dc4e206bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:21 +0000

    Add package online-news-classification/1.1.4/online_news_classification-1.1.4-py3-none-any.whl

---
+++ packages/online-news-classification/online_news_classification-1.1.4-py3-none-any.whl/online_news_classification/datasets_simplication/merge_files.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import sys
+import time
+
+import pandas as pd
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from functions import manage_datasets, setup
+from natsort import realsorted
+
+load_dotenv()
+
+
+def extract_integer(filename):
+    logging.info(filename)
+    if filename.endswith(".csv"):
+        return int(filename.split(".")[0].split("_")[1])
+    else:
+        return -1
+
+
+def main():
+    args = setup.get_arg_parser_merge().parse_args()
+    start_time = setup.initialize("merge_files_" + args.dataset)
+    input_dir = os.path.join(os.getcwd(), os.getenv("DATASETS_FOLDER") + args.input_dir)
+    lines = []
+    for filename in realsorted(os.listdir(input_dir)):
+        if filename.endswith(".csv"):
+            lines.append(pd.read_csv(os.path.join(input_dir, filename), delimiter=";"))
+    df_res = pd.concat(lines, ignore_index=True)
+    df_res = df_res.drop(["Unnamed: 0"], axis=1)
+    manage_datasets.save_dataset(df_res, args.output_file)
+    logging.info("--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == "__main__":
+    main()
