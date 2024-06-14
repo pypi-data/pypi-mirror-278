4689aa6c54cbfa2dfb2992b56b07573e2b9020bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:46 +0000

    Add package optimizeai/0.1.1/optimizeai-0.1.1.tar.gz

---
+++ packages/optimizeai/optimizeai-0.1.1.tar.gz/optimizeai-0.1.1/optimizeai/config.py
@@ -0,0 +1,11 @@
+# optimizeai/config.py
+
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    """Config class for OptimAI"""
+    llm: str
+    key: str
+    model: str
+    mode: str = "online"
