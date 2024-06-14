e143f8d425fbe29bf9de0342d708c5e741832624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:22 +0000

    Add package esgpull/0.6.3/esgpull-0.6.3-py3-none-any.whl

---
+++ packages/esgpull/esgpull-0.6.3-py3-none-any.whl/esgpull/presets.py
@@ -0,0 +1,13 @@
+# from esgpull.models import Query
+
+
+# IPCC_SCENARIOS = Query(
+#     select=dict(
+#         query=(
+#             "experiment:(rcp26 rcp45 rcp60 rcp85) OR "
+#             "experiment_id:(ssp119 ssp126 ssp245 ssp370 ssp460 ssp585)"
+#         )
+#     )
+# )
+
+# TEMPERATURE = Query(select=dict(variable=["tas", "tos", "tasmin", "tasmax"]))
