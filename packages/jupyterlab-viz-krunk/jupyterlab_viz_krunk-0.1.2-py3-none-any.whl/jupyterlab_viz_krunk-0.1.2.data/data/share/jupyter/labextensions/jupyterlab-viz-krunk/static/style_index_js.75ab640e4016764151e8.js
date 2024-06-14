286dc984944729f8bb6d7578aa6ee415239c09dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:09 +0000

    Add package jupyterlab-viz-krunk/0.1.2/jupyterlab_viz_krunk-0.1.2-py3-none-any.whl

---
+++ packages/jupyterlab-viz-krunk/jupyterlab_viz_krunk-0.1.2-py3-none-any.whl/jupyterlab_viz_krunk-0.1.2.data/data/share/jupyter/labextensions/jupyterlab-viz-krunk/static/style_index_js.75ab640e4016764151e8.js
@@ -0,0 +1,13 @@
+(self["webpackChunkjupyterlab_viz_krunk"] = self["webpackChunkjupyterlab_viz_krunk"] || []).push([["style_index_js"],{
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: Identifier 'observer' has already been declared (95:6)\nFile was processed with these loaders:\n * ./node_modules/@jupyterlab/builder/node_modules/source-map-loader/dist/cjs.js\nYou may need an additional loader to handle the result of these loaders.\n| \n| // Create an observer instance\n> const observer = new MutationObserver(handleMutations);\n| \n| // Configuration of the observer");
+
+/***/ })
+
+}]);
