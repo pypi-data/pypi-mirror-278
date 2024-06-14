b593695af411546983ff18248a9a1b66d5a64b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:28 +0000

    Add package testjuanmferreira93/0.0.3/testjuanmferreira93-0.0.3.tar.gz

---
+++ packages/testjuanmferreira93/testjuanmferreira93-0.0.3.tar.gz/testjuanmferreira93-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testjuanmferreira93',
+    version='0.0.3',
+    description='This is a test library',
+    author='Juanma Ferreira',
+    packages=find_packages()
+)
