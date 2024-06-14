3110e43e0d6d028913f6e378fefe015a97754a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:42 +0000

    Add package TkSel/1.0.0/tksel-1.0.0.tar.gz

---
+++ packages/TkSel/tksel-1.0.0.tar.gz/tksel-1.0.0/src/tksel/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present Marceau-h <106751184+Marceau-h@users.noreply.github.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+from .__about__ import __version__
+from .TkSel import TkSel
+
+__all__ = ["TkSel", "__version__"]
