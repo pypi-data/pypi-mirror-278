b8f22c52c3a6242828c8df9eb67807f8b6fbd29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:18 +0000

    Add package ex4nicegui/0.6.8/ex4nicegui-0.6.8-py3-none-any.whl

---
+++ packages/ex4nicegui/ex4nicegui-0.6.8-py3-none-any.whl/ex4nicegui/reactive/officials/button.py
@@ -0,0 +1,60 @@
+from typing import (
+    Any,
+    Callable,
+    Optional,
+)
+from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
+from ex4nicegui.utils.signals import (
+    TGetterOrReadonlyRef,
+    _TMaybeRef as TMaybeRef,
+    to_value,
+)
+from nicegui import ui
+from .base import BindableUi, DisableableMixin
+
+
+class ButtonBindableUi(BindableUi[ui.button], DisableableMixin):
+    def __init__(
+        self,
+        text: TMaybeRef[str] = "",
+        *,
+        on_click: Optional[Callable[..., Any]] = None,
+        color: Optional[TMaybeRef[str]] = "primary",
+        icon: Optional[TMaybeRef[str]] = None,
+    ) -> None:
+        pc = ParameterClassifier(
+            locals(), maybeRefs=["text", "color", "icon"], events=["on_click"]
+        )
+
+        element = ui.button(**pc.get_values_kws())
+
+        super().__init__(element)
+
+        for key, value in pc.get_bindings().items():
+            self.bind_prop(key, value)  # type: ignore
+
+    def bind_prop(self, prop: str, ref_ui: TGetterOrReadonlyRef):
+        if prop == "text":
+            return self.bind_text(ref_ui)
+        if prop == "icon":
+            return self.bind_icon(ref_ui)
+
+        return super().bind_prop(prop, ref_ui)
+
+    def bind_text(self, ref_ui: TGetterOrReadonlyRef):
+        @self._ui_effect
+        def _():
+            ele = self.element
+            ele._props["label"] = to_value(ref_ui)
+            ele.update()
+
+        return self
+
+    def bind_icon(self, ref_ui: TGetterOrReadonlyRef[str]):
+        @self._ui_effect
+        def _():
+            ele = self.element
+            ele._props["icon"] = to_value(ref_ui)
+            ele.update()
+
+        return self
---
+++ packages/ex4nicegui/ex4nicegui-0.6.8-py3-none-any.whl/ex4nicegui/reactive/officials/label.py
@@ -0,0 +1,52 @@
+from typing import Any
+from ex4nicegui.reactive.services.reactive_service import ParameterClassifier
+from ex4nicegui.utils.signals import (
+    TGetterOrReadonlyRef,
+    to_value,
+    _TMaybeRef as TMaybeRef,
+)
+from nicegui import ui
+from .base import BindableUi
+
+
+class LabelBindableUi(BindableUi[ui.label]):
+    def __init__(
+        self,
+        text: TMaybeRef[Any] = "",
+    ) -> None:
+        pc = ParameterClassifier(locals(), maybeRefs=["text"], events=[])
+
+        element = ui.label(**pc.get_values_kws())
+        super().__init__(element)
+
+        for key, value in pc.get_bindings().items():
+            self.bind_prop(key, value)  # type: ignore
+
+    @property
+    def text(self):
+        return self.element.text
+
+    def bind_prop(self, prop: str, ref_ui: TGetterOrReadonlyRef):
+        if prop == "text":
+            return self.bind_text(ref_ui)
+
+        if prop == "color":
+            return self.bind_color(ref_ui)
+
+        return super().bind_prop(prop, ref_ui)
+
+    def bind_color(self, ref_ui: TGetterOrReadonlyRef):
+        @self._ui_effect
+        def _():
+            ele = self.element
+            color = to_value(ref_ui)
+            ele._style["color"] = color
+            ele.update()
+
+    def bind_text(self, ref_ui: TGetterOrReadonlyRef):
+        @self._ui_effect
+        def _():
+            self.element.set_text(str(to_value(ref_ui)))
+            self.element.update()
+
+        return self
