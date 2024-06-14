6f6f7904ca4ba906b784fe8ea6a391b30e6c4a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:14 +0000

    Add package GSCpy/1.0.2/gscpy-1.0.2.tar.gz

---
+++ packages/GSCpy/gscpy-1.0.2.tar.gz/gscpy-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='GSCpy',
+    version='1.0.2',
+    description='Generalized Spectral Clustering in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Malik Hacini',
+    author_email='mhacini.pro@gmail.com',
+    url='https://github.com/Malik-Hacini/GSCpy',  # Update this with your project's URL
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy','matplotlib','scipy','scikit-learn']
+)
