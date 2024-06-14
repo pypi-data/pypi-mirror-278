076ce9ae724317690aac327f31c02c59f52c4974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:02 +0000

    Add package jupyterlab-viz-krunk/0.1.4/jupyterlab_viz_krunk-0.1.4.tar.gz

---
+++ packages/jupyterlab-viz-krunk/jupyterlab_viz_krunk-0.1.4.tar.gz/jupyterlab_viz_krunk-0.1.4/jupyterlab_viz_krunk/labextension/static/style_index_js.4efe18f268621978c558.js
@@ -0,0 +1,13 @@
+(self["webpackChunkjupyterlab_viz_krunk"] = self["webpackChunkjupyterlab_viz_krunk"] || []).push([["style_index_js"],{
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ (() => {
+
+throw new Error("Module parse failed: Unexpected token (47:31)\nFile was processed with these loaders:\n * ./node_modules/@jupyterlab/builder/node_modules/source-map-loader/dist/cjs.js\nYou may need an additional loader to handle the result of these loaders.\n|             if (node.nodeType === Node.ELEMENT_NODE) {\n|                 const nestedElements = node.querySelectorAll(\"code.language-graphviz\");\n>                 nestedElements..forEach(handleLanguageVizElement);\n|                 // const nestedElements2 = node.querySelectorAll(\"code.language-graphviz-box\");\n|                 // nestedElements2.forEach(element => handleLanguageVizElement(element, 'box'));");
+
+/***/ })
+
+}]);
