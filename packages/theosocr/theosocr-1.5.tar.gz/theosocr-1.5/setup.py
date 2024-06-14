5c772b14b04127ecb9c6d17fcac3338d110be2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:27 +0000

    Add package theosocr/1.5/theosocr-1.5.tar.gz

---
+++ packages/theosocr/theosocr-1.5.tar.gz/theosocr-1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='theosocr',
+    version='1.5',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='theostekno',
+    author_email='theostekno@gmail.com',
+    description='A simple Theos library using Theos API',
+    url='https://github.com/emretecno/theosocr',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/theosocr/theosocr-1.5.tar.gz/theosocr-1.5/theosocr/__init__.py
@@ -0,0 +1,1 @@
+from .ocr import theos_ocr_file
