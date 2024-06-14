608e8e4ef3e61c0ef5c55119ec84ffae7be016f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-14 03:05:21 +0000

    Add package bagbag/0.69.3/bagbag-0.69.3-py3-none-any.whl

---
+++ packages/bagbag/bagbag-0.69.3-py3-none-any.whl/bagbag/Os/__init__.py
@@ -0,0 +1,62 @@
+
+from typing import TYPE_CHECKING
+from lazy_imports import LazyImporter
+import sys
+
+_import_structure = {
+    "src": [
+        "Exit", 
+        "System",
+        "Mkdir",
+        "ListDir",
+        "ListFiles",
+        "Getenv",
+        "Getcwd",
+        "Unlink",
+        "Move",
+        "Copy",
+        "GetLoginUserName",
+        "Walk",
+        "GetUID",
+        "Args",
+        "GetCurrentThreadID",
+        "Chdir",
+        "Touch",
+        "Stdin",
+        "Stdout"
+    ],
+    "Path": ["Path"]
+}
+
+if TYPE_CHECKING:
+    from .src import (
+        Exit,
+        System,
+        Mkdir,
+        ListDir,
+        ListFiles,
+        Getenv,
+        Getcwd,
+        Unlink,
+        Move,
+        Copy,
+        GetLoginUserName,
+        Walk,
+        GetUID,
+        Args,
+        GetCurrentThreadID,
+        Chdir,
+        Touch,
+        Stdin,
+        Stdout
+    )
+    from . import Path
+else:
+    sys.modules[__name__] = LazyImporter(
+        __name__,
+        globals()["__file__"],
+        _import_structure,
+        extra_objects={},
+    )
+
+
